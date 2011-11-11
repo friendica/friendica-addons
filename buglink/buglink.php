580863bddc32a7133ce22638675cbde92faa04ea
Friendika <info@friendika.com>
2011-11-11 01:26:46 -0800

    missed semicolon

--- buglink/buglink.php
+++ buglink/buglink.php
@@ -10,6 +10,6 @@
 function buglink_install() { register_hook('page_end', 'addon/buglink/buglink.php', 'buglink_active'); }
 
 
-function buglink_uninstall() { unregister_hook('page_end', 'addon/buglink/buglink.php', 'buglink_active') }
+function buglink_uninstall() { unregister_hook('page_end', 'addon/buglink/buglink.php', 'buglink_active'); }
 
 function buglink_active(&$a,&$b) { $b .= '<div style="position: fixed; bottom: 5px; left: 5px;"><a href="http://bugs.friendica.com" title="' . t('Report Bug') . '"><img src="addon/buglink/bug-x.gif" alt="' . t('Report Bug') . '" /></a></div>'; } 
