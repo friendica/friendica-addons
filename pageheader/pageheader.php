6493559ee827cfddda8e57452f96314dae2a4b6c
root <root@debian.hp.keith.li>
2011-12-28 07:36:15 +0000

    new file:   pageheader.tgz For admins to make system announcements 	new file:   pageheader/README For admins to make system announcements 	new file:   pageheader/pageheader.css For admins to make system announcements 	new file:   pageheader/pageheader.php For admins to make system announcements

---
+++ pageheader/pageheader.php
@@ -0,0 +1,78 @@
+<?php
+
+
+/**
+ * Name: Page Header
+ * Description: Inserts a page header
+ * Version: 1.0
+ * Author: Keith Fernie <http://friendika.me4.it/profile/keith>
+ * 
+ */
+
+function pageheader_install() {
+    register_hook('page_header', 'addon/pageheader/pageheader.php', 'pageheader_fetch');
+	register_hook('plugin_settings', 'addon/pageheader/pageheader.php', 'pageheader_addon_settings');
+	register_hook('plugin_settings_post', 'addon/pageheader/pageheader.php', 'pageheader_addon_settings_post');
+
+}
+
+
+function pageheader_uninstall() {
+    unregister_hook('page_header', 'addon/pageheader/pageheader.php', 'pageheader_fetch');
+	unregister_hook('plugin_settings', 'addon/pageheader/pageheader.php', 'pageheader_addon_settings');
+	unregister_hook('plugin_settings_post', 'addon/pageheader/pageheader.php', 'pageheader_addon_settings_post');
+
+}
+
+
+
+
+
+function pageheader_addon_settings(&$a,&$s) {
+
+
+	if(! is_site_admin())
+		return;
+
+    /* Add our stylesheet to the page so we can make our settings look nice */
+
+    $a->page['htmlhead'] .= '<link rel="stylesheet"  type="text/css" href="' . $a->get_baseurl() . '/addon/pageheader/pageheader.css' . '" media="all" />' . "\r\n";
+
+
+	$words = get_config('pageheader','text');
+	if(! $words)
+		$words = 'pageheader,';
+
+    $s .= '<div class="settings-block">';
+    $s .= '<h3>' . t('"pageheader" Settings') . '</h3>';
+    $s .= '<div id="pageheader-wrapper">';
+    $s .= '<input id="pageheader-words" type="text" size="80" name="pageheader-words" value="' . $words .'" />';
+    $s .= '</div><div class="clear"></div>';
+
+    $s .= '<div class="settings-submit-wrapper" ><input type="submit" id="pageheader-submit" name="pageheader-submit" class="settings-submit" value="' . t('Submit') . '" /></div></div>';
+
+	return;
+
+}
+
+function pageheader_addon_settings_post(&$a,&$b) {
+
+	if(! is_site_admin())
+		return;
+
+	if($_POST['pageheader-submit']) {
+		set_config('pageheader','text',trim(strip_tags($_POST['pageheader-words'])));
+		info( t('pageheader Settings saved.') . EOL);
+	}
+}
+
+function pageheader_fetch($a,&$b) {
+
+    $a->page['htmlhead'] .= '<link rel="stylesheet" type="text/css" href="'
+        . $a->get_baseurl() . '/addon/pageheader/pageheader.css' . '" media="all" />' . "\r\n";
+    $s = get_config('pageheader','text');
+    if(! $s)
+        $s = '';
+    if ($s != '')
+    	$b .= '<div class="pageheader">' . $s . '</div>';
+}
