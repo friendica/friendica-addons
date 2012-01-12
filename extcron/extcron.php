65011212a8d282da4ae4ff1a05c332f3ef5830cb
friendica <info@friendica.com>
2012-01-11 20:56:22 -0800

    add extcron

---
+++ extcron/extcron.php
@@ -0,0 +1,21 @@
+<?php
+
+
+/**
+ * Name: external cron
+ * Description: Use external server or service to run poller regularly
+ * Version: 1.0
+ * Author: Mike Macgirvin <http://macgirvin.com/profile/mike>
+ * 
+ * Notes: External service needs to make a web request to http(s)://yoursite/extcron
+ */
+
+function extcron_install() {}
+
+function nsfw_uninstall() {}
+
+function extcron_module() {}
+
+function extcron_init(&$a) {
+	proc_run('php','include/poller.php');
+}
