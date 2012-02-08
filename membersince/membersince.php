08af41bc9a21be7201d863a90bf38f589698892f
friendica <info@friendica.com>
2012-02-07 20:18:42 -0800

    fix membersince for new profile html

--- membersince/membersince.php
+++ membersince/membersince.php
@@ -14,4 +14,7 @@ function membersince_install(){ register_hook('profile_advanced','addon/membersi
 
 function membersince_uninstall(){ unregister_hook('profile_advanced','addon/membersince/membersince.php','membersince_display'); }
 
-function membersince_display(&$a,&$b) { $b = str_replace('</div>' . "\n" . '<div id="advanced-profile-name-end"></div>',sprintf( t(' - Member since: %s') . EOL, datetime_convert('UTC',date_default_timezone_get(),$a->profile['register_date'])) . '</div>' . "\n" . '<div id="advanced-profile-name-end"></div>',$b); }
+function membersince_display(&$a,&$b) { 
+$b = preg_replace('/<\/dl>/',"</dl>\n<dl><dt>" . t('Member since:') . '</dt><dd>' . datetime_convert('UTC',date_default_timezone_get(),$a->profile['register_date']) . '</dd></dl>' ,$b, 1); 
+//$b = str_replace('</div>' . "\n" . '<div id="advanced-profile-name-end"></div>',sprintf( t(' - Member since: %s') . EOL, datetime_convert('UTC',date_default_timezone_get(),$a->profile['register_date'])) . '</div>' . "\n" . '<div id="advanced-profile-name-end"></div>',$b); 
+}
