999dbb07b5467336ca7aaa12f3a98cd0c1bd31b6
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2012-04-23 21:49:34 +0200

    IMPRESSUM: added bbcode support, stripping html

--- impressum/impressum.php
+++ impressum/impressum.php
@@ -2,10 +2,12 @@
 /**
  * Name: Impressum
  * Description: Plugin to add contact information to the about page (/friendica)
- * Version: 1.1
+ * Version: 1.2
  * Author: Tobias Diekershoff <http://diekershoff.homeunix.net/friendika/profile/tobias>
  * License: 3-clause BSD license
  */
+
+require_once('include/bbcode.php');
 
 function impressum_install() {
     register_hook('about_hook', 'addon/impressum/impressum.php', 'impressum_show');
@@ -24,7 +26,7 @@ function obfuscate_email ($s) {
     return $s;
 }
 function impressum_footer($a, &$b) {
-    $text = get_config('impressum','footer_text');
+    $text = bbcode(get_config('impressum','footer_text'), true);
     if (! $text == '') {
         $a->page['htmlhead'] .= '<link rel="stylesheet" type="text/css" href="'.$a->get_baseurl().'/addon/impressum/impressum.css" media="all" />';
         $b .= '<div id="impressum_footer">'.$text.'</div>';
@@ -34,8 +36,8 @@ function impressum_show($a,&$b) {
     $b .= '<h3>'.t('Impressum').'</h3>';
     $owner = get_config('impressum', 'owner');
     $owner_profile = get_config('impressum','ownerprofile');
-    $postal = get_config('impressum', 'postal');
-    $notes = get_config('impressum', 'notes');
+    $postal = bbcode(get_config('impressum', 'postal'), true);
+    $notes = bbcode(get_config('impressum', 'notes'), true);
     $email = obfuscate_email( get_config('impressum','email') );
     if (strlen($owner)) {
         if (strlen($owner_profile)) {
@@ -66,12 +68,12 @@ function impressum_plugin_admin_post (&$a) {
     $notes = ((x($_POST, 'notes')) ? (trim($_POST['notes'])) : '');
     $email = ((x($_POST, 'email')) ? notags(trim($_POST['email'])) : '');
     $footer_text = ((x($_POST, 'footer_text')) ? (trim($_POST['footer_text'])) : '');
-    set_config('impressum','owner',$owner);
-    set_config('impressum','ownerprofile',$ownerprofile);
-    set_config('impressum','postal',$postal);
-    set_config('impressum','email',$email);
-    set_config('impressum','notes',$notes);
-    set_config('impressum','footer_text',$footer_text);
+    set_config('impressum','owner',strip_tags($owner));
+    set_config('impressum','ownerprofile',strip_tags($ownerprofile));
+    set_config('impressum','postal',strip_tags($postal));
+    set_config('impressum','email',strip_tags($email));
+    set_config('impressum','notes',strip_tags($notes));
+    set_config('impressum','footer_text',strip_tags($footer_text));
     info( t('Settings updated.'). EOL );
 }
 function impressum_plugin_admin (&$a, &$o) {
@@ -80,9 +82,9 @@ function impressum_plugin_admin (&$a, &$o) {
         '$submit' => t('Submit'),
         '$owner' => array('owner', t('Site Owner'), get_config('impressum','owner'), t('The page operators name.')),
         '$ownerprofile' => array('ownerprofile', t('Site Owners Profile'), get_config('impressum','ownerprofile'), t('Profile address of the operator.')),
-        '$postal' => array('postal', t('Postal Address'), get_config('impressum','postal'), t('How to contact the operator via snail mail.')),
-        '$notes' => array('notes', t('Notes'), get_config('impressum','notes'), t('Additional notes that are displayed beneath the contact information.')),
+        '$postal' => array('postal', t('Postal Address'), get_config('impressum','postal'), t('How to contact the operator via snail mail. You can use BBCode here.')),
+        '$notes' => array('notes', t('Notes'), get_config('impressum','notes'), t('Additional notes that are displayed beneath the contact information. You can use BBCode here.')),
         '$email' => array('email', t('Email Address'), get_config('impressum','email'), t('How to contact the operator via email. (will be displayed obfuscated)')),
-        '$footer_text' => array('footer_text', t('Footer note'), get_config('impressum','footer_text'), t('Text for the footer.')),
+        '$footer_text' => array('footer_text', t('Footer note'), get_config('impressum','footer_text'), t('Text for the footer. You can use BBCode here.')),
     ));
 }
