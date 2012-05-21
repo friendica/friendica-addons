94e6820e85ee098e2eb771a1893f14e3d1e6af92
friendica <info@friendica.com>
2012-05-21 16:41:44 -0700

    globaldir submission blocking

--- testdrive/testdrive.php
+++ testdrive/testdrive.php
@@ -15,6 +15,7 @@ function testdrive_install() {
 	register_hook('register_account', 'addon/testdrive/testdrive.php', 'testdrive_register_account');
 	register_hook('cron', 'addon/testdrive/testdrive.php', 'testdrive_cron');
 	register_hook('enotify','addon/testdrive/testdrive.php', 'testdrive_enotify');
+	register_hook('globaldir_update','addon/testdrive/testdrive.php', 'testdrive_globaldir_update');
 
 }
 
@@ -24,7 +25,12 @@ function testdrive_uninstall() {
 	unregister_hook('register_account', 'addon/testdrive/testdrive.php', 'testdrive_register_account');
 	unregister_hook('cron', 'addon/testdrive/testdrive.php', 'testdrive_cron');
 	unregister_hook('enotify','addon/testdrive/testdrive.php', 'testdrive_enotify');
+	unregister_hook('globaldir_update','addon/testdrive/testdrive.php', 'testdrive_globaldir_update');
 
+}
+
+function testdrive_globaldir_update($a,&$b) {
+	$b['url'] = '';
 }
 
 function testdrive_register_account($a,$b) {
