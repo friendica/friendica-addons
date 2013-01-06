03fc4eb1168643aae372c17db73590a79de32c4f
friendica <info@friendica.com>
2013-01-05 21:50:11 -0800

    add procrunner - 1/2 of poormancron

--- extcron/extcron.php
+++ extcron/extcron.php
@@ -5,7 +5,7 @@
  * Name: external cron
  * Description: Use external server or service to run poller regularly
  * Version: 1.0
- * Author: Mike Macgirvin <http://macgirvin.com/profile/mike>
+ * Author: Mike Macgirvin <https://macgirvin.com/profile/mike>
  * 
  * Notes: External service needs to make a web request to http(s)://yoursite/extcron
  */
