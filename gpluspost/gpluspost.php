0c5ff14d87d13561619e241fe5e49a513b7a1046
Michael Vogel <icarus@dabo.de>
2013-03-02 22:24:57 +0100

    gpluspost: Posting to G+ is now working

--- gpluspost/gpluspost.php
+++ gpluspost/gpluspost.php
@@ -2,15 +2,140 @@
 
 /**
  * Name: G+ Post
+ * Description: Posts to a Google+ page with the help of Seesmic
  * Version: 0.1
  * Author: Michael Vogel <https://pirati.ca/profile/heluecht>
  */
 
 function gpluspost_install() {
+	register_hook('post_local',           'addon/gpluspost/gpluspost.php', 'gpluspost_post_local');
+	register_hook('notifier_normal',      'addon/gpluspost/gpluspost.php', 'gpluspost_send');
+	register_hook('jot_networks',         'addon/gpluspost/gpluspost.php', 'gpluspost_jot_nets');
+	register_hook('connector_settings',      'addon/gpluspost/gpluspost.php', 'gpluspost_settings');
+	register_hook('connector_settings_post', 'addon/gpluspost/gpluspost.php', 'gpluspost_settings_post');
 }
 
 
 function gpluspost_uninstall() {
+	unregister_hook('post_local',       'addon/gpluspost/gpluspost.php', 'gpluspost_post_local');
+	unregister_hook('notifier_normal',  'addon/gpluspost/gpluspost.php', 'gpluspost_send');
+	unregister_hook('jot_networks',     'addon/gpluspost/gpluspost.php', 'gpluspost_jot_nets');
+	unregister_hook('connector_settings',      'addon/gpluspost/gpluspost.php', 'gpluspost_settings');
+	unregister_hook('connector_settings_post', 'addon/gpluspost/gpluspost.php', 'gpluspost_settings_post');
+}
+
+function gpluspost_jot_nets(&$a,&$b) {
+	if(! local_user())
+		return;
+
+	$post = get_pconfig(local_user(),'gpluspost','post');
+	if(intval($post) == 1) {
+		$defpost = get_pconfig(local_user(),'gpluspost','post_by_default');
+		$selected = ((intval($defpost) == 1) ? ' checked="checked" ' : '');
+		$b .= '<div class="profile-jot-net"><input type="checkbox" name="gpluspost_enable"' . $selected . ' value="1" /> '
+			. t('Post to Google+') . '</div>';
+    }
+}
+
+function gpluspost_settings(&$a,&$s) {
+
+	if(! local_user())
+		return;
+
+	$enabled = get_pconfig(local_user(),'gpluspost','post');
+
+	$checked = (($enabled) ? ' checked="checked" ' : '');
+
+	$def_enabled = get_pconfig(local_user(),'gpluspost','post_by_default');
+
+	$def_checked = (($def_enabled) ? ' checked="checked" ' : '');
+
+	$s .= '<div class="settings-block">';
+	$s .= '<h3>' . t('Google+ Post Settings') . '</h3>';
+	$s .= '<div id="gpluspost-enable-wrapper">';
+	$s .= '<label id="gpluspost-enable-label" for="gpluspost-checkbox">' . t('Enable Google+ Post Plugin') . '</label>';
+	$s .= '<input id="gpluspost-checkbox" type="checkbox" name="gpluspost" value="1" ' . $checked . '/>';
+	$s .= '</div><div class="clear"></div>';
+
+	$s .= '<div id="gpluspost-bydefault-wrapper">';
+	$s .= '<label id="gpluspost-bydefault-label" for="gpluspost-bydefault">' . t('Post to Google+ by default') . '</label>';
+	$s .= '<input id="gpluspost-bydefault" type="checkbox" name="gpluspost_bydefault" value="1" ' . $def_checked . '/>';
+	$s .= '</div><div class="clear"></div>';
+
+	/* provide a submit button */
+
+	$s .= '<div class="settings-submit-wrapper" ><input type="submit" id="gpluspost-submit" name="gpluspost-submit" class="settings-submit" value="' . t('Submit') . '" /></div>';
+	$s .= 'Register an account at <a href="https://hootsuite.com">Hootsuite</a>, add your G+ page and add the feed-url there.<br />';
+	//$s .= 'Feed-url: '.$a->get_baseurl().'/gpluspost/'.$a->user["uid"].'</div>';
+	$s .= 'Feed-url: '.$a->get_baseurl().'/gpluspost/'.urlencode($a->user["nickname"]).'</div>';
+}
+
+function gpluspost_settings_post(&$a,&$b) {
+
+	if(x($_POST,'gpluspost-submit')) {
+		set_pconfig(local_user(),'gpluspost','post',intval($_POST['gpluspost']));
+		set_pconfig(local_user(),'gpluspost','post_by_default',intval($_POST['gpluspost_bydefault']));
+	}
+}
+
+function gpluspost_post_local(&$a,&$b) {
+
+	if($b['edit'])
+		return;
+
+	if((! local_user()) || (local_user() != $b['uid']))
+		return;
+
+	if($b['private'] || $b['parent'])
+		return;
+
+	$post   = intval(get_pconfig(local_user(),'gpluspost','post'));
+
+	$enable = (($post && x($_REQUEST,'gpluspost_enable')) ? intval($_REQUEST['gpluspost_enable']) : 0);
+
+	if(intval(get_pconfig(local_user(),'gpluspost','post_by_default')))
+		$enable = 1;
+
+	if(!$enable)
+		return;
+
+	if(strlen($b['postopts']))
+		$b['postopts'] .= ',';
+
+	$b['postopts'] .= 'gplus';
+}
+
+function gpluspost_send(&$a,&$b) {
+
+	logger('gpluspost_send: invoked for post '.$b['id']);
+
+	if($b['deleted'] || $b['private'] || ($b['created'] !== $b['edited']))
+		return;
+
+	if(! strstr($b['postopts'],'gplus'))
+		return;
+
+	if($b['parent'] != $b['id'])
+		return;
+
+	// if post comes from Google+ don't send it back
+	if($b['app'] == "Google+")
+		return;
+
+	$itemlist = get_pconfig($b["uid"],'gpluspost','itemlist');
+	$items = explode(",", $itemlist);
+
+	$i = 0;
+	$newitems = array($b['id']);
+	foreach ($items AS $item)
+		if ($i++ < 9)
+			$newitems[] = $item;
+
+	$itemlist = implode(",", $newitems);
+
+	logger('gpluspost_send: new itemlist: '.$itemlist." for uid ".$b["uid"]);
+
+	set_pconfig($b["uid"],'gpluspost','itemlist', $itemlist);
 }
 
 function gpluspost_module() {}
@@ -18,20 +143,24 @@ function gpluspost_module() {}
 function gpluspost_init() {
 	global $a, $_SERVER;
 
-	$uid = 1;
+	$uid = 0;
 
 	if (isset($a->argv[1])) {
 		$uid = (int)$a->argv[1];
+		if ($uid == 0) {
+			$contacts = q("SELECT `name`, `id` FROM contact WHERE `nick` = '%s' LIMIT 1", dbesc($a->argv[1]));
+			if ($contacts)
+				$uid = $contacts[0]["id"];
+		} else
+			$contacts = q("SELECT `name` FROM contact WHERE ID=%d LIMIT 1", intval($uid));
 	}
-	$pid = 317976;
-
-	$contacts = q("SELECT `name` from contact where ID=%d LIMIT 1", intval($uid));
 
 	header("content-type: application/atom+xml");
 	echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
 	echo '<feed xmlns="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">'."\n";
 	echo "\t".'<title type="html"><![CDATA['.$a->config['sitename'].']]></title>'."\n";
-	echo "\t".'<subtitle type="html"><![CDATA['.$contacts[0]["name"]."]]></subtitle>\n";
+	if ($uid != 0)
+		echo "\t".'<subtitle type="html"><![CDATA['.$contacts[0]["name"]."]]></subtitle>\n";
 	echo "\t".'<link rel="self" href="'.$a->get_baseurl().'/gpluspost"/>'."\n";
 	echo "\t<id>".$a->get_baseurl()."/</id>\n";
 	echo "\t".'<link rel="alternate" type="text/html" href="'.$a->get_baseurl().'"/>'."\n";
@@ -39,20 +168,49 @@ function gpluspost_init() {
 	// <rights>Copyright ... </rights>
 	echo "\t".'<generator uri="'.$a->get_baseurl().'">'.$a->config['sitename'].'</generator>'."\n";
 
-	$pidlist = "262568,262567,269154,271508,270121,273721,314735,312616,311570,308771,308247,306100,295372,291096,290390,290389,283242,283060,280465,273725";
-	$pids = explode(",", $pidlist);
+	if ($uid != 0) {
+		$itemlist = get_pconfig(local_user(),'gpluspost','itemlist');
+		//$itemlist = "262568,262567,269154,271508,270121,273721,314735,312616,311570,308771,308247,306100,295372,291096,290390,290389,283242,283060,280465,273725";
+		$items = explode(",", $itemlist);
 
-	$items = q("SELECT `id` FROM `item` FORCE INDEX (`received`) WHERE `item`.`visible` = 1 AND `item`.`deleted` = 0 and `item`.`moderated` = 0 AND `item`.`allow_cid` = ''  AND `item`.`allow_gid` = '' AND `item`.`deny_cid`  = '' AND `item`.`deny_gid`  = '' AND `item`.`private` = 0 AND `item`.`wall` = 1 AND `item`.`id` = `item`.`parent` ORDER BY `received` DESC LIMIT 50");
-	//foreach ($items AS $item)
-	//	gpluspost_feeditem($item["id"]);
-	foreach ($pids AS $pid)
-		gpluspost_feeditem($pid);
-
+		foreach ($items AS $item)
+			gpluspost_feeditem($item, $uid);
+	} else {
+		$items = q("SELECT `id` FROM `item` FORCE INDEX (`received`) WHERE `item`.`visible` = 1 AND `item`.`deleted` = 0 and `item`.`moderated` = 0 AND `item`.`allow_cid` = ''  AND `item`.`allow_gid` = '' AND `item`.`deny_cid`  = '' AND `item`.`deny_gid`  = '' AND `item`.`private` = 0 AND `item`.`wall` = 1 AND `item`.`id` = `item`.`parent` ORDER BY `received` DESC LIMIT 10");
+		foreach ($items AS $item)
+			gpluspost_feeditem($item["id"], $uid);
+	}
 	echo "</feed>\n";
 	killme();
 }
 
-function gpluspost_feeditem($pid) {
+function gpluspost_ShareAttributes($match) {
+
+        $attributes = $match[1];
+
+        $author = "";
+        preg_match("/author='(.*?)'/ism", $attributes, $matches);
+        if ($matches[1] != "")
+                $author = $matches[1];
+
+        preg_match('/author="(.*?)"/ism', $attributes, $matches);
+        if ($matches[1] != "")
+                $author = $matches[1];
+
+        $headline = '<div class="shared_header">';
+
+        $headline .= sprintf(t('%s:'), $author);
+
+        $headline .= "</div>";
+
+        //$text = "<br />".$headline."</strong><blockquote>".$match[2]."</blockquote>";
+	//$text = "\n\t".$match[2].":\t";
+	$text = $author.": ".$match[2];
+
+        return($text);
+}
+
+function gpluspost_feeditem($pid, $uid) {
 	global $a;
 
 	require_once('include/bbcode.php');
@@ -62,8 +220,6 @@ function gpluspost_feeditem($pid) {
 
 	$items = q("SELECT `uri`, `plink`, `author-link`, `author-name`, `created`, `edited`, `id`, `title`, `body` from `item` WHERE id=%d", intval($pid));
 	foreach ($items AS $item) {
-		// To-Do:
-		// extract the link from the body if there is exactly one link
 
 		// Looking for the first image
 		$image = '';
@@ -87,7 +243,10 @@ function gpluspost_feeditem($pid) {
 
 		$multiplelinks = (strpos($item['body'], "[bookmark") != strrpos($item['body'], "[bookmark"));
 
-		$html = bbcode($item["body"], false, false);
+		$body = $item['body'];
+		$body = preg_replace_callback("/\[share(.*?)\]\s?(.*?)\s?\[\/share\]/ism","gpluspost_ShareAttributes", $body);
+
+		$html = bbcode($body, false, false);
 		$msg = trim(html2plain($html, 0, true));
 
 		// If there is no bookmark element then take the first link
@@ -110,11 +269,11 @@ function gpluspost_feeditem($pid) {
 		else if ($image != "")
 			$msglink = $image;
 
-		//if (($msglink == "")  and strlen($msg) > $max_char)
 		if ($msglink == "")
 			$msglink = $item["plink"];
 
-		$html = trim(str_replace($msglink, "", $html));
+		if ($image != $msglink)
+			$html = trim(str_replace($msglink, "", $html));
 
 		// Fetching the title - or the first line
 		if ($item["title"] != "")
@@ -123,6 +282,10 @@ function gpluspost_feeditem($pid) {
 			$lines = explode("\n", $msg);
 			$title = $lines[0];
 		}
+
+		if ($uid == 0)
+			$title = $item["author-name"].": ".$title;
+
 		$title = str_replace("&", "&amp;", $title);
 		//$html = str_replace("&", "&amp;", $html);
 
@@ -131,10 +294,8 @@ function gpluspost_feeditem($pid) {
 		echo "\t\t".'<link rel="alternate" type="text/html" href="'.$msglink.'" />'."\n";
 		// <link rel="enclosure" type="audio/mpeg" length="1337" href="http://example.org/audio/ph34r_my_podcast.mp3"/>
 		echo "\t\t<id>".$item["uri"]."</id>\n";
-		//echo "\t\t<updated>".date("c", strtotime($item["edited"]))."</updated>\n";
-		//echo "\t\t<published>".date("c", strtotime($item["created"]))."</published>\n";
-		echo "\t\t<updated>".date("c")."</updated>\n";
-		echo "\t\t<published>".date("c")."</published>\n";
+		echo "\t\t<updated>".date("c", strtotime($item["edited"]))."</updated>\n";
+		echo "\t\t<published>".date("c", strtotime($item["created"]))."</published>\n";
 		echo "\t\t<author>\n\t\t\t<name><![CDATA[".$item["author-name"]."]]></name>\n";
 		echo "\t\t\t<uri>".$item["author-link"]."</uri>\n\t\t</author>\n";
 		//echo '<content type="image/png" src="http://media.example.org/the_beach.png"/>';
