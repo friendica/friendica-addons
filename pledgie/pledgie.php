4f4df19bd2d37c6b5a8d35b87b6626890eb07c27
Thomas Willingham <founder@kakste.com>
2013-03-02 16:14:03 +0000

    Pledgie - adjust postion to prevent overlap when themes add random buttons to page end.

--- pledgie/pledgie.php
+++ pledgie/pledgie.php
@@ -12,5 +12,5 @@ function pledgie_install() { register_hook('page_end', 'addon/pledgie/pledgie.ph
 
 function pledgie_uninstall() { unregister_hook('page_end', 'addon/pledgie/pledgie.php', 'pledgie_active'); }
 
-function pledgie_active(&$a,&$b) { $b .= '<div style="position: fixed; bottom: 25px; left: 5px;"><a href=\'http://www.pledgie.com/campaigns/18417\'><img alt=\'Click here to lend your support to: Beyond Social Networking and make a donation at www.pledgie.com !\' src=\'http://www.pledgie.com/campaigns/18417.png?skin_name=chrome\' border=\'0\' target=\'_blank\' /></a></div>'; } 
+function pledgie_active(&$a,&$b) { $b .= '<div style="position: fixed; bottom: 5px; left: 60px;"><a href=\'http://www.pledgie.com/campaigns/18417\'><img alt=\'Click here to lend your support to: Beyond Social Networking and make a donation at www.pledgie.com !\' src=\'http://www.pledgie.com/campaigns/18417.png?skin_name=chrome\' border=\'0\' target=\'_blank\' /></a></div>'; } 
 
