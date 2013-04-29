4a400894aac9e71d33bec4562de6feb8265b40a4
friendica <info@friendica.com>
2013-04-28 21:22:45 -0700

    oops - forgot the files

---
+++ superblock/lang/it/strings.php
@@ -0,0 +1,10 @@
+<?php
+
+$a->strings["\"Blockem\" Settings"] = "Impostazioni \"Blockem\"";
+$a->strings["Comma separated profile URLS to block"] = "Lista, separata da virgola, di indirizzi da bloccare";
+$a->strings["Submit"] = "Invia";
+$a->strings["BLOCKEM Settings saved."] = "Impostazioni salvate.";
+$a->strings["Blocked %s - Click to open/close"] = "%s bloccato - Clicca per aprire/chiudere";
+$a->strings["Unblock Author"] = "Sblocca autore";
+$a->strings["Block Author"] = "Blocca autore";
+$a->strings["blockem settings updated"] = "Impostazioni 'blockem' aggiornate.";
