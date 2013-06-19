fc5670530dba7b720fbf497203eb28de68ccf6eb
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2013-06-19 14:22:09 +0200

    cal: typo in DE translation

--- cal/lang/de/strings.php
+++ cal/lang/de/strings.php
@@ -6,6 +6,6 @@ $a->strings["You can download public events from: "] = "Öffentliche Veranstaltu
 $a->strings["The user does not export the calendar."] = "Dieser Nutzer exportiert keine Veranstaltungen.";
 $a->strings["This calendar format is not supported"] = "Dieses Kalenderformat wird nicht unterstützt ";
 $a->strings["Export Events"] = "Export von Veranstaltungen";
-$a->strings["If this is enabled, your public events will be available at"] = "Ist dieses Addpn aktiviert werden deine öffentlichen Veranstaltungen exportiert. Die Adresse zum Download lautet";
+$a->strings["If this is enabled, your public events will be available at"] = "Ist dieses Addon aktiviert werden deine öffentlichen Veranstaltungen exportiert. Die Adresse zum Download lautet";
 $a->strings["Currently supported formats are ical and csv."] = "Derzeit werden die Formate ical und csv unterstützt.";
 $a->strings["Enable calendar export"] = "Aktiviere den Kalenderexport";
