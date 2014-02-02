c27ee8a3ace0f115e0045a91373ca64a37d07f6f
Michael Vogel <icarus@dabo.de>
2014-02-02 10:05:23 +0100

    statistics: For performance reasons the calculations are done via cron. Additionally changed calculation for posts

--- statistics_json/statistics_json.php
+++ statistics_json/statistics_json.php
@@ -8,10 +8,12 @@
  */
 
 function statistics_json_install() {
+	register_hook('cron', 'addon/statistics_json/statistics_json.php', 'statistics_json_cron');
 }
 
 
 function statistics_json_uninstall() {
+	unregister_hook('cron', 'addon/statistics_json/statistics_json.php', 'statistics_json_cron');
 }
 
 function statistics_json_module() {}
@@ -23,7 +25,31 @@ function statistics_json_init() {
 			"name" => $a->config["sitename"],
 			"version" => FRIENDICA_VERSION,
 			"registrations_open" => ($a->config['register_policy'] != 0),
+			"total_users" => get_config('statistics_json','total_users'),
+			"active_users_halfyear" => get_config('statistics_json','active_users_halfyear'),
+			"active_users_monthly" => get_config('statistics_json','active_users_monthly'),
+			"local_posts" => get_config('statistics_json','local_posts')
 			);
+
+	header("Content-Type: application/json");
+	echo json_encode($statistics);
+	logger("statistics_init: printed ".print_r($statistics, true));
+	killme();
+}
+
+function statistics_json_cron($a,$b) {
+	$last = get_config('statistics_json','last_calucation');
+
+	if($last) {
+		// Calculate all 3 hours
+		$next = $last + (180 * 60);
+		if($next > time()) {
+			logger('statistics_json_cron: calculation intervall not reached');
+			return;
+		}
+	}
+        logger('statistics_json_cron: cron_start');
+
 
 	$users = q("SELECT profile.*, `user`.`login_date`, `lastitem`.`lastitem_date`
 			FROM (SELECT MAX(`item`.`changed`) as `lastitem_date`, `item`.`uid`
@@ -39,14 +65,10 @@ function statistics_json_init() {
 					AND NOT `user`.`account_removed`
 					AND NOT `user`.`account_expired`");
 
-	if (!is_array($users)) {
-			$statistics["total_users"] = -1;
-			$statistics["active_users_halfyear"] = -1;
-			$statistics["active_users_monthly"] = -1;
-	} else {
-			$statistics["total_users"] = count($users);
-			$statistics["active_users_halfyear"] = 0;
-			$statistics["active_users_monthly"] = 0;
+	if (is_array($users)) {
+			$total_users = count($users);
+			$active_users_halfyear = 0;
+			$active_users_monthly = 0;
 
 			$halfyear = time() - (180 * 24 * 60 * 60);
 			$month = time() - (30 * 24 * 60 * 60);
@@ -54,23 +76,31 @@ function statistics_json_init() {
 			foreach ($users AS $user) {
 				if ((strtotime($user['login_date']) > $halfyear) OR
 					(strtotime($user['lastitem_date']) > $halfyear))
-					++$statistics["active_users_halfyear"];
+					++$active_users_halfyear;
 
 				if ((strtotime($user['login_date']) > $month) OR
 					(strtotime($user['lastitem_date']) > $month))
-					++$statistics["active_users_monthly"];
+					++$active_users_monthly;
 
 			}
+			set_config('statistics_json','total_users', $total_users);
+		        logger('statistics_json_cron: total_users: '.$total_users, LOGGER_DEBUG);
+
+			set_config('statistics_json','active_users_halfyear', $active_users_halfyear);
+			set_config('statistics_json','active_users_monthly', $active_users_monthly);
 	}
 
-	$posts = q("SELECT COUNT(*) AS local_posts FROM `item` WHERE `wall`");
-	if (!is_array($posts))
-		$statistics["local_posts"] = -1;
-	else
-		$statistics["local_posts"] = $posts[0]["local_posts"];
+	$posts = q("SELECT COUNT(*) AS local_posts FROM `item` WHERE `wall` AND id=parent");
 
-	header("Content-Type: application/json");
-	echo json_encode($statistics);
-	logger("statistics_init: printed ".print_r($statistics, true));
-	killme();
+	if (!is_array($posts))
+		$local_posts = -1;
+	else
+		$local_posts = $posts[0]["local_posts"];
+
+	set_config('statistics_json','local_posts', $local_posts);
+
+        logger('statistics_json_cron: local_posts: '.$local_posts, LOGGER_DEBUG);
+
+        logger('statistics_json_cron: cron_end');
+	set_config('statistics_json','last_calucation', time());
 }
