f53154a4318f52dd330e291d8853884b8183819b
hauke <hauke@grlg.org>
2015-04-29 20:31:16 +0200

    Pageheader addon now accepts bbcode

--- pageheader/pageheader.php
+++ pageheader/pageheader.php
@@ -6,8 +6,12 @@
  * Description: Inserts a page header
  * Version: 1.0
  * Author: Keith Fernie <http://friendika.me4.it/profile/keith>
+ *         Hauke Altmann <https://snarl.de/profile/tugelblend>
  * 
  */
+
+require_once('include/bbcode.php');
+require_once('mod/proxy.php');
 
 function pageheader_install() {
     register_hook('page_content_top', 'addon/pageheader/pageheader.php', 'pageheader_fetch');
@@ -49,7 +53,8 @@ function pageheader_addon_settings(&$a,&$s) {
     $s .= '<div class="settings-block">';
     $s .= '<h3>' . t('"pageheader" Settings') . '</h3>';
     $s .= '<div id="pageheader-wrapper">';
-    $s .= '<input id="pageheader-words" type="text" size="80" name="pageheader-words" value="' . $words .'" />';
+    $s .= '<label id="pageheader-label" for="pageheader-words">' . t('Message to display on every page on this server') . ' </label>';
+    $s .= '<textarea id="pageheader-words" type="text" name="pageheader-words">' . $words . '</textarea>';
     $s .= '</div><div class="clear"></div>';
 
     $s .= '<div class="settings-submit-wrapper" ><input type="submit" id="pageheader-submit" name="pageheader-submit" class="settings-submit" value="' . t('Save Settings') . '" /></div></div>';
@@ -73,9 +78,9 @@ function pageheader_fetch($a,&$b) {
 
     $a->page['htmlhead'] .= '<link rel="stylesheet" type="text/css" href="'
         . $a->get_baseurl() . '/addon/pageheader/pageheader.css' . '" media="all" />' . "\r\n";
-    $s = get_config('pageheader','text');
+    $s = proxy_parse_html(bbcode(get_config('pageheader', 'text'), true));
     if(! $s)
         $s = '';
     if ($s != '')
-    	$b .= '<div class="pageheader">' . $s . '</div>';
+        $b .= '<div class="pageheader">' . $s . '</div>';
 }
