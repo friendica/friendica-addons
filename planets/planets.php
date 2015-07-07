e8a01cada181bde91d202460049c8c3678c044c3
Dylan Thiedeke <dylan@thiedeke.com.au>
2015-07-08 08:35:44 +1000

    Fixed spelling for Kashyyk and Dagobah

--- planets/planets.php
+++ planets/planets.php
@@ -92,7 +92,7 @@ function planets_post_hook($a, &$item) {
 	 *
 	 */
 
-	$planets = array('Alderaan','Tatooine','Dagoba','Polis Massa','Coruscant','Hoth','Endor','Kamino','Rattatak','Mustafar','Iego','Geonosis','Felucia','Dantooine','Ansion','Artaru','Bespin','Boz Pity','Cato Neimoidia','Christophsis','Kashyyk','Kessel','Malastare','Mygeeto','Nar Shaddaa','Ord Mantell','Saleucami','Subterrel','Death Star','Teth','Tund','Utapau','Yavin');
+	$planets = array('Alderaan','Tatooine','Dagobah','Polis Massa','Coruscant','Hoth','Endor','Kamino','Rattatak','Mustafar','Iego','Geonosis','Felucia','Dantooine','Ansion','Artaru','Bespin','Boz Pity','Cato Neimoidia','Christophsis','Kashyyyk','Kessel','Malastare','Mygeeto','Nar Shaddaa','Ord Mantell','Saleucami','Subterrel','Death Star','Teth','Tund','Utapau','Yavin');
 
 	$planet = array_rand($planets,1);
 	$item['location'] = $planets[$planet];
