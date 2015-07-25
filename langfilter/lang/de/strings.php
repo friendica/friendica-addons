a3daf90a269c9d1efa068bf386037f9469fdd095
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2015-07-25 10:30:23 +0200

    DE: added translatiosn for the langfilter addon

---
+++ langfilter/lang/de/strings.php
@@ -0,0 +1,15 @@
+<?php
+
+if(! function_exists("string_plural_select_de")) {
+function string_plural_select_de($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["Language Filter"] = "Sprachfilter";
+$a->strings["This addon tries to identify the language of a postings. If it does not match any language spoken by you (see below) the posting will be collapsed. Remember detecting the language is not perfect, especially with short postings."] = "Dieses Addon versucht die Sprache eines Beitrags zu ermitteln und verbirgt den Inhalt, wenn du die Sprache nicht sprichst. Die Spracherkennung ist allerdings nicht perfekt, insbesondere bei kurzen Beiträgen.";
+$a->strings["Use the language filter"] = "Den Sprachfilter verwenden";
+$a->strings["I speak"] = "Ich spreche";
+$a->strings["List of abbreviations for languages you speak, comma seperated. For excample \"de,it\"."] = "Liste von Abkürzungen der Sprachen die du sprichst, getrennt durch Komma. Zum Beispiel: \"de,it\".";
+$a->strings["Save Settings"] = "Einstellungen speichern";
+$a->strings["Language Filter Settings saved."] = "Sprachfilter Einstellungen gespeichert.";
+$a->strings["unspoken language %s - Click to open/close"] = "nicht gesprochene Sprache %s - Zum öffnen/schließen klicken";
