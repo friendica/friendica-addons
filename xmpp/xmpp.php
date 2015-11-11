27a6e5829de8eacc6292d9b5549688ad15797e66
Michael Vogel <icarus@dabo.de>
2015-11-11 22:27:11 +0100

    XMPP: User configurations

--- xmpp/xmpp.php
+++ xmpp/xmpp.php
@@ -7,13 +7,80 @@
  */
 
 function xmpp_install() {
+	register_hook('plugin_settings', 'addon/xmpp/xmpp.php', 'xmpp_plugin_settings');
+	register_hook('plugin_settings_post', 'addon/xmpp/xmpp.php', 'xmpp_plugin_settings_post');
 	register_hook('page_end', 'addon/xmpp/xmpp.php', 'xmpp_script');
 	register_hook('logged_in', 'addon/xmpp/xmpp.php', 'xmpp_login');
 }
 
 function xmpp_uninstall() {
+	unregister_hook('plugin_settings', 'addon/xmpp/xmpp.php', 'xmpp_plugin_settings');
+	unregister_hook('plugin_settings_post', 'addon/xmpp/xmpp.php', 'xmpp_plugin_settings_post');
 	unregister_hook('page_end', 'addon/xmpp/xmpp.php', 'xmpp_script');
 	unregister_hook('logged_in', 'addon/xmpp/xmpp.php', 'xmpp_login');
+}
+
+function xmpp_plugin_settings_post($a,$post) {
+	if(! local_user() || (! x($_POST,'xmpp-settings-submit')))
+		return;
+	set_pconfig(local_user(),'xmpp','enabled',intval($_POST['xmpp_enabled']));
+	set_pconfig(local_user(),'xmpp','individual',intval($_POST['xmpp_individual']));
+	set_pconfig(local_user(),'xmpp','bosh_proxy',$_POST['xmpp_bosh_proxy']);
+
+	info( t('XMPP settings updated.') . EOL);
+}
+
+function xmpp_plugin_settings(&$a,&$s) {
+
+	if(! local_user())
+		return;
+
+	/* Add our stylesheet to the xmpp so we can make our settings look nice */
+
+	$a->page['htmlhead'] .= '<link rel="stylesheet"  type="text/css" href="' . $a->get_baseurl() . '/addon/xmpp/xmpp.css' . '" media="all" />' . "\r\n";
+
+	/* Get the current state of our config variable */
+
+	$enabled = intval(get_pconfig(local_user(),'xmpp','enabled'));
+	$enabled_checked = (($enabled) ? ' checked="checked" ' : '');
+
+	$individual = intval(get_pconfig(local_user(),'xmpp','individual'));
+	$individual_checked = (($individual) ? ' checked="checked" ' : '');
+
+	$bosh_proxy = get_pconfig(local_user(),"xmpp","bosh_proxy");
+
+	/* Add some HTML to the existing form */
+	$s .= '<span id="settings_xmpp_inflated" class="settings-block fakelink" style="display: block;" onclick="openClose(\'settings_xmpp_expanded\'); openClose(\'settings_xmpp_inflated\');">';
+	$s .= '<h3>' . t('XMPP-Chat (Jabber)') . '</h3>';
+	$s .= '</span>';
+	$s .= '<div id="settings_xmpp_expanded" class="settings-block" style="display: none;">';
+	$s .= '<span class="fakelink" onclick="openClose(\'settings_xmpp_expanded\'); openClose(\'settings_xmpp_inflated\');">';
+	$s .= '<h3>' . t('XMPP-Chat (Jabber)') . '</h3>';
+	$s .= '</span>';
+
+	$s .= '<div id="xmpp-settings-wrapper">';
+	$s .= '<label id="xmpp-enabled-label" for="xmpp-enabled">' . t('Enable Webchat') . '</label>';
+	$s .= '<input id="xmpp-enabled" type="checkbox" name="xmpp_enabled" value="1" ' . $enabled_checked . '/>';
+	$s .= '<div class="clear"></div>';
+
+	if (get_config("xmpp", "central_userbase")) {
+		$s .= '<label id="xmpp-individual-label" for="xmpp-individual">' . t('Individual Credentials') . '</label>';
+		$s .= '<input id="xmpp-individual" type="checkbox" name="xmpp_individual" value="1" ' . $individual_checked . '/>';
+		$s .= '<div class="clear"></div>';
+	}
+
+	if (!get_config("xmpp", "central_userbase") OR get_pconfig(local_user(),"xmpp","individual")) {
+		$s .= '<label id="xmpp-bosh-proxy-label" for="xmpp-bosh-proxy">'.t('Jabber BOSH host').'</label>';
+		$s .= ' <input id="xmpp-bosh-proxy" type="text" name="xmpp_bosh_proxy" value="'.$bosh_proxy.'" />';
+		$s .= '<div class="clear"></div>';
+	}
+
+	$s .= '</div>';
+
+	/* provide a submit button */
+
+	$s .= '<div class="settings-submit-wrapper" ><input type="submit" name="xmpp-settings-submit" class="settings-submit" value="' . t('Save Settings') . '" /></div></div>';
+
 }
 
 function xmpp_login($a,$b) {
@@ -24,26 +91,25 @@ function xmpp_login($a,$b) {
 }
 
 function xmpp_plugin_admin(&$a, &$o){
-        $t = get_markup_template("admin.tpl", "addon/xmpp/");
+	$t = get_markup_template("admin.tpl", "addon/xmpp/");
 
-        $o = replace_macros($t, array(
-                '$submit' => t('Save Settings'),
-                '$bosh_proxy'       => array('bosh_proxy', t('BOSH proxy'),            get_config('xmpp', 'bosh_proxy'), ''),
-                '$central_userbase' => array('central_userbase', t('Use central userbase'), get_config('xmpp', 'central_userbase'), t('If enabled, users will automatically login to an ejabberd server that has to be installed on this machine with synchronized credentials via the "auth_ejabberd.php" script.')),
-        ));
+	$o = replace_macros($t, array(
+		'$submit' => t('Save Settings'),
+		'$bosh_proxy'       => array('bosh_proxy', t('Jabber BOSH host'),            get_config('xmpp', 'bosh_proxy'), ''),
+		'$central_userbase' => array('central_userbase', t('Use central userbase'), get_config('xmpp', 'central_userbase'), t('If enabled, users will automatically login to an ejabberd server that has to be installed on this machine with synchronized credentials via the "auth_ejabberd.php" script.')),
+	));
 }
 
 function xmpp_plugin_admin_post(&$a){
-        $bosh_proxy       = ((x($_POST,'bosh_proxy')) ?       trim($_POST['bosh_proxy']) : '');
-        $central_userbase = ((x($_POST,'central_userbase')) ? intval($_POST['central_userbase']) : false);
-        set_config('xmpp','bosh_proxy',$bosh_proxy);
-        set_config('xmpp','central_userbase',$central_userbase);
-        info( t('Settings updated.'). EOL );
+	$bosh_proxy       = ((x($_POST,'bosh_proxy')) ?       trim($_POST['bosh_proxy']) : '');
+	$central_userbase = ((x($_POST,'central_userbase')) ? intval($_POST['central_userbase']) : false);
+	set_config('xmpp','bosh_proxy',$bosh_proxy);
+	set_config('xmpp','central_userbase',$central_userbase);
+	info( t('Settings updated.'). EOL );
 }
 
 function xmpp_script(&$a,&$s) {
 	xmpp_converse($a,$s);
-	//xmpp_jappix($a,$s);
 }
 
 function xmpp_converse(&$a,&$s) {
@@ -56,12 +122,15 @@ function xmpp_converse(&$a,&$s) {
 	if ($a->is_mobile || $a->is_tablet)
 		return;
 
+	if (!get_pconfig(local_user(),"xmpp","enabled"))
+		return;
+
 	$a->page['htmlhead'] .= '<link type="text/css" rel="stylesheet" media="screen" href="addon/xmpp/converse/css/converse.css" />'."\n";
 	$a->page['htmlhead'] .= '<script src="addon/xmpp/converse/builds/converse.min.js"></script>'."\n";
 
-	$bosh_proxy = get_config("xmpp", "bosh_proxy");
+	if (get_config("xmpp", "central_userbase") AND !get_pconfig(local_user(),"xmpp","individual")) {
+		$bosh_proxy = get_config("xmpp", "bosh_proxy");
 
-	if (get_config("xmpp", "central_userbase")) {
 		$password = get_pconfig(local_user(), "xmpp", "password");
 
 		if ($password == "") {
@@ -75,10 +144,15 @@ function xmpp_converse(&$a,&$s) {
 			authentication: 'login',
 			jid: '$jid',
 			password: '$password',
-			allow_logout: true,
-			auto_list_rooms: true,";
-	} else
+			allow_logout: false,";
+	} else {
+		$bosh_proxy = get_pconfig(local_user(), "xmpp", "bosh_proxy");
+
 		$auto_login = "";
+	}
+
+	if ($bosh_proxy == "")
+		return;
 
 	if (in_array($a->argv[0], array("manage", "logout")))
 		$additional_commands = "converse.user.logout();\n";
@@ -86,9 +160,6 @@ function xmpp_converse(&$a,&$s) {
 		$additional_commands = "";
 
 	$on_ready = "";
-	//$on_ready = "converse.rooms.open(['support@conference.pirati.ca']);\n";
-
-//  converse.contacts.add('ike@jabber.piratenpartei.de');
 
 	$initialize = "converse.initialize({
 					bosh_service_url: '$bosh_proxy',
@@ -123,60 +194,4 @@ function xmpp_converse(&$a,&$s) {
 					});
 				</script>";
 }
-
-function xmpp_jappix(&$a,&$s) {
-	if (!local_user())
-		return;
-
-	if ($_GET["mode"] == "minimal")
-		return;
-
-	$bosh_proxy = get_config("xmpp", "bosh_proxy");
-
-	if (get_config("xmpp", "central_userbase")) {
-		$password = get_pconfig(local_user(), "xmpp", "password");
-
-		if ($password == "") {
-			$password = substr(random_string(),0,16);
-			set_pconfig(local_user(), "xmpp", "password", $password);
-		}
-
-		$user = $a->user["nickname"];
-		$domain = $a->get_hostname();
-
-		$auto_login = "auto_login: true,
-			authentication: 'login',
-			jid: '$jid',
-			password: '$password',
-			allow_logout: false,";
-	} else
-		$auto_login = "";
-
-	//$a->page['htmlhead'] .= "<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>";
-
-	$a->page['htmlhead'] .= "<script type='text/javascript' src='addon/xmpp/jappix/javascripts/mini.js'></script>
-					<script type='text/javascript'>
-						jQuery(document).ready(function() {
-							JAPPIX_STATIC = 'addon/xmpp/jappix/';
-							HOST_BOSH = '$bosh_proxy'
-
-							JappixMini.launch({
-						           connection: {
-						             user: '$user',
-						             password: '$password',
-						             domain: '$domain'
-						           },
-						           application: {
-						             network: {
-						               autoconnect: true
-						             },
-						             interface: {
-						               showpane: false
-						             }
-						           }
-						        });
-						});
-				</script>";
-}
-
 ?>
