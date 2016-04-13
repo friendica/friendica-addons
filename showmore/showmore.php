16315e876f392c2c76e579c22da4c5ce803f6fb5
rabuzarus <>
2016-04-14 01:28:04 +0200

    showmore: add css classes to elements

--- showmore/showmore.php
+++ showmore/showmore.php
@@ -121,9 +121,9 @@ function showmore_prepare_body(&$a,&$b) {
 
 	if($found) {
 		$rnd = random_string(8);
-		$b['html'] = '<span id="showmore-teaser-'.$rnd.'" style="display: block;">'.$shortened." ".
-				'<span id="showmore-wrap-'.$rnd.'" style="white-space:nowrap;" class="fakelink" onclick="openClose(\'showmore-'.$rnd.'\'); openClose(\'showmore-teaser-'.$rnd.'\');" >'.sprintf(t('show more')).'</span></span>'.
-				'<div id="showmore-'.$rnd.'" style="display: none;">'.$b['html'].'</div>';
+		$b['html'] = '<span id="showmore-teaser-'.$rnd.'" class="showmore-teaser" style="display: block;">'.$shortened." ".
+				'<span id="showmore-wrap-'.$rnd.'" style="white-space:nowrap;" class="showmore-wrap fakelink" onclick="openClose(\'showmore-'.$rnd.'\'); openClose(\'showmore-teaser-'.$rnd.'\');" >'.sprintf(t('show more')).'</span></span>'.
+				'<div id="showmore-'.$rnd.'" class="showmore-content" style="display: none;">'.$b['html'].'</div>';
 	}
 }
 
