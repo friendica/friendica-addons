a7a5d9b9aa4c9496b3bddf4aa626243b68d93bde
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2016-04-22 17:42:52 +0200

    ES translation thx TupambaeNet

--- blackout/lang/es/strings.php
+++ blackout/lang/es/strings.php
@@ -1,3 +1,14 @@
 <?php
 
-$a->strings["Submit"] = "Envíar";
+if(! function_exists("string_plural_select_es")) {
+function string_plural_select_es($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["Save Settings"] = "Guardar configuración";
+$a->strings["Redirect URL"] = "Redirigir URL";
+$a->strings["all your visitors from the web will be redirected to this URL"] = "todos los visitantes de la web serán redirigidos a esta dirección.";
+$a->strings["Begin of the Blackout"] = "Inicio del apagón.";
+$a->strings["format is <em>YYYY</em> year, <em>MM</em> month, <em>DD</em> day, <em>hh</em> hour and <em>mm</em> minute"] = "formato es <em>YYYY</em> año, <em>MM</em> mes, <em>DD</em> día, <em>hh</em> hora y <em>mm</em> minuto";
+$a->strings["End of the Blackout"] = "Fin del apagón.";
+$a->strings["The end-date is prior to the start-date of the blackout, you should fix this."] = "La fecha de final del apagón es antes de la fecha de inicio, deberías arreglar esto.";
--- nsfw/lang/es/strings.php
+++ nsfw/lang/es/strings.php
@@ -1,10 +1,15 @@
 <?php
 
-$a->strings["Not Safe For Work (General Purpose Content Filter) settings"] = "Configuración \"Not Safe For Work\" (Filtro de contenido de carácter general)";
-$a->strings["This plugin looks in posts for the words/text you specify below, and collapses any content containing those keywords so it is not displayed at inappropriate times, such as sexual innuendo that may be improper in a work setting. It is polite and recommended to tag any content containing nudity with #NSFW.  This filter can also match any other word/text you specify, and can thereby be used as a general purpose content filter."] = "Este complemento busca las palabras clave que especifiques y oculta cualquier comentario que contenga dichas claves, para que no aparezcan en momentos inoportunos, como por ejemplo, material sexual. Se considera de buena educación y correcto etiquetar los desnudos con #NSFW.  Este filtro puede servir para filtrar otro contenido, así que te puede servir como un filtro de carácter general, escogiendo las palabras clave adecuadas.";
-$a->strings["Enable Content filter"] = "Activar el filtro de contenido";
-$a->strings["Comma separated list of keywords to hide"] = "Palabras clave para ocultar, lista separada por comas";
-$a->strings["Submit"] = "Envíar";
-$a->strings["Use /expression/ to provide regular expressions"] = "Usa /expresión/ para proporcionar expresiones regulares";
+if(! function_exists("string_plural_select_es")) {
+function string_plural_select_es($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["Not Safe For Work (General Purpose Content Filter) settings"] = "Configuración \"No apto para trabajar\" (Filtro genérico de contenido)";
+$a->strings["This plugin looks in posts for the words/text you specify below, and collapses any content containing those keywords so it is not displayed at inappropriate times, such as sexual innuendo that may be improper in a work setting. It is polite and recommended to tag any content containing nudity with #NSFW.  This filter can also match any other word/text you specify, and can thereby be used as a general purpose content filter."] = "Este plugin se fija por el contenido del texto y colapsa todo tema o respuesta que contiene las palabras establecidas. Como tales pueden ser contenido sexual o de otra índole que no conviene desplegar en el trabajo o ambientes correspondientes. Es de buena educación y recomendado de identificar todo tipo de contenido explicito con #NSFW. Este filtro además puede ser usado con cualquier palabra a especificar y por lo tanto ser usado como un filtro generico de contenido.";
+$a->strings["Enable Content filter"] = "Habilitar filtro de contenido";
+$a->strings["Comma separated list of keywords to hide"] = "Lista de palabras claves separadas por coma para colapsar el contenido correspondiente.";
+$a->strings["Submit"] = "Enviar";
+$a->strings["Use /expression/ to provide regular expressions"] = "Utiliza /expresión/ para proveer expresiones regulares.";
 $a->strings["NSFW Settings saved."] = "Configuración NSFW guardada.";
-$a->strings["%s - Click to open/close"] = "%s - Pulsa aquí para abrir/cerrar";
+$a->strings["%s - Click to open/close"] = "%s - Click aquí para abrir/cerrar";
