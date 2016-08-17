70beedf0751a09b07fca570a8044dd5534124c3e
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2016-08-17 08:06:47 +0200

    PT BR translations THX Beatriz Vital

---
+++ notifyall/lang/pt-br/strings.php
@@ -0,0 +1,16 @@
+<?php
+
+if(! function_exists("string_plural_select_pt_br")) {
+function string_plural_select_pt_br($n){
+	return ($n > 1);;
+}}
+;
+$a->strings["Send email to all members"] = "Enviar e-mail para todos os membros";
+$a->strings["%s Administrator"] = "Administrador de %s";
+$a->strings["%1\$s, %2\$s Administrator"] = "%1\$s, administrador de %2\$s";
+$a->strings["No recipients found."] = "Não foi encontrado nenhum destinatário.";
+$a->strings["Emails sent"] = "E-mails enviados";
+$a->strings["Send email to all members of this Friendica instance."] = "Enviar e-mail para todos os membros desta instância do Friendica.";
+$a->strings["Message subject"] = "Assunto da mensagem";
+$a->strings["Test mode (only send to administrator)"] = "Modo de teste (enviar só para o administrador)";
+$a->strings["Submit"] = "Enviar";
--- tictac/lang/pt-br/strings.php
+++ tictac/lang/pt-br/strings.php
@@ -1,14 +1,19 @@
 <?php
 
-$a->strings["Three Dimensional Tic-Tac-Toe"] = "Jogo da Velha tridimensional";
+if(! function_exists("string_plural_select_pt_br")) {
+function string_plural_select_pt_br($n){
+	return ($n > 1);;
+}}
+;
+$a->strings["Three Dimensional Tic-Tac-Toe"] = "Jogo da Velha Tridimensional";
 $a->strings["3D Tic-Tac-Toe"] = "Jogo da Velha 3D";
 $a->strings["New game"] = "Novo jogo";
 $a->strings["New game with handicap"] = "Novo jogo com limitador";
-$a->strings["Three dimensional tic-tac-toe is just like the traditional game except that it is played on multiple levels simultaneously. "] = "O Jogo da Velha tridimensional é similar ao jogo tradicional, com a exceção que ele é jogado simultaneamente em múltiplos níveis.";
-$a->strings["In this case there are three levels. You win by getting three in a row on any level, as well as up, down, and diagonally across the different levels."] = "Neste caso, existem três níveis. Ganha quem conseguir alinhar três casas, não só no mesmo nível como também pra cima, pra baixo ou na diagonal entre os níveis.";
-$a->strings["The handicap game disables the center position on the middle level because the player claiming this square often has an unfair advantage."] = "O jogo com limitador desabilita a posição central do nível do meio, uma vez que o jogador que obtém essa casa ganha uma vantagem desleal.";
-$a->strings["You go first..."] = "Você começa...";
-$a->strings["I'm going first this time..."] = "Eu vou começar desta vez...";
-$a->strings["You won!"] = "Você ganhou!";
+$a->strings["Three dimensional tic-tac-toe is just like the traditional game except that it is played on multiple levels simultaneously. "] = "O jogo da velha tridimensional é como o jogo tradicional, exceto por ser jogado em vários níveis simultaneamente.";
+$a->strings["In this case there are three levels. You win by getting three in a row on any level, as well as up, down, and diagonally across the different levels."] = "Neste caso, há três níveis. Vence quem conseguir alinhar três quadrados em qualquer um dos níveis, assim como para cima, para baixo e na diagonal, em níveis diferentes.";
+$a->strings["The handicap game disables the center position on the middle level because the player claiming this square often has an unfair advantage."] = "No jogo com limitador, a posição central do nível do meio é desativada, porque o jogador que marcasse esse quadrado teria uma vantagem injusta.";
+$a->strings["You go first..."] = "Você vai primeiro...";
+$a->strings["I'm going first this time..."] = "Eu vou primeiro desta vez...";
+$a->strings["You won!"] = "Você venceu!";
 $a->strings["\"Cat\" game!"] = "Empatamos!";
-$a->strings["I won!"] = "Eu ganhei!";
+$a->strings["I won!"] = "Venci!";
