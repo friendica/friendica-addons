aa8c44dcc8df95db4d38fcd33103cb71562f3b66
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2016-08-23 07:46:05 +0200

    some more PT BR translations THX Beatriz Vital

--- libertree/lang/pt-br/strings.php
+++ libertree/lang/pt-br/strings.php
@@ -1,9 +1,14 @@
 <?php
 
-$a->strings["Post to libertree"] = "";
-$a->strings["libertree Post Settings"] = "";
-$a->strings["Enable Libertree Post Plugin"] = "";
+if(! function_exists("string_plural_select_pt_br")) {
+function string_plural_select_pt_br($n){
+	return ($n > 1);;
+}}
+;
+$a->strings["Post to libertree"] = "Publicar no Libertree";
+$a->strings["libertree Post Settings"] = "Configurações de publicação do Libertree";
+$a->strings["Enable Libertree Post Plugin"] = "Habilitar plug-in para publicar no Libertree";
 $a->strings["Libertree API token"] = "";
 $a->strings["Libertree site URL"] = "";
-$a->strings["Post to Libertree by default"] = "";
+$a->strings["Post to Libertree by default"] = "Publicar no Libertree por padrão";
 $a->strings["Submit"] = "Enviar";
--- public_server/lang/pt-br/strings.php
+++ public_server/lang/pt-br/strings.php
@@ -1,5 +1,11 @@
 <?php
 
-$a->strings["Administrator"] = "Administrador";
-$a->strings["Your account on %s will expire in a few days."] = "Sua conta em %s expirará em poucos dias.";
-$a->strings["Your Friendica account is about to expire."] = "";
+if(! function_exists("string_plural_select_pt_br")) {
+function string_plural_select_pt_br($n){
+	return ($n > 1);;
+}}
+;
+$a->strings["Administrator"] = "";
+$a->strings["Your account on %s will expire in a few days."] = "Sua conta do %s vai expirar em alguns dias.";
+$a->strings["Your Friendica account is about to expire."] = "Sua conta no Friendica está prestes a expirar.";
+$a->strings["Hi %1\$s,\n\nYour account on %2\$s will expire in less than five days. You may keep your account by logging in at least once every 30 days"] = "Olá, %1\$s,\n\nSua conta no %2\$s vai expirar em menos de cinco dias. Para manter sua conta, lembre-se de entrar pelo menos uma vez a cada 30 dias.";
