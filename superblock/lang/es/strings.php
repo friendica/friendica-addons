87697cc233c30926e4cdd24e60e6526dd63efd44
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2016-11-19 06:17:07 +0100

    ES translation work THX Albert

--- superblock/lang/es/strings.php
+++ superblock/lang/es/strings.php
@@ -1,10 +1,15 @@
 <?php
 
-$a->strings["\"Blockem\" Settings"] = "Configuración de \"Blockem\"";
-$a->strings["Comma separated profile URLS to block"] = "Direcciones separadas por coma de los perfiles a bloquear";
-$a->strings["Submit"] = "Envíar";
-$a->strings["BLOCKEM Settings saved."] = "Configuracion Blockem guardada.";
-$a->strings["Blocked %s - Click to open/close"] = "%s bloqueado. Pulsa aquí para mostrar/ocultar";
+if(! function_exists("string_plural_select_es")) {
+function string_plural_select_es($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["\"Blockem\" Settings"] = "Ajustes de \"Blockem\"";
+$a->strings["Comma separated profile URLS to block"] = "Perfil de URLS a bloque separado por comas";
+$a->strings["Submit"] = "Enviar";
+$a->strings["BLOCKEM Settings saved."] = "Ajustes de BLOCKEM guardados.";
+$a->strings["Blocked %s - Click to open/close"] = "%s bloqueados - Click para abrir/cerrar";
 $a->strings["Unblock Author"] = "Desbloquear Autor";
-$a->strings["Block Author"] = "Bloquear Autor";
-$a->strings["blockem settings updated"] = "Configuración de Blockem actualizada";
+$a->strings["Block Author"] = "Bloquear autor";
+$a->strings["blockem settings updated"] = "Ajustes de Blockem actualizados";
