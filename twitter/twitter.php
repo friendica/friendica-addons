9082f4c437e2fcc2a596968cd28485b500e8810c
Michael <heluecht@pirati.ca>
2016-12-10 07:15:00 +0000

    Twitter: Videos are embedded, mirrored posts are improved

--- twitter/twitter.php
+++ twitter/twitter.php
@@ -734,6 +734,41 @@ function twitter_prepare_body(&$a,&$b) {
 	}
 }
 
+function twitter_do_mirrorpost($a, $uid, $post) {
+	$datarray["type"] = "wall";
+	$datarray["api_source"] = true;
+	$datarray["profile_uid"] = $uid;
+	$datarray["extid"] = NETWORK_TWITTER;
+	$datarray['message_id'] = item_new_uri($a->get_hostname(), $uid, NETWORK_TWITTER.":".$post->id);
+	$datarray["title"] = "";
+
+	if (is_object($post->retweeted_status)) {
+		$item = twitter_createpost($a, $uid, $post, array('id' => 0), false, false, true);
+
+		$datarray['body'] = "\n".share_header($item['author-name'], $item['author-link'], $item['author-avatar'], "",
+					$item['created'], $item['plink']);
+
+		$datarray['body'] .= $item['body'].'[/share]';
+	} else {
+		$item = twitter_createpost($a, $uid, $post, array('id' => 0), false, false, false);
+
+		$datarray['body'] = $item['body'];
+	}
+
+	$datarray["source"] = $item['app'];
+	$datarray["verb"] = $item['verb'];
+
+	if (isset($item["location"])) {
+		$datarray["location"] = $item["location"];
+	}
+
+	if (isset($item["coord"])) {
+		$datarray["coord"] = $item["coord"];
+	}
+
+	return $datarray;
+}
+
 function twitter_fetchtimeline($a, $uid) {
 	$ckey    = get_config('twitter', 'consumerkey');
 	$csecret = get_config('twitter', 'consumersecret');
@@ -755,7 +790,8 @@ function twitter_fetchtimeline($a, $uid) {
 	require_once('library/twitteroauth.php');
 	$connection = new TwitterOAuth($ckey,$csecret,$otoken,$osecret);
 
-	$parameters = array("exclude_replies" => true, "trim_user" => false, "contributor_details" => true, "include_rts" => true);
+	//$parameters = array("exclude_replies" => true, "trim_user" => false, "contributor_details" => true, "include_rts" => true);
+	$parameters = array("exclude_replies" => true, "trim_user" => false, "contributor_details" => true, "include_rts" => true, "tweet_mode" => "extended");
 
 	$first_time = ($lastid == "");
 
@@ -780,9 +816,13 @@ function twitter_fetchtimeline($a, $uid) {
 			continue;
 
 		if (!stristr($post->source, $application_name)) {
+
 			$_SESSION["authenticated"] = true;
 			$_SESSION["uid"] = $uid;
 
+			$_REQUEST = twitter_do_mirrorpost($a, $uid, $post);
+
+/*
 			unset($_REQUEST);
 			$_REQUEST["type"] = "wall";
 			$_REQUEST["api_source"] = true;
@@ -870,7 +910,7 @@ function twitter_fetchtimeline($a, $uid) {
 
 			if (is_array($post->coordinates->coordinates))
 				$_REQUEST["coord"] = $post->coordinates->coordinates[1]." ".$post->coordinates->coordinates[0];
-
+*/
 			//print_r($_REQUEST);
 			logger('twitter: posting for user '.$uid);
 
@@ -1325,9 +1365,71 @@ function twitter_expand_entities($a, $body, $item, $no_tags = false, $picture) {
 	return array("body" => $body, "tags" => $tags, "plain" => $plain);
 }
 
-function twitter_createpost($a, $uid, $post, $self, $create_user, $only_existing_contact) {
+/**
+ * @brief Fetch media entities and add media links to the body
+ *
+ * @param object $post Twitter object with the post
+ * @param array $postarray Array of the item that is about to be posted
+ *
+ * @return $picture string Returns a a single picture string if it isn't a media post
+ */
+function twitter_media_entities($post, &$postarray) {
 
-	$has_picture = false;
+	// There are no media entities? So we quit.
+	if (!is_array($post->extended_entities->media)) {
+		return "";
+	}
+
+	// When the post links to an external page, we only take one picture.
+	// This could be improved in the future.
+	if (count($post->entities->urls) > 0) {
+		$picture = "";
+		foreach($post->extended_entities->media AS $medium) {
+			if (isset($medium->media_url_https)) {
+				$picture = $medium->media_url_https;
+				$postarray['body'] = str_replace($medium->url, "", $postarray['body']);
+			}
+		}
+		return $picture;
+	}
+
+	// This is a pure media post, first search for all media urls
+	$media = array();
+	foreach($post->extended_entities->media AS $medium) {
+		switch($medium->type) {
+			case 'photo':
+				$media[$medium->url] .= "\n[img]".$medium->media_url_https."[/img]";
+				$postarray['object-type'] = ACTIVITY_OBJ_IMAGE;
+				break;
+			case 'video':
+			case 'animated_gif':
+				$media[$medium->url] .= "\n[img]".$medium->media_url_https."[/img]";
+				$postarray['object-type'] = ACTIVITY_OBJ_VIDEO;
+				if (is_array($medium->video_info->variants)) {
+					$bitrate = 0;
+					// We take the video with the highest bitrate
+					foreach ($medium->video_info->variants AS $variant) {
+						if (($variant->content_type == "video/mp4") AND ($variant->bitrate >= $bitrate)) {
+							$media[$medium->url] = "\n[video]".$variant->url."[/video]";
+							$bitrate = $variant->bitrate;
+						}
+					}
+				}
+				break;
+			// The following code will only be activated for test reasons
+			//default:
+			//	$postarray['body'] .= print_r($medium, true);
+		}
+	}
+
+	// Now we replace the media urls.
+	foreach ($media AS $key => $value) {
+		$postarray['body'] = str_replace($key, "\n".$value."\n", $postarray['body']);
+	}
+	return "";
+}
+
+function twitter_createpost($a, $uid, $post, $self, $create_user, $only_existing_contact, $noquote) {
 
 	$postarray = array();
 	$postarray['network'] = NETWORK_TWITTER;
@@ -1436,24 +1538,13 @@ function twitter_createpost($a, $uid, $post, $self, $create_user, $only_existing
 		$postarray['body'] = $post->text;
 	}
 
-	$picture = "";
-
-	// media
-	if (is_array($post->entities->media)) {
-		foreach($post->entities->media AS $media) {
-			switch($media->type) {
-				case 'photo':
-					//$postarray['body'] = str_replace($media->url, "\n\n[img]".$media->media_url_https."[/img]\n", $postarray['body']);
-					//$has_picture = true;
-					$postarray['body'] = str_replace($media->url, "", $postarray['body']);
-					$postarray['object-type'] = ACTIVITY_OBJ_IMAGE;
-					$picture = $media->media_url_https;
-					break;
-				default:
-					$postarray['body'] .= print_r($media, true);
-			}
-		}
+	// When the post contains links then use the correct object type
+	if (count($post->entities->urls) > 0) {
+		$postarray['object-type'] = ACTIVITY_OBJ_BOOKMARK;
 	}
+
+	// Search for media links
+	$picture = twitter_media_entities($post, $postarray);
 
 	$converted = twitter_expand_entities($a, $postarray['body'], $post, false, $picture);
 	$postarray['body'] = $converted["body"];
@@ -1476,8 +1567,9 @@ function twitter_createpost($a, $uid, $post, $self, $create_user, $only_existing
 		$postarray["coord"] = $post->coordinates->coordinates[1]." ".$post->coordinates->coordinates[0];
 	}
 	if (is_object($post->retweeted_status)) {
-		$retweet = twitter_createpost($a, $uid, $post->retweeted_status, $self, false, false);
+		$retweet = twitter_createpost($a, $uid, $post->retweeted_status, $self, false, false, $noquote);
 
+		$retweet['object'] = $postarray['object'];
 		$retweet['contact-id'] = $postarray['contact-id'];
 		$retweet['owner-name'] = $postarray['owner-name'];
 		$retweet['owner-link'] = $postarray['owner-link'];
@@ -1486,8 +1578,8 @@ function twitter_createpost($a, $uid, $post, $self, $create_user, $only_existing
 		$postarray = $retweet;
 	}
 
-	if (is_object($post->quoted_status)) {
-		$quoted = twitter_createpost($a, $uid, $post->quoted_status, $self, false, false);
+	if (is_object($post->quoted_status) AND !$noquote) {
+		$quoted = twitter_createpost($a, $uid, $post->quoted_status, $self, false, false, true);
 
 		$postarray['body'] = $statustext;
 
@@ -1573,7 +1665,7 @@ function twitter_fetchparentposts($a, $uid, $post, $connection, $self, $own_id) 
 	$posts = array();
 
 	while ($post->in_reply_to_status_id_str != "") {
-		$parameters = array("trim_user" => false, "id" => $post->in_reply_to_status_id_str);
+		$parameters = array("trim_user" => false, "tweet_mode" => "extended", "id" => $post->in_reply_to_status_id_str);
 
 		$post = $connection->get('statuses/show', $parameters);
 
@@ -1599,7 +1691,7 @@ function twitter_fetchparentposts($a, $uid, $post, $connection, $self, $own_id) 
 
 	if (count($posts)) {
 		foreach ($posts as $post) {
-			$postarray = twitter_createpost($a, $uid, $post, $self, false, false);
+			$postarray = twitter_createpost($a, $uid, $post, $self, false, false, false);
 
 			if (trim($postarray['body']) == "")
 				continue;
@@ -1711,7 +1803,7 @@ function twitter_fetchhometimeline($a, $uid) {
 			if ($post->in_reply_to_status_id_str != "")
 				twitter_fetchparentposts($a, $uid, $post, $connection, $self, $own_id);
 
-			$postarray = twitter_createpost($a, $uid, $post, $self, $create_user, true);
+			$postarray = twitter_createpost($a, $uid, $post, $self, $create_user, true, false);
 
 			if (trim($postarray['body']) == "")
 				continue;
@@ -1758,7 +1850,7 @@ function twitter_fetchhometimeline($a, $uid) {
 			if ($post->in_reply_to_status_id_str != "")
 				twitter_fetchparentposts($a, $uid, $post, $connection, $self, $own_id);
 
-			$postarray = twitter_createpost($a, $uid, $post, $self, false, false);
+			$postarray = twitter_createpost($a, $uid, $post, $self, false, false, false);
 
 			if (trim($postarray['body']) == "")
 				continue;
