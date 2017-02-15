402b6e5c522ede6ac90bb33150d63be0f442d8a2
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-02-15 08:22:08 +0100

    SV translation THX Jonatan Nyberg

--- blackout/lang/sv/strings.php
+++ blackout/lang/sv/strings.php
@@ -1,3 +1,14 @@
 <?php
 
-$a->strings["Submit"] = "Spara";
+if(! function_exists("string_plural_select_sv")) {
+function string_plural_select_sv($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["Save Settings"] = "Spara inställningar";
+$a->strings["Redirect URL"] = "";
+$a->strings["all your visitors from the web will be redirected to this URL"] = "";
+$a->strings["Begin of the Blackout"] = "";
+$a->strings["format is <em>YYYY</em> year, <em>MM</em> month, <em>DD</em> day, <em>hh</em> hour and <em>mm</em> minute"] = "";
+$a->strings["End of the Blackout"] = "";
+$a->strings["The end-date is prior to the start-date of the blackout, you should fix this."] = "";
