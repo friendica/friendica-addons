724fc839240bec7c3a4d96343e8d9671ffdd3c36
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-04-09 08:25:58 +0200

    RU translation of group_text addon THX pztrn

--- group_text/lang/ru/strings.php
+++ group_text/lang/ru/strings.php
@@ -1,5 +1,11 @@
 <?php
 
-$a->strings["Group Text"] = "";
-$a->strings["Use a text only (non-image) group selector in the \"group edit\" menu"] = "";
-$a->strings["Submit"] = "Подтвердить";
+if(! function_exists("string_plural_select_ru")) {
+function string_plural_select_ru($n){
+	return ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14) ? 1 : $n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)? 2 : 3);;
+}}
+;
+$a->strings["Group Text settings updated."] = "Настройки Group Text обновлены.";
+$a->strings["Group Text"] = "Group Text";
+$a->strings["Use a text only (non-image) group selector in the \"group edit\" menu"] = "Используйте текстовый (не изображение) селектор группы в режиме редактирования группы";
+$a->strings["Submit"] = "Добавить";
