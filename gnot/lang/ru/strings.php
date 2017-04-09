9b6f3cd52c5d2d1edd6590793389225e764ee095
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-04-09 08:18:43 +0200

    RU translation of gnot addon THX pztrn

--- gnot/lang/ru/strings.php
+++ gnot/lang/ru/strings.php
@@ -1,8 +1,13 @@
 <?php
 
-$a->strings["Gnot settings updated."] = "";
-$a->strings["Gnot Settings"] = "";
-$a->strings["Allows threading of email comment notifications on Gmail and anonymising the subject line."] = "";
-$a->strings["Enable this plugin/addon?"] = "Включить этот плагин / аддон?";
-$a->strings["Submit"] = "Подтвердить";
-$a->strings["[Friendica:Notify] Comment to conversation #%d"] = "";
+if(! function_exists("string_plural_select_ru")) {
+function string_plural_select_ru($n){
+	return ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14) ? 1 : $n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)? 2 : 3);;
+}}
+;
+$a->strings["Gnot settings updated."] = "Настройки Gnot обновлены.";
+$a->strings["Gnot Settings"] = "Настройки Gnot";
+$a->strings["Allows threading of email comment notifications on Gmail and anonymising the subject line."] = "Разрешить нитевание уведомлений о комментариях на Gmail и анонимизировать поле \"Тема\".";
+$a->strings["Enable this plugin/addon?"] = "Включить этот плагин/аддон?";
+$a->strings["Submit"] = "Добавить";
+$a->strings["[Friendica:Notify] Comment to conversation #%d"] = "[Friendica:Notify] Комментарий в теме #%d";
