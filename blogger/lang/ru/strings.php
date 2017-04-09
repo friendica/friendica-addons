34f61b1dbdc80a0f70550f49326a5751d1c597e9
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-04-09 07:50:09 +0200

    RU translation of blogger addon THX pztrn

--- blogger/lang/ru/strings.php
+++ blogger/lang/ru/strings.php
@@ -1,11 +1,16 @@
 <?php
 
-$a->strings["Post to blogger"] = "";
-$a->strings["Blogger Post Settings"] = "";
-$a->strings["Enable Blogger Post Plugin"] = "";
-$a->strings["Blogger username"] = "";
-$a->strings["Blogger password"] = "";
-$a->strings["Blogger API URL"] = "";
-$a->strings["Post to Blogger by default"] = "";
-$a->strings["Submit"] = "Подтвердить";
+if(! function_exists("string_plural_select_ru")) {
+function string_plural_select_ru($n){
+	return ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14) ? 1 : $n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)? 2 : 3);;
+}}
+;
+$a->strings["Post to blogger"] = "Написать в Blogger";
+$a->strings["Blogger Export"] = "Экспорт в Blogger";
+$a->strings["Enable Blogger Post Plugin"] = "Включить плагин Blogger";
+$a->strings["Blogger username"] = "Имя пользователя Blogger";
+$a->strings["Blogger password"] = "Пароль Blogger";
+$a->strings["Blogger API URL"] = "Blogger API URL";
+$a->strings["Post to Blogger by default"] = "Отправлять в Blogger по умолчанию";
+$a->strings["Save Settings"] = "Сохранить настройки";
 $a->strings["Post from Friendica"] = "Сообщение от Friendica";
