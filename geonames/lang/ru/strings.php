2348099e2c55c8d4987ced9e28a1cddd7a995df5
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-04-09 08:17:42 +0200

    RU translation of geonames addon THX pztrn

--- geonames/lang/ru/strings.php
+++ geonames/lang/ru/strings.php
@@ -1,6 +1,11 @@
 <?php
 
-$a->strings["Geonames settings updated."] = "";
-$a->strings["Geonames Settings"] = "";
-$a->strings["Enable Geonames Plugin"] = "Включить Geonames  плагин";
-$a->strings["Submit"] = "Подтвердить";
+if(! function_exists("string_plural_select_ru")) {
+function string_plural_select_ru($n){
+	return ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14) ? 1 : $n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)? 2 : 3);;
+}}
+;
+$a->strings["Geonames settings updated."] = "Настройки Geonames обновлены.";
+$a->strings["Geonames Settings"] = "Настройки Geonames";
+$a->strings["Enable Geonames Plugin"] = "Включить плагин Geonames";
+$a->strings["Submit"] = "Добавить";
