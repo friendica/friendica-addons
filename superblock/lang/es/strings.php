49e49fea0da0ca4410015e7dee854c0af1a8f2b7
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-10-27 11:10:40 +0200

    ES translation of the superblock addon THX Albert

--- superblock/lang/es/strings.php
+++ superblock/lang/es/strings.php
@@ -5,9 +5,9 @@ function string_plural_select_es($n){
 	return ($n != 1);;
 }}
 ;
-$a->strings["\"Superblock\""] = "";
+$a->strings["\"Superblock\""] = "«Superbloque»";
 $a->strings["Comma separated profile URLS to block"] = "Perfil de URLS a bloque separado por comas";
 $a->strings["Save Settings"] = "Guardar configuración";
-$a->strings["SUPERBLOCK Settings saved."] = "";
+$a->strings["SUPERBLOCK Settings saved."] = "Ajustes de SUPERBLOQUE guardados.";
 $a->strings["Block Completely"] = "Bloquear completamente";
-$a->strings["superblock settings updated"] = "";
+$a->strings["superblock settings updated"] = "ajustes de superbloque actualizados";
