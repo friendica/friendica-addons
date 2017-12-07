e064d2da84f62028a497553bade56b5ce35d49b5
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-12-07 07:29:41 +0100

    added DE translation of IFTTT

---
+++ ifttt/lang/de/strings.php
@@ -0,0 +1,14 @@
+<?php
+
+if(! function_exists("string_plural_select_de")) {
+function string_plural_select_de($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["IFTTT Mirror"] = "IFTTT Spiegel";
+$a->strings["Create an account at <a href=\"http://www.ifttt.com\">IFTTT</a>. Create three Facebook recipes that are connected with <a href=\"https://ifttt.com/maker\">Maker</a> (In the form \"if Facebook then Maker\") with the following parameters:"] = "Lege ein Konto bei <a href=\"http://www.ifttt.com\"> IFTTT an</a>. Gebe drei Facebook Empfänger ein, die mit <a href=\"https://ifttt.com/maker\">Maker </a> verbunden sind (in der Form \"wenn Facebook dann Maker\"), mit den folgenden Parametern:";
+$a->strings["Body for \"new status message\""] = "Body für \"Neue Status Meldung\"";
+$a->strings["Body for \"new photo upload\""] = "Body für \"Neues Foto hochgeladen\"";
+$a->strings["Body for \"new link post\""] = "Body für \"Neue Nachricht\"";
+$a->strings["Generate new key"] = "Generiere neuen Schlüssel";
+$a->strings["Save Settings"] = "Speichere Einstellungen";
