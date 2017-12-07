25a7ca76f58ddd9be62fed6d10de0b125b38ba3a
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2017-12-07 07:28:27 +0100

    updated FR translation of rendertime addon

--- rendertime/lang/fr/strings.php
+++ rendertime/lang/fr/strings.php
@@ -1,2 +1,8 @@
 <?php
 
+if(! function_exists("string_plural_select_fr")) {
+function string_plural_select_fr($n){
+	return ($n > 1);;
+}}
+;
+$a->strings["Performance: Database: %s, Network: %s, Rendering: %s, Parser: %s, I/O: %s, Other: %s, Total: %s"] = "Performances: Base de données : %s, Réseau : %s, Rendu : %s, Parser : %s, Entrées/sorties : %s, Autre : %s, Total : %s";
