32315922c4f23320d64daa88b4207b080b87e3f5
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-03-02 11:35:45 +0100

    ZH CN translation of mathjax addon THX 朱陈锬

--- mathjax/lang/zh-cn/strings.php
+++ mathjax/lang/zh-cn/strings.php
@@ -1,9 +1,14 @@
 <?php
 
-$a->strings["Settings"] = "配置";
-$a->strings["The MathJax addon renders mathematical formulae written using the LaTeX syntax surrounded by the usual $$ or an eqnarray block in the postings of your wall,network tab and private mail."] = "MathJax插件表示用LaTeX句法的数学公式，围绕$";
-$a->strings["Use the MathJax renderer"] = "用MathJax表示器";
+if(! function_exists("string_plural_select_zh_cn")) {
+function string_plural_select_zh_cn($n){
+	return 0;;
+}}
+;
+$a->strings["Settings"] = "设置";
+$a->strings["The MathJax addon renders mathematical formulae written using the LaTeX syntax surrounded by the usual $$ or an eqnarray block in the postings of your wall,network tab and private mail."] = "";
+$a->strings["Use the MathJax renderer"] = "使用 MathJax 渲染";
 $a->strings["Submit"] = "提交";
-$a->strings["Settings updated."] = "设置跟新了";
-$a->strings["MathJax Base URL"] = "MathJax基础URL";
-$a->strings["The URL for the javascript file that should be included to use MathJax. Can be either the MathJax CDN or another installation of MathJax."] = "URL JavaScript文件应该包括为用MathJax。可以MathJax CDN或者别的MathJax安装。";
+$a->strings["Settings updated."] = "设置已更新";
+$a->strings["MathJax Base URL"] = "";
+$a->strings["The URL for the javascript file that should be included to use MathJax. Can be either the MathJax CDN or another installation of MathJax."] = "";
