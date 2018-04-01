77c278a27fb6c2e8709c9cf3c792ad4ac39e12c0
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 19:40:44 +0200

    group_text PL translation

--- group_text/lang/pl/strings.php
+++ group_text/lang/pl/strings.php
@@ -1,6 +1,11 @@
 <?php
 
-$a->strings["Group Text settings updated."] = "";
-$a->strings["Group Text"] = "";
-$a->strings["Use a text only (non-image) group selector in the \"group edit\" menu"] = "";
-$a->strings["Submit"] = "Potwierdź";
+if(! function_exists("string_plural_select_pl")) {
+function string_plural_select_pl($n){
+	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
+}}
+;
+$a->strings["Group Text settings updated."] = "Zaktualizowano ustawienia tekstu grupowego.";
+$a->strings["Group Text"] = "Grupuj tekst";
+$a->strings["Use a text only (non-image) group selector in the \"group edit\" menu"] = "Użyj selektora grupy tylko tekst (inny niż obraz) w menu \"Edycja grupy\"";
+$a->strings["Submit"] = "Zatwierdź";
