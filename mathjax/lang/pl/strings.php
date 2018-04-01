a658d74ff2caff15e197d523430a6505769e0dbe
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 18:22:35 +0200

    mathjax PL translation

--- mathjax/lang/pl/strings.php
+++ mathjax/lang/pl/strings.php
@@ -1,9 +1,14 @@
 <?php
 
+if(! function_exists("string_plural_select_pl")) {
+function string_plural_select_pl($n){
+	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
+}}
+;
 $a->strings["Settings"] = "Ustawienia";
-$a->strings["The MathJax addon renders mathematical formulae written using the LaTeX syntax surrounded by the usual $$ or an eqnarray block in the postings of your wall,network tab and private mail."] = "";
-$a->strings["Use the MathJax renderer"] = "";
-$a->strings["Submit"] = "Potwierdź";
-$a->strings["Settings updated."] = "Zaktualizowano ustawienia.";
-$a->strings["MathJax Base URL"] = "";
-$a->strings["The URL for the javascript file that should be included to use MathJax. Can be either the MathJax CDN or another installation of MathJax."] = "";
+$a->strings["The MathJax addon renders mathematical formulae written using the LaTeX syntax surrounded by the usual $$ or an eqnarray block in the postings of your wall,network tab and private mail."] = "Dodatek MathJax renderuje formuły matematyczne napisane przy użyciu składni LaTeX otoczonej zwykłym blokiem $$ lub eqnarray w wiadomościach na ścianie, karcie sieciowej i prywatnej korespondencji.";
+$a->strings["Use the MathJax renderer"] = "Użyj renderer MathJax";
+$a->strings["Submit"] = "Wyślij";
+$a->strings["Settings updated."] = "Ustawienia zaktualizowane.";
+$a->strings["MathJax Base URL"] = "Podstawowy adres URL MathJax";
+$a->strings["The URL for the javascript file that should be included to use MathJax. Can be either the MathJax CDN or another installation of MathJax."] = "Adres URL pliku javascript, który powinien być dołączony do korzystania z MathJax. Może to być MathJax CDN lub inna instalacja MathJax.";
