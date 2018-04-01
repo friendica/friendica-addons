2fe45ed3d3bc112052c49904aadfdb5936211fe0
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 11:36:02 +0200

    langfilter DE translation

--- langfilter/lang/de/strings.php
+++ langfilter/lang/de/strings.php
@@ -6,12 +6,14 @@ function string_plural_select_de($n){
 }}
 ;
 $a->strings["Language Filter"] = "Sprachfilter";
-$a->strings["This addon tries to identify the language of a postings. If it does not match any language spoken by you (see below) the posting will be collapsed. Remember detecting the language is not perfect, especially with short postings."] = "Dieses Addon versucht die Sprache eines Beitrags zu ermitteln und verbirgt den Inhalt, wenn du die Sprache nicht sprichst. Die Spracherkennung ist allerdings nicht perfekt, insbesondere bei kurzen Beiträgen.";
+$a->strings["This addon tries to identify the language posts are writen in. If it does not match any language specifed below, posts will be hidden by collapsing them."] = "Dieses Addon versucht zu identifizieren, in welche Sprachen Posts geschrieben werden. Wenn es nicht mit einer der unten angegebenen Sprachen übereinstimmt, werden Posts durch Zusammenklappen verborgen.";
 $a->strings["Use the language filter"] = "Den Sprachfilter verwenden";
-$a->strings["I speak"] = "Ich spreche";
+$a->strings["Able to read"] = "Lesbar";
 $a->strings["List of abbreviations (iso2 codes) for languages you speak, comma separated. For example \"de,it\"."] = "Liste von Abkürzungen (ISO2 Codes) der Sprachen die du sprichst, getrennt durch Komma. Zum Beispiel: \"de,it\".";
 $a->strings["Minimum confidence in language detection"] = "Minimales Vertrauenslevel in die Spracherkennung";
 $a->strings["Minimum confidence in language detection being correct, from 0 to 100. Posts will not be filtered when the confidence of language detection is below this percent value."] = "Minimales Vertrauen in die Richtigkeit der erkannten Sprache. Wert zwischen 0 und 100. Beiträge mit einem niedrigeren Vertrauenslevel werden nicht gefiltert.";
+$a->strings["Minimum length of message body"] = "Mindestlänge des Nachrichtentexts";
+$a->strings["Minimum number of characters in message body for filter to be used. Posts shorter than this will not be filtered. Note: Language detection is unreliable for short content (<200 characters)."] = "Mindestanzahl von Zeichen im Nachrichtentext die vom Filter benutz werden soll. Beiträge die kürzer sind werden nicht gefiltert. Hinweis: Die Spracherkennung ist bei kurze Inhalte (<200 Zeichen) nicht zuverlässig.";
 $a->strings["Save Settings"] = "Einstellungen speichern";
 $a->strings["Language Filter Settings saved."] = "Sprachfilter Einstellungen gespeichert.";
-$a->strings["unspoken language %s - Click to open/close"] = "nicht gesprochene Sprache %s - Zum öffnen/schließen klicken";
+$a->strings["Hidden content in %s - Click to open/close"] = "Versteckter Inhalt in %s - Klicken zum Öffnen / Schließen";
