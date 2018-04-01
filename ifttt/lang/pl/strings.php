6e617bb60d1e1f6486ea63f4739d3405ff449f02
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 19:17:14 +0200

    added ifttt PL translation

---
+++ ifttt/lang/pl/strings.php
@@ -0,0 +1,14 @@
+<?php
+
+if(! function_exists("string_plural_select_pl")) {
+function string_plural_select_pl($n){
+	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
+}}
+;
+$a->strings["IFTTT Mirror"] = "Lustro IFTTT";
+$a->strings["Create an account at <a href=\"http://www.ifttt.com\">IFTTT</a>. Create three Facebook recipes that are connected with <a href=\"https://ifttt.com/maker\">Maker</a> (In the form \"if Facebook then Maker\") with the following parameters:"] = "Utwórz konto na <a href=\"http://www.ifttt.com\">IFTTT</a>. Utwórz trzy przepisy na Facebooku, które są powiązane z <a href=\"https://ifttt.com/maker\">Maker</a> (W formie \"jeśli Facebook następnie Maker\") z następujących parametrów:";
+$a->strings["Body for \"new status message\""] = "Treść dla 'nowego statusu wiadomości'";
+$a->strings["Body for \"new photo upload\""] = "Treść dla 'nowego przesyłania zdjęć'";
+$a->strings["Body for \"new link post\""] = "Treść dla 'nowego łącza postu'";
+$a->strings["Generate new key"] = "Wygeneruj nowy klucz";
+$a->strings["Save Settings"] = "Zapisz ustawienia";
