a1769362aa26a17c1167f79c4f0d8018fa9bd804
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 11:48:26 +0200

    blogger PL translation

--- blogger/lang/pl/strings.php
+++ blogger/lang/pl/strings.php
@@ -1,11 +1,16 @@
 <?php
 
-$a->strings["Post to blogger"] = "Post na blogger";
-$a->strings["Blogger Post Settings"] = "Ustawienia postów na Blogger";
-$a->strings["Enable Blogger Post Addon"] = "";
-$a->strings["Blogger username"] = "Nazwa użytkownika na Blogger";
-$a->strings["Blogger password"] = "Hasło do Blogger";
-$a->strings["Blogger API URL"] = "";
+if(! function_exists("string_plural_select_pl")) {
+function string_plural_select_pl($n){
+	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
+}}
+;
+$a->strings["Post to blogger"] = "Opublikuj w bloggerze";
+$a->strings["Blogger Export"] = "Eksport Bloggera";
+$a->strings["Enable Blogger Post Addon"] = "Włącz dodatek Blogger";
+$a->strings["Blogger username"] = "Nazwa użytkownika Bloggera";
+$a->strings["Blogger password"] = "Hasło Bloggera";
+$a->strings["Blogger API URL"] = "Adres URL interfejsu API Blogger";
 $a->strings["Post to Blogger by default"] = "";
-$a->strings["Submit"] = "Potwierdź";
-$a->strings["Post from Friendica"] = "Post z Friendica";
+$a->strings["Save Settings"] = "Zapisz ustawienia";
+$a->strings["Post from Friendica"] = "Post od Friendica";
