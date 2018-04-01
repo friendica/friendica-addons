2d4a3b1c5868d18d5deeb09b74a3c68fc243cce7
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 18:32:27 +0200

    libertree PL translation

--- libertree/lang/pl/strings.php
+++ libertree/lang/pl/strings.php
@@ -1,9 +1,14 @@
 <?php
 
-$a->strings["Post to libertree"] = "";
-$a->strings["libertree Post Settings"] = "";
-$a->strings["Enable Libertree Post Addon"] = "";
-$a->strings["Libertree API token"] = "";
-$a->strings["Libertree site URL"] = "";
-$a->strings["Post to Libertree by default"] = "";
-$a->strings["Submit"] = "Potwierdź";
+if(! function_exists("string_plural_select_pl")) {
+function string_plural_select_pl($n){
+	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
+}}
+;
+$a->strings["Post to libertree"] = "Opublikuj na libertree";
+$a->strings["libertree Post Settings"] = "libertree Ustawienia Post";
+$a->strings["Enable Libertree Post Addon"] = "Włącz dodatek Libertree Post";
+$a->strings["Libertree API token"] = "Token API Libertree";
+$a->strings["Libertree site URL"] = "Adres URL witryny Libertree";
+$a->strings["Post to Libertree by default"] = "Wyślij domyślnie do Libertree";
+$a->strings["Submit"] = "Wyślij";
