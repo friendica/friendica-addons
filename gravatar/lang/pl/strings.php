410799269616341d6bb2afe16142580af2f5eaff
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 19:04:00 +0200

    gravatar PL translation

--- gravatar/lang/pl/strings.php
+++ gravatar/lang/pl/strings.php
@@ -1,15 +1,20 @@
 <?php
 
-$a->strings["generic profile image"] = "generuj obraz profilowy";
-$a->strings["random geometric pattern"] = "przypadkowy wzorzec geometryczny";
-$a->strings["monster face"] = "monster face";
-$a->strings["computer generated face"] = "";
-$a->strings["retro arcade style face"] = "";
-$a->strings["Information"] = "";
-$a->strings["Libravatar addon is installed, too. Please disable Libravatar addon or this Gravatar addon.<br>The Libravatar addon will fall back to Gravatar if nothing was found at Libravatar."] = "";
-$a->strings["Submit"] = "Potwierdź";
-$a->strings["Default avatar image"] = "Domyślny awatar";
-$a->strings["Select default avatar image if none was found at Gravatar. See README"] = "";
-$a->strings["Rating of images"] = "";
-$a->strings["Select the appropriate avatar rating for your site. See README"] = "";
-$a->strings["Gravatar settings updated."] = "Zaktualizowane ustawienie Gravatara";
+if(! function_exists("string_plural_select_pl")) {
+function string_plural_select_pl($n){
+	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
+}}
+;
+$a->strings["generic profile image"] = "ogólny obraz profilu";
+$a->strings["random geometric pattern"] = "przypadkowy wzór geometryczny";
+$a->strings["monster face"] = "twarz potwora";
+$a->strings["computer generated face"] = "wygenerowane komputerowo twarz";
+$a->strings["retro arcade style face"] = "twarz w stylu retro arcade";
+$a->strings["Information"] = "Informacje";
+$a->strings["Libravatar addon is installed, too. Please disable Libravatar addon or this Gravatar addon.<br>The Libravatar addon will fall back to Gravatar if nothing was found at Libravatar."] = "Zainstalowany jest także dodatek Libravatar. Wyłącz dodatek Libravatar lub ten dodatek Gravatar.<br> Dodatek Libravatar powróci do Gravatar, jeśli w Libravatar nie zostanie znaleziony żaden przedmiot.";
+$a->strings["Submit"] = "Zatwierdź";
+$a->strings["Default avatar image"] = "Domyślny obraz awatara";
+$a->strings["Select default avatar image if none was found at Gravatar. See README"] = "Wybierz domyślny obraz awatara, jeśli nie znaleziono go w Gravatar. Zobacz README";
+$a->strings["Rating of images"] = "Ocena obrazów";
+$a->strings["Select the appropriate avatar rating for your site. See README"] = "Wybierz odpowiednią ocenę awataru dla swojej witryny. Zobacz README";
+$a->strings["Gravatar settings updated."] = "Zaktualizowano ustawienia Gravatar.";
