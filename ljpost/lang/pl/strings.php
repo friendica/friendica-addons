90875067f521566b49fa3b7d807bc93006023241
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 18:31:05 +0200

    ljpost PL translation

--- ljpost/lang/pl/strings.php
+++ ljpost/lang/pl/strings.php
@@ -1,9 +1,14 @@
 <?php
 
-$a->strings["Post to LiveJournal"] = "Post do LiveJournal";
-$a->strings["LiveJournal Post Settings"] = "Ustawienia postów do LiveJournal";
-$a->strings["Enable LiveJournal Post Addon"] = "";
-$a->strings["LiveJournal username"] = "Nazwa użytkownika do LiveJournal";
-$a->strings["LiveJournal password"] = "Hasło do LiveJournal";
-$a->strings["Post to LiveJournal by default"] = "automatycznie publikuj na LiveJournal";
-$a->strings["Submit"] = "Potwierdź";
+if(! function_exists("string_plural_select_pl")) {
+function string_plural_select_pl($n){
+	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
+}}
+;
+$a->strings["Post to LiveJournal"] = "Opublikuj w LiveJournal";
+$a->strings["LiveJournal Post Settings"] = "Ustawienia Postów LiveJournal";
+$a->strings["Enable LiveJournal Post Addon"] = "Włącz dodatek LiveJournal";
+$a->strings["LiveJournal username"] = "Nazwa użytkownika LiveJournal";
+$a->strings["LiveJournal password"] = "Hasło LiveJournal";
+$a->strings["Post to LiveJournal by default"] = "Opublikuj domyślnie w LiveJournal";
+$a->strings["Submit"] = "Wyślij";
