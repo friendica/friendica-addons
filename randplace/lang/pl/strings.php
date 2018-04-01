9696091547510d9b30e50c0c7e8890cda646ee0c
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 11:52:42 +0200

    randplace PL translation

--- randplace/lang/pl/strings.php
+++ randplace/lang/pl/strings.php
@@ -1,5 +1,10 @@
 <?php
 
+if(! function_exists("string_plural_select_pl")) {
+function string_plural_select_pl($n){
+	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
+}}
+;
 $a->strings["Randplace Settings"] = "Ustawienia Randplace";
-$a->strings["Enable Randplace Addon"] = "Włącz Randplace Addon";
-$a->strings["Submit"] = "Potwierdź";
+$a->strings["Enable Randplace Addon"] = "Włącz dodatek Randplace";
+$a->strings["Submit"] = "Wyślij";
