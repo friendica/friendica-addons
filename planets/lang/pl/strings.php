5519a2f656132b175de0d0c049d534f4d500f939
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 11:41:43 +0200

    planets PL translation

--- planets/lang/pl/strings.php
+++ planets/lang/pl/strings.php
@@ -1,5 +1,10 @@
 <?php
 
-$a->strings["Planets Settings"] = "";
-$a->strings["Enable Planets Addon"] = "";
-$a->strings["Submit"] = "Potwierdź";
+if(! function_exists("string_plural_select_pl")) {
+function string_plural_select_pl($n){
+	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
+}}
+;
+$a->strings["Planets Settings"] = "Ustawienia Planet";
+$a->strings["Enable Planets Addon"] = "Włącz dodatek Planets";
+$a->strings["Submit"] = "Wyślij";
