950115a16241e4ef96db5bead3ec456e006d7495
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-01 18:14:32 +0200

    remote_permissions PL translation

--- remote_permissions/lang/pl/strings.php
+++ remote_permissions/lang/pl/strings.php
@@ -1,14 +1,19 @@
 <?php
 
-$a->strings["Remote Permissions Settings"] = "";
-$a->strings["Allow recipients of your private posts to see the other recipients of the posts"] = "";
-$a->strings["Submit"] = "Potwierdź";
-$a->strings["Remote Permissions settings updated."] = "";
-$a->strings["Visible to:"] = "Widoczne dla:";
-$a->strings["Visible to"] = "Widoczne dla";
-$a->strings["may only be a partial list"] = "";
-$a->strings["Global"] = "Ogólne";
-$a->strings["The posts of every user on this server show the post recipients"] = "";
-$a->strings["Individual"] = "Indywidualne";
-$a->strings["Each user chooses whether his/her posts show the post recipients"] = "";
+if(! function_exists("string_plural_select_pl")) {
+function string_plural_select_pl($n){
+	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
+}}
+;
+$a->strings["Remote Permissions Settings"] = "Ustawienia uprawnień zdalnych";
+$a->strings["Allow recipients of your private posts to see the other recipients of the posts"] = "Zezwalaj odbiorcom prywatnych wpisów na wyświetlanie innych adresatów postów";
+$a->strings["Submit"] = "Wyślij";
+$a->strings["Remote Permissions settings updated."] = "Zaktualizowano ustawienia uprawnień zdalnych.";
+$a->strings["Visible to:"] = "Widoczny dla:";
+$a->strings["Visible to"] = "Widoczny dla";
+$a->strings["may only be a partial list"] = "mogą być tylko częściowe listy";
+$a->strings["Global"] = "Ogólnoświatowy";
+$a->strings["The posts of every user on this server show the post recipients"] = "Wpisy każdego użytkownika na tym serwerze pokazują odbiorców wiadomości";
+$a->strings["Individual"] = "Indywidualny";
+$a->strings["Each user chooses whether his/her posts show the post recipients"] = "Każdy użytkownik wybiera, czy jego/jej posty pokazują odbiorców postów";
 $a->strings["Settings updated."] = "Zaktualizowano ustawienia.";
