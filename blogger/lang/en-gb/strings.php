07a2e3f5d825101e505b445165e65218b5cea7ee
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-09 10:34:15 +0200

    en-gb translation of blogger, catavatar, diaspora, dwpost, langfilter

---
+++ blogger/lang/en-gb/strings.php
@@ -0,0 +1,16 @@
+<?php
+
+if(! function_exists("string_plural_select_en_gb")) {
+function string_plural_select_en_gb($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["Post to blogger"] = "Post to Blogger";
+$a->strings["Blogger Export"] = "";
+$a->strings["Enable Blogger Post Addon"] = "Enable Blogger Post Addon";
+$a->strings["Blogger username"] = "Blogger username";
+$a->strings["Blogger password"] = "Blogger password";
+$a->strings["Blogger API URL"] = "";
+$a->strings["Post to Blogger by default"] = "Post to Blogger by default";
+$a->strings["Save Settings"] = "Save Settings";
+$a->strings["Post from Friendica"] = "Post from Friendica";
