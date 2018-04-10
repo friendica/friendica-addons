12d3992da9193d308dea9233fcbe8476e013fe4c
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-10 07:37:41 +0200

    EN-GB and EN-US localisation updates to blackout, blockem, langfilter, showmore and xmpp addons

---
+++ blackout/lang/en-us/strings.php
@@ -0,0 +1,14 @@
+<?php
+
+if(! function_exists("string_plural_select_en_us")) {
+function string_plural_select_en_us($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["Save Settings"] = "Save Settings";
+$a->strings["Redirect URL"] = "Redirect URL";
+$a->strings["all your visitors from the web will be redirected to this URL"] = "All your visitors from the web will be redirected to this URL.";
+$a->strings["Begin of the Blackout"] = "Start time of the Blackout";
+$a->strings["format is <em>YYYY</em> year, <em>MM</em> month, <em>DD</em> day, <em>hh</em> hour and <em>mm</em> minute"] = "Format is <em>YYYY</em> year, <em>MM</em> month, <em>DD</em> day, <em>hh</em> hour and <em>mm</em> minute";
+$a->strings["End of the Blackout"] = "End time of the Blackout";
+$a->strings["The end-date is prior to the start-date of the blackout, you should fix this."] = "The end-date is prior to the start-date of the blackout, you should fix this.";
--- blockem/lang/en-us/strings.php
+++ blockem/lang/en-us/strings.php
@@ -5,12 +5,12 @@ function string_plural_select_en_us($n){
 	return ($n != 1);;
 }}
 ;
-$a->strings["\"Blockem\""] = "\"Blockem\"";
+$a->strings["\"Blockem\""] = "Blockem";
 $a->strings["Hides user's content by collapsing posts. Also replaces their avatar with generic image."] = "Hides user's content by collapsing posts. Also replaces their avatar with generic image.";
 $a->strings["Comma separated profile URLS:"] = "Comma separated profile URLs:";
 $a->strings["Save Settings"] = "Save settings";
 $a->strings["BLOCKEM Settings saved."] = "Blockem settings saved.";
-$a->strings["Hidden content by %s - Click to open/close"] = "Hidden content by %s - Reveal/hide";
+$a->strings["Filtered user: %s"] = "Filtered user: %s";
 $a->strings["Unblock Author"] = "Unblock author";
 $a->strings["Block Author"] = "Block author";
 $a->strings["blockem settings updated"] = "Blockem settings updated";
