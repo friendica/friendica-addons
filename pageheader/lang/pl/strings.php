da25740b28bd7e795ef240679dee1f46beaaa41e
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-11 07:40:26 +0200

    PL update to the pageheader and startpage addon

--- pageheader/lang/pl/strings.php
+++ pageheader/lang/pl/strings.php
@@ -5,8 +5,8 @@ function string_plural_select_pl($n){
 	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
 }}
 ;
-$a->strings["\"pageheader\" Settings"] = "\"pageheader\" Ustawienia";
+$a->strings["\"pageheader\" Settings"] = "Ustawienia \"nagłówek strony\"";
 $a->strings["Message"] = "Wiadomość";
-$a->strings["Message to display on every page on this server (or put a pageheader.html file in your docroot)"] = "Wiadomość do wyświetlenia na każdej stronie tego serwera (lub umieść plik pageheader.html w swoim dokumencie roboczym)";
+$a->strings["Message to display on every page on this server (or put a pageheader.html file in your docroot)"] = "Komunikat wyświetlany na każdej stronie, na tym serwerze (lub umieść plik pageheader.html w swoim dokumencie roboczym)";
 $a->strings["Save Settings"] = "Zapisz ustawienia";
-$a->strings["pageheader Settings saved."] = "pageheader Ustawienia zapisane.";
+$a->strings["pageheader Settings saved."] = "nagłówek strony Ustawienia zapisane.";
--- startpage/lang/pl/strings.php
+++ startpage/lang/pl/strings.php
@@ -6,6 +6,6 @@ function string_plural_select_pl($n){
 }}
 ;
 $a->strings["Startpage Settings"] = "Ustawienia strony startowej";
-$a->strings["Home page to load after login  - leave blank for profile wall"] = "Strona główna do załadowania po zalogowaniu - pozostaw puste pole dla ściany profilowej";
+$a->strings["Home page to load after login  - leave blank for profile wall"] = "Strona główna do załadowania po zalogowaniu - pozostaw puste pole dla strony profilowej";
 $a->strings["Examples: &quot;network&quot; or &quot;notifications/system&quot;"] = "Przykłady: &quot;sieć&quot; lub &quot;powiadomienia/system&quot;";
 $a->strings["Submit"] = "Wyślij";
