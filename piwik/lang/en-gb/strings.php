21b57cb6d80eb5c7213c3e332ee679165c167660
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-16 07:31:24 +0200

    piwik addon added EN_GB translation THX Kris

---
+++ piwik/lang/en-gb/strings.php
@@ -0,0 +1,16 @@
+<?php
+
+if(! function_exists("string_plural_select_en_gb")) {
+function string_plural_select_en_gb($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["This website is tracked using the <a href='http://www.piwik.org'>Piwik</a> analytics tool."] = "Traffic on this website is tracked using the <a href='http://www.piwik.org'>Piwik</a> analytics tool.";
+$a->strings["If you do not want that your visits are logged in this way you <a href='%s'>can set a cookie to prevent Piwik from tracking further visits of the site</a> (opt-out)."] = "If you do not want your visits to be logged in this way, you <a href='%s'>can set a cookie to prevent Piwik from tracking your future visits to this site</a> (opt-out).";
+$a->strings["Save Settings"] = "Save settings";
+$a->strings["Piwik Base URL"] = "Piwik base URL";
+$a->strings["Absolute path to your Piwik installation. (without protocol (http/s), with trailing slash)"] = "Absolute path to your Piwik installation. (without the protocol (http or https), but with the trailing slash(/) )";
+$a->strings["Site ID"] = "Site ID";
+$a->strings["Show opt-out cookie link?"] = "Show opt-out cookie link?";
+$a->strings["Asynchronous tracking"] = "Asynchronous tracking";
+$a->strings["Settings updated."] = "Settings updated.";
