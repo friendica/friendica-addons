fec12801fbdd9dd803f537d488a12d492c5b8948
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-16 07:10:53 +0200

    added EN_BG translation to fromgplus addon THX Kris

---
+++ fromgplus/lang/en-gb/strings.php
@@ -0,0 +1,15 @@
+<?php
+
+if(! function_exists("string_plural_select_en_gb")) {
+function string_plural_select_en_gb($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["Google+ Mirror"] = "Google+ Mirror";
+$a->strings["Enable Google+ Import"] = "Enable Google+ Import";
+$a->strings["Google Account ID"] = "Google Account ID";
+$a->strings["Add keywords to post"] = "Add keywords to post";
+$a->strings["Save Settings"] = "Save Settings";
+$a->strings["Google+ Import Settings saved."] = "Google+ Import Settings saved.";
+$a->strings["Key"] = "Key";
+$a->strings["Settings updated."] = "Settings updated.";
