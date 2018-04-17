f1aa2bc5c63c09f0d69d09829b24ecb077f40c98
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-17 07:19:35 +0200

    added FI translation to the morepokes addon

---
+++ morepokes/lang/fi-fi/strings.php
@@ -0,0 +1,43 @@
+<?php
+
+if(! function_exists("string_plural_select_fi_fi")) {
+function string_plural_select_fi_fi($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["bitchslap"] = "";
+$a->strings["bitchslapped"] = "";
+$a->strings["shag"] = "";
+$a->strings["shagged"] = "";
+$a->strings["do something obscenely biological to"] = "";
+$a->strings["did something obscenely biological to"] = "";
+$a->strings["point out the poke feature to"] = "";
+$a->strings["pointed out the poke feature to"] = "";
+$a->strings["declare undying love for"] = "";
+$a->strings["declared undying love for"] = "";
+$a->strings["patent"] = "patentoi";
+$a->strings["patented"] = "patentoi";
+$a->strings["stroke beard"] = "";
+$a->strings["stroked their beard at"] = "";
+$a->strings["bemoan the declining standards of modern secondary and tertiary education to"] = "";
+$a->strings["bemoans the declining standards of modern secondary and tertiary education to"] = "";
+$a->strings["hug"] = "halaa";
+$a->strings["hugged"] = "halasi";
+$a->strings["kiss"] = "";
+$a->strings["kissed"] = "";
+$a->strings["raise eyebrows at"] = "";
+$a->strings["raised their eyebrows at"] = "";
+$a->strings["insult"] = "loukkaa";
+$a->strings["insulted"] = "loukkasi";
+$a->strings["praise"] = "kehu";
+$a->strings["praised"] = "kehui";
+$a->strings["be dubious of"] = "";
+$a->strings["was dubious of"] = "";
+$a->strings["eat"] = "syö";
+$a->strings["ate"] = "söi";
+$a->strings["giggle and fawn at"] = "";
+$a->strings["giggled and fawned at"] = "";
+$a->strings["doubt"] = "epäile";
+$a->strings["doubted"] = "epäili";
+$a->strings["glare"] = "mulkoile";
+$a->strings["glared at"] = "mulkoili";
