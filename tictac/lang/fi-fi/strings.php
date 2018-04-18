98dde22a57c463018f9dcebb41ec4389655f2d4d
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-18 08:31:36 +0200

    added translation for the TicTac addon THX Kris

---
+++ tictac/lang/fi-fi/strings.php
@@ -0,0 +1,19 @@
+<?php
+
+if(! function_exists("string_plural_select_fi_fi")) {
+function string_plural_select_fi_fi($n){
+	return ($n != 1);;
+}}
+;
+$a->strings["Three Dimensional Tic-Tac-Toe"] = "Kolmiulotteinen ristinolla";
+$a->strings["3D Tic-Tac-Toe"] = "3D ristinolla";
+$a->strings["New game"] = "Uusi peli";
+$a->strings["New game with handicap"] = "Uusi peli haitalla";
+$a->strings["Three dimensional tic-tac-toe is just like the traditional game except that it is played on multiple levels simultaneously. "] = "";
+$a->strings["In this case there are three levels. You win by getting three in a row on any level, as well as up, down, and diagonally across the different levels."] = "";
+$a->strings["The handicap game disables the center position on the middle level because the player claiming this square often has an unfair advantage."] = "";
+$a->strings["You go first..."] = "Aloita sinä...";
+$a->strings["I'm going first this time..."] = "Minä aloitan...";
+$a->strings["You won!"] = "Sinä voitit!";
+$a->strings["\"Cat\" game!"] = "";
+$a->strings["I won!"] = "Minä voitin!";
