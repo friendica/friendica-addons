14a0e36e05c6e54421aff99a4cd0de421d5dd011
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-04-28 10:10:08 +0200

    catavatar translation update for PL - THX waldis

--- catavatar/lang/pl/strings.php
+++ catavatar/lang/pl/strings.php
@@ -8,7 +8,7 @@ function string_plural_select_pl($n){
 $a->strings["Use Cat as Avatar"] = "Użyj kota jako awatara";
 $a->strings["More Random Cat!"] = "Więcej losowych kotów!";
 $a->strings["Reset to email Cat"] = "Resetuj na e-mail Kot";
-$a->strings["Cat Avatar Settings"] = "Ustawienia Cat Avatar";
+$a->strings["Cat Avatar Settings"] = "Ustawienia Kot Avatar";
 $a->strings["The cat hadn't found itself."] = "Kot się nie znalazł.";
 $a->strings["There was an error, the cat ran away."] = "Wystąpił błąd, kot uciekł.";
 $a->strings["Profile Photos"] = "Zdjęcie profilowe";
