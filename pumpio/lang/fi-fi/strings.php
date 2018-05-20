fed141ba05ad01d44e4109046b0e0a944f31704d
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-05-20 19:38:15 +0200

    update to the FI translations THX kris

--- pumpio/lang/fi-fi/strings.php
+++ pumpio/lang/fi-fi/strings.php
@@ -2,6 +2,7 @@
 
 if(! function_exists("string_plural_select_fi_fi")) {
 function string_plural_select_fi_fi($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
@@ -14,14 +15,14 @@ $a->strings["Pump.io Import/Export/Mirror"] = "";
 $a->strings["pump.io username (without the servername)"] = "pump.io käyttäjätunnus (ilman palvelinnimeä)";
 $a->strings["pump.io servername (without \"http://\" or \"https://\" )"] = "pump.io palvelinnimi (ilman \"http://\" tai \"https://\" )";
 $a->strings["Authenticate your pump.io connection"] = "";
-$a->strings["Import the remote timeline"] = "";
+$a->strings["Import the remote timeline"] = "Tuo etäaikajana";
 $a->strings["Enable pump.io Post Addon"] = "Ota pump.io -viestilisäosa käyttöön";
 $a->strings["Post to pump.io by default"] = "Lähetä pump.iohon oletuksena";
 $a->strings["Should posts be public?"] = "";
-$a->strings["Mirror all public posts"] = "";
+$a->strings["Mirror all public posts"] = "Peilaa kaikki julkiset julkaisut";
 $a->strings["Check to delete this preset"] = "";
 $a->strings["Save Settings"] = "Tallenna asetukset";
-$a->strings["Pump.io post failed. Queued for retry."] = "";
-$a->strings["Pump.io like failed. Queued for retry."] = "";
+$a->strings["Pump.io post failed. Queued for retry."] = "Pump.io -julkaisu epäonnistui. Jonossa uudelleenyritykseen.";
+$a->strings["Pump.io like failed. Queued for retry."] = "Pump.io -tykkäys epäonnistui. Jonossa uudelleenyritykseen.";
 $a->strings["status"] = "tila";
 $a->strings["%1\$s likes %2\$s's %3\$s"] = "";
--- statusnet/lang/fi-fi/strings.php
+++ statusnet/lang/fi-fi/strings.php
@@ -2,21 +2,22 @@
 
 if(! function_exists("string_plural_select_fi_fi")) {
 function string_plural_select_fi_fi($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
 $a->strings["Post to GNU Social"] = "Lähetä GNU Socialiin";
-$a->strings["Please contact your site administrator.<br />The provided API URL is not valid."] = "";
+$a->strings["Please contact your site administrator.<br />The provided API URL is not valid."] = "Ota yhteyttä sivuston ylläpitäjään.<br />API URL-osoite on virheellinen.";
 $a->strings["We could not contact the GNU Social API with the Path you entered."] = "";
 $a->strings["GNU Social settings updated."] = "GNU Social -asetukset päivitetty.";
-$a->strings["GNU Social Import/Export/Mirror"] = "";
+$a->strings["GNU Social Import/Export/Mirror"] = "GNU social tuonti/vienti/peili";
 $a->strings["Globally Available GNU Social OAuthKeys"] = "";
 $a->strings["There are preconfigured OAuth key pairs for some GNU Social servers available. If you are using one of them, please use these credentials. If not feel free to connect to any other GNU Social instance (see below)."] = "";
 $a->strings["Save Settings"] = "Tallenna asetukset";
 $a->strings["Provide your own OAuth Credentials"] = "";
 $a->strings["No consumer key pair for GNU Social found. Register your Friendica Account as an desktop client on your GNU Social account, copy the consumer key pair here and enter the API base root.<br />Before you register your own OAuth key pair ask the administrator if there is already a key pair for this Friendica installation at your favorited GNU Social installation."] = "";
-$a->strings["OAuth Consumer Key"] = "";
-$a->strings["OAuth Consumer Secret"] = "";
+$a->strings["OAuth Consumer Key"] = "OAuth kuluttajan avain";
+$a->strings["OAuth Consumer Secret"] = "OAuth kuluttajasalaisuus";
 $a->strings["Base API Path (remember the trailing /)"] = "";
 $a->strings["To connect to your GNU Social account click the button below to get a security code from GNU Social which you have to copy into the input box below and submit the form. Only your <strong>public</strong> posts will be posted to GNU Social."] = "";
 $a->strings["Log in with GNU Social"] = "Kirjaudu sisään GNU socialilla";
@@ -28,13 +29,13 @@ $a->strings["Currently connected to: "] = "";
 $a->strings["If enabled all your <strong>public</strong> postings can be posted to the associated GNU Social account. You can choose to do so by default (here) or for every posting separately in the posting options when writing the entry."] = "";
 $a->strings["<strong>Note</strong>: Due your privacy settings (<em>Hide your profile details from unknown viewers?</em>) the link potentially included in public postings relayed to GNU Social will lead the visitor to a blank page informing the visitor that the access to your profile has been restricted."] = "";
 $a->strings["Allow posting to GNU Social"] = "Salli julkaisut GNU socialiin";
-$a->strings["Send public postings to GNU Social by default"] = "";
-$a->strings["Mirror all posts from GNU Social that are no replies or repeated messages"] = "";
-$a->strings["Import the remote timeline"] = "";
+$a->strings["Send public postings to GNU Social by default"] = "Lähetä oletuksena kaikki julkiset julkaisut GNU socialiin";
+$a->strings["Mirror all posts from GNU Social that are no replies or repeated messages"] = "Peilaa kaikki julkaisut GNU socialista jotka eivät ole vastauksia tai toistettuja viestejä";
+$a->strings["Import the remote timeline"] = "Tuo etäaikajana";
 $a->strings["Disabled"] = "Poistettu käytöstä";
-$a->strings["Full Timeline"] = "";
+$a->strings["Full Timeline"] = "Koko aikajana";
 $a->strings["Only Mentions"] = "";
 $a->strings["Clear OAuth configuration"] = "";
 $a->strings["Site name"] = "Sivuston nimi";
-$a->strings["Consumer Secret"] = "";
-$a->strings["Consumer Key"] = "";
+$a->strings["Consumer Secret"] = "Kuluttajasalaisuus";
+$a->strings["Consumer Key"] = "Kuluttajan avain";
