be02acb8d343dce189a41318fce22c4bf70df2b3
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-05-20 19:20:27 +0200

    ES translation of the langfilter addon THX Albert

--- langfilter/lang/es/strings.php
+++ langfilter/lang/es/strings.php
@@ -2,16 +2,19 @@
 
 if(! function_exists("string_plural_select_es")) {
 function string_plural_select_es($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
 $a->strings["Language Filter"] = "Filtro de Idioma";
-$a->strings["This addon tries to identify the language of a postings. If it does not match any language spoken by you (see below) the posting will be collapsed. Remember detecting the language is not perfect, especially with short postings."] = "Este addon intenta identificar el idioma de las publicaciones. Si no encuentra ningún idioma hablado por usted (ver abajo) la entrada se colapsará. Recordar detectar el idioma no es perfecto, especialmente con entradas cortas.";
+$a->strings["This addon tries to identify the language posts are writen in. If it does not match any language specifed below, posts will be hidden by collapsing them."] = "Este complemento intenta identificar en qué idioma se han escrito las publicaciones. Si no coincide con el idioma especificado a continuación, las publicaciones se ocultarán al contraerlas.";
 $a->strings["Use the language filter"] = "Usar el filtro de idioma";
-$a->strings["I speak"] = "Yo hablo";
+$a->strings["Able to read"] = "Capaz de leer";
 $a->strings["List of abbreviations (iso2 codes) for languages you speak, comma separated. For example \"de,it\"."] = "Lista de abreviaciones (códigos iso2) para los idiomas que habla, separadas por comas. Por ejemplo \"de,it\".";
 $a->strings["Minimum confidence in language detection"] = "Mínima confianza en la detección de idioma";
 $a->strings["Minimum confidence in language detection being correct, from 0 to 100. Posts will not be filtered when the confidence of language detection is below this percent value."] = "Mínima confianza en que la detección de idioma sea correcta, de 0 a 100. Las entradas no se filtrarán cuando la confianza de la detección del idioma es inferior a su valor de porcentaje.";
+$a->strings["Minimum length of message body"] = "Longitud mínima del cuerpo del mensaje";
+$a->strings["Minimum number of characters in message body for filter to be used. Posts shorter than this will not be filtered. Note: Language detection is unreliable for short content (<200 characters)."] = "Número mínimo de caracteres en el cuerpo del mensaje para que se use el filtro. Las publicaciones más cortas que esto no serán filtradas. Nota: La detección de idioma no es fiable para contenido corto (<200 caracteres).";
 $a->strings["Save Settings"] = "Guardar Ajustes";
 $a->strings["Language Filter Settings saved."] = "Ajustes de Filtro de Idioma guardados.";
-$a->strings["unspoken language %s - Click to open/close"] = "Idioma sobreentendido %s - Click para abrir/cerrar";
+$a->strings["Filtered language: %s"] = "Idioma filtrado: %s";
