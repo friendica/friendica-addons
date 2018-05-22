105278ec353452aa5ba58d35230bb67167f72d32
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-05-22 07:25:57 +0200

    missing strings.php file

---
+++ catavatar/lang/es/strings.php
@@ -0,0 +1,16 @@
+<?php
+
+if(! function_exists("string_plural_select_es")) {
+function string_plural_select_es($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Use Cat as Avatar"] = "Usar Gato como Avatar";
+$a->strings["More Random Cat!"] = "¡Más gato al azar!";
+$a->strings["Reset to email Cat"] = "Restablecer a correo electrónico Gato";
+$a->strings["Cat Avatar Settings"] = "Ajustes de Avatar Gato";
+$a->strings["The cat hadn't found itself."] = "El gato no se había encontrado.";
+$a->strings["There was an error, the cat ran away."] = "Hubo un error, el gato se escapó.";
+$a->strings["Profile Photos"] = "Fotos de perfil";
+$a->strings["Meow!"] = "¡Miau!";
