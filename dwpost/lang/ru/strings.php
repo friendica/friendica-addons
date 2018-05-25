56ed2bccb7bcebb28623aa56356839aa730e0691
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-05-25 11:14:31 +0200

    RU translation dwpost addon THX Stanislav N

--- dwpost/lang/ru/strings.php
+++ dwpost/lang/ru/strings.php
@@ -2,12 +2,13 @@
 
 if(! function_exists("string_plural_select_ru")) {
 function string_plural_select_ru($n){
+	$n = intval($n);
 	return ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14) ? 1 : $n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)? 2 : 3);;
 }}
 ;
 $a->strings["Post to Dreamwidth"] = "Отправить в Dreamwidth";
 $a->strings["Dreamwidth Post Settings"] = "Настройки сообщений Dreamwidth";
-$a->strings["Enable dreamwidth Post Addon"] = "Включить плагин отправки сообщений в Dreamwidth";
+$a->strings["Enable dreamwidth Post Addon"] = "Включить аддон dreamwidth Post";
 $a->strings["dreamwidth username"] = "Имя пользователя Dreamwidth";
 $a->strings["dreamwidth password"] = "Пароль Dreamwidth";
 $a->strings["Post to dreamwidth by default"] = "Отправлять сообщения в Dreamwidth по умолчанию";
