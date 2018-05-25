565887fb91dec88e6ff2156b763435e84c7a3785
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-05-25 10:46:43 +0200

    IS translation buglink addon THX Sveinn í Felli

--- buglink/lang/is/strings.php
+++ buglink/lang/is/strings.php
@@ -1,3 +1,9 @@
 <?php
 
-$a->strings["Report Bug"] = "Tilkynna bilun";
+if(! function_exists("string_plural_select_is")) {
+function string_plural_select_is($n){
+	$n = intval($n);
+	return ($n % 10 != 1 || $n % 100 == 11);;
+}}
+;
+$a->strings["Report Bug"] = "Tilkynna villu";
