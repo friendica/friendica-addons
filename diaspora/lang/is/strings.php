e0c18c77f447fd1b9e2d542d4b40c47e407958c6
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-05-25 10:57:09 +0200

    IS translation diaspora addon THX Sveinn í Felli

---
+++ diaspora/lang/is/strings.php
@@ -0,0 +1,18 @@
+<?php
+
+if(! function_exists("string_plural_select_is")) {
+function string_plural_select_is($n){
+	$n = intval($n);
+	return ($n % 10 != 1 || $n % 100 == 11);;
+}}
+;
+$a->strings["Post to Diaspora"] = "Senda færslu á Diaspora";
+$a->strings["Can't login to your Diaspora account. Please check username and password and ensure you used the complete address (including http...)"] = "";
+$a->strings["Diaspora Export"] = "Diaspora útflutningur";
+$a->strings["Enable Diaspora Post Addon"] = "Virkja sendiviðbót fyrir Diaspora";
+$a->strings["Diaspora username"] = "Notandanafn á Diaspora";
+$a->strings["Diaspora password"] = "Lykilorð á Diaspora";
+$a->strings["Diaspora site URL"] = "Slóð á Diaspora-vefsvæði";
+$a->strings["Post to Diaspora by default"] = "Senda sjálfgefið færslur á Diaspora";
+$a->strings["Save Settings"] = "Vista stillingar";
+$a->strings["Diaspora post failed. Queued for retry."] = "Færsla Diaspora mistókst. Sett í biðröð til endurtekningar.";
