df19b133b957cb764ad68da4fb8f58d77edd21b4
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-05-25 10:14:01 +0200

    IS translation viewsrc addon THX Sveinn í Felli

--- viewsrc/lang/is/strings.php
+++ viewsrc/lang/is/strings.php
@@ -1,3 +1,9 @@
 <?php
 
-$a->strings["View Source"] = "";
+if(! function_exists("string_plural_select_is")) {
+function string_plural_select_is($n){
+	$n = intval($n);
+	return ($n % 10 != 1 || $n % 100 == 11);;
+}}
+;
+$a->strings["View Source"] = "Skoða uppruna";
