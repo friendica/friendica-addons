c6f4981cdfe0568a0c55a21cba18d07d4f70474e
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-09 10:17:39 +0200

    catavatar PL translation THX AgnesElisa

--- catavatar/lang/nl/strings.php
+++ catavatar/lang/nl/strings.php
@@ -2,6 +2,7 @@
 
 if(! function_exists("string_plural_select_nl")) {
 function string_plural_select_nl($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
@@ -9,5 +10,7 @@ $a->strings["Use Cat as Avatar"] = "Gebruik een kat als avatar";
 $a->strings["More Random Cat!"] = "Meer willekeurige kat!";
 $a->strings["Reset to email Cat"] = "Email kat opnieuw instellen.";
 $a->strings["Cat Avatar Settings"] = "Kat Avatar instellingen";
+$a->strings["The cat hadn't found itself."] = "Het katje kon zichzelf niet vinden.";
 $a->strings["There was an error, the cat ran away."] = "Er was een fout, de kat liep weg.";
+$a->strings["Profile Photos"] = "Profielfoto's";
 $a->strings["Meow!"] = "Miauw!";
