febd69ab98b9b1682417be9de0b0f963f7fe49d6
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-09 10:33:12 +0200

    buglink added NL translation THX Pascal

---
+++ buglink/lang/nl/strings.php
@@ -0,0 +1,9 @@
+<?php
+
+if(! function_exists("string_plural_select_nl")) {
+function string_plural_select_nl($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Report Bug"] = "Bug rapporteren";
