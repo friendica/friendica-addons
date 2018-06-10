c834f2e54c549a88b520616dd3b60530f05178f5
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-10 08:31:11 +0200

    added CS translation of the catavatar addon THX Aditoo

---
+++ catavatar/lang/cs/strings.php
@@ -0,0 +1,16 @@
+<?php
+
+if(! function_exists("string_plural_select_cs")) {
+function string_plural_select_cs($n){
+	$n = intval($n);
+	return ($n == 1 && $n % 1 == 0) ? 0 : ($n >= 2 && $n <= 4 && $n % 1 == 0) ? 1: ($n % 1 != 0 ) ? 2 : 3;;
+}}
+;
+$a->strings["Use Cat as Avatar"] = "Použít kočku jako avatar";
+$a->strings["More Random Cat!"] = "Další náhodné kočky!";
+$a->strings["Reset to email Cat"] = "";
+$a->strings["Cat Avatar Settings"] = "Nastavení Cat Avatar";
+$a->strings["The cat hadn't found itself."] = "Kočka se nenašla.";
+$a->strings["There was an error, the cat ran away."] = "Vyskytla se chyba, kočka utekla.";
+$a->strings["Profile Photos"] = "Profilové fotky";
+$a->strings["Meow!"] = "Mňau!";
