6de947b158118c2560915307da8536cea5a835eb
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-10 08:10:57 +0200

    updated CS translation of the diaspora addon THX Aditoo

--- diaspora/lang/cs/strings.php
+++ diaspora/lang/cs/strings.php
@@ -2,15 +2,16 @@
 
 if(! function_exists("string_plural_select_cs")) {
 function string_plural_select_cs($n){
-	return ($n==1) ? 0 : ($n>=2 && $n<=4) ? 1 : 2;;
+	$n = intval($n);
+	return ($n == 1 && $n % 1 == 0) ? 0 : ($n >= 2 && $n <= 4 && $n % 1 == 0) ? 1: ($n % 1 != 0 ) ? 2 : 3;;
 }}
 ;
-$a->strings["Post to Diaspora"] = "Příspěvek na Diaspora";
+$a->strings["Post to Diaspora"] = "Odeslat příspěvek na Diasporu";
 $a->strings["Can't login to your Diaspora account. Please check username and password and ensure you used the complete address (including http...)"] = "Nelze se přihlásit na váš Diaspora účet. Prosím ověřte své uživatelské jméno a heslo a ujistěte se, že jste použili kompletní adresu (včetně http...)";
 $a->strings["Diaspora Export"] = "Diaspora export";
-$a->strings["Enable Diaspora Post Addon"] = "Povolit Diaspora Addon";
-$a->strings["Diaspora username"] = "Diaspora uživatelské jméno";
-$a->strings["Diaspora password"] = "Diaspora heslo";
+$a->strings["Enable Diaspora Post Addon"] = "Povolit doplněk Diaspora Post";
+$a->strings["Diaspora username"] = "Uživatelské jméno na Diaspora";
+$a->strings["Diaspora password"] = "Heslo na Diaspora";
 $a->strings["Diaspora site URL"] = "Adresa webu Diaspora";
 $a->strings["Post to Diaspora by default"] = "Defaultně zasílat příspěvky na Diaspora";
 $a->strings["Save Settings"] = "Uložit Nastavení";
