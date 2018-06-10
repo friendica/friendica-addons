ebf9119a5a264907dbaa130c9ec862ab9ebdca83
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-10 08:17:01 +0200

    added CS translation of the windowsphonepush addon THX Aditoo

---
+++ windowsphonepush/lang/cs/strings.php
@@ -0,0 +1,13 @@
+<?php
+
+if(! function_exists("string_plural_select_cs")) {
+function string_plural_select_cs($n){
+	$n = intval($n);
+	return ($n == 1 && $n % 1 == 0) ? 0 : ($n >= 2 && $n <= 4 && $n % 1 == 0) ? 1: ($n % 1 != 0 ) ? 2 : 3;;
+}}
+;
+$a->strings["WindowsPhonePush settings updated."] = "Nastavení WindowsPhonePush aktualizována";
+$a->strings["WindowsPhonePush Settings"] = "Nastavení WindowsPhonePush";
+$a->strings["Enable WindowsPhonePush Addon"] = "Povolit doplněk WindowsPhonePush";
+$a->strings["Push text of new item"] = "Načíst text nové položky";
+$a->strings["Save Settings"] = "Uložit nastavení";
