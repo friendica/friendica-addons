56eefaa5d24ae110ee97517327ce161fdf6d201f
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-12 16:13:22 +0200

    updated CS translation of the planets addon THX Aditoo

--- planets/lang/cs/strings.php
+++ planets/lang/cs/strings.php
@@ -2,9 +2,10 @@
 
 if(! function_exists("string_plural_select_cs")) {
 function string_plural_select_cs($n){
-	return ($n==1) ? 0 : ($n>=2 && $n<=4) ? 1 : 2;;
+	$n = intval($n);
+	return ($n == 1 && $n % 1 == 0) ? 0 : ($n >= 2 && $n <= 4 && $n % 1 == 0) ? 1: ($n % 1 != 0 ) ? 2 : 3;;
 }}
 ;
-$a->strings["Planets Settings"] = "Planets Nastavení";
-$a->strings["Enable Planets Addon"] = "Povolit Planets addon";
+$a->strings["Planets Settings"] = "Nastavení Planets";
+$a->strings["Enable Planets Addon"] = "Povolit doplněk Planets";
 $a->strings["Submit"] = "Odeslat";
