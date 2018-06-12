93c564ecc5eb92503e85b30e87710ffd8a270b84
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-12 16:19:21 +0200

    updated CS translation of the pumpio addon THX Aditoo

--- pumpio/lang/cs/strings.php
+++ pumpio/lang/cs/strings.php
@@ -2,7 +2,8 @@
 
 if(! function_exists("string_plural_select_cs")) {
 function string_plural_select_cs($n){
-	return ($n==1) ? 0 : ($n>=2 && $n<=4) ? 1 : 2;;
+	$n = intval($n);
+	return ($n == 1 && $n % 1 == 0) ? 0 : ($n >= 2 && $n <= 4 && $n % 1 == 0) ? 1: ($n % 1 != 0 ) ? 2 : 3;;
 }}
 ;
 $a->strings["Permission denied."] = "Přístup odmítnut.";
@@ -15,13 +16,13 @@ $a->strings["pump.io username (without the servername)"] = "uživatelské jméno
 $a->strings["pump.io servername (without \"http://\" or \"https://\" )"] = "jméno serveru pump.io  (bez \"http://\" nebo \"https://\" )";
 $a->strings["Authenticate your pump.io connection"] = "Přihlásit ke spojení na pump.io";
 $a->strings["Import the remote timeline"] = "Importovat vzdálenou časovou osu";
-$a->strings["Enable pump.io Post Addon"] = "Aktivovat pump.io Post Addon";
-$a->strings["Post to pump.io by default"] = "Defaultní umístění na pump.oi ";
+$a->strings["Enable pump.io Post Addon"] = "Povolit doplněk pumo.io Post";
+$a->strings["Post to pump.io by default"] = "Posílat příspěvky na pump.io automaticky";
 $a->strings["Should posts be public?"] = "Mají být příspěvky veřejné?";
 $a->strings["Mirror all public posts"] = "Zrcadlit všechny veřejné příspěvky";
 $a->strings["Check to delete this preset"] = "Zaškrtnout pro smazání tohoto nastavení";
 $a->strings["Save Settings"] = "Uložit Nastavení";
 $a->strings["Pump.io post failed. Queued for retry."] = "Zaslání příspěvku na Pump.io selhalo. Příspěvek byl zařazen do fronty pro opakované odeslání.";
 $a->strings["Pump.io like failed. Queued for retry."] = "Zaslání příspěvku na Pump.io zřejmě selhalo. Příspěvek byl zařazen do fronty pro opakované odeslání.";
-$a->strings["status"] = "Stav";
-$a->strings["%1\$s likes %2\$s's %3\$s"] = "Uživateli %1\$s se líbí příspěvek %3\$s uživatele %2\$s";
+$a->strings["status"] = "stav";
+$a->strings["%1\$s likes %2\$s's %3\$s"] = "%1\$s se líbí %3\$s %2\$s";
