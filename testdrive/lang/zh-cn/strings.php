24781527ee2d9e9d053e95b38d871b4a3059847b
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-18 11:19:22 +0200

    ZH CN testdrive addon

--- testdrive/lang/zh-cn/strings.php
+++ testdrive/lang/zh-cn/strings.php
@@ -1,5 +1,12 @@
 <?php
 
+if(! function_exists("string_plural_select_zh_cn")) {
+function string_plural_select_zh_cn($n){
+	$n = intval($n);
+	return 0;;
+}}
+;
 $a->strings["Administrator"] = "管理员";
-$a->strings["Your account on %s will expire in a few days."] = "您账户在%s几天后过期了。";
-$a->strings["Your Friendica test account is about to expire."] = "您Friendica化验账户快过期了。";
+$a->strings["Your account on %s will expire in a few days."] = "您在 %s 的账户将在几天内过期。";
+$a->strings["Your Friendica test account is about to expire."] = "您的 Friendica 测试账户即将过期。";
+$a->strings["Hi %1\$s,\n\nYour test account on %2\$s will expire in less than five days. We hope you enjoyed this test drive and use this opportunity to find a permanent Friendica website for your integrated social communications. A list of public sites is available at %s/siteinfo - and for more information on setting up your own Friendica server please see the Friendica project website at http://friendica.com."] = "嘿 %1\$s，\n\n您在 %2\$s 的测试账户将在五天内过期。我们希望您享受这次试用并以此机会为您的综合社交找到常驻的 Friendica 站点。公开可用站点罗列在 %s/siteinfo - 若要了解更多关于建立自己的 Friendica 服务站点的信息，请浏览 Friendica 项目的官方网站 http://friendica.com 。";
