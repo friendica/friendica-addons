767e7b78e5c8ebd42a5a1891e761bde10c876ee5
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-18 11:30:35 +0200

    ZH CN windowsphonepush translation

---
+++ windowsphonepush/lang/zh-cn/strings.php
@@ -0,0 +1,13 @@
+<?php
+
+if(! function_exists("string_plural_select_zh_cn")) {
+function string_plural_select_zh_cn($n){
+	$n = intval($n);
+	return 0;;
+}}
+;
+$a->strings["WindowsPhonePush settings updated."] = "WindowsPhonePush 设置已更新。";
+$a->strings["WindowsPhonePush Settings"] = "WindowsPhonePush 设置";
+$a->strings["Enable WindowsPhonePush Addon"] = "启用 WindowsPhonePush 插件";
+$a->strings["Push text of new item"] = "推送新项目的文本";
+$a->strings["Save Settings"] = "保存设置";
