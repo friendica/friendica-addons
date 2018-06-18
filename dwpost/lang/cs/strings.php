af81c197c050ff15455091c3b3d548fc9e267fb9
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-18 08:48:59 +0200

    CS dwpost addon

--- dwpost/lang/cs/strings.php
+++ dwpost/lang/cs/strings.php
@@ -2,13 +2,14 @@
 
 if(! function_exists("string_plural_select_cs")) {
 function string_plural_select_cs($n){
-	return ($n==1) ? 0 : ($n>=2 && $n<=4) ? 1 : 2;;
+	$n = intval($n);
+	return ($n == 1 && $n % 1 == 0) ? 0 : ($n >= 2 && $n <= 4 && $n % 1 == 0) ? 1: ($n % 1 != 0 ) ? 2 : 3;;
 }}
 ;
 $a->strings["Post to Dreamwidth"] = "Poslat na Dreamwidth";
-$a->strings["Dreamwidth Post Settings"] = "Dreamwidth nastavení příspěvků";
-$a->strings["Enable dreamwidth Post Addon"] = "Povolit dreamwidth Addon";
+$a->strings["Dreamwidth Post Settings"] = "Nastavení Dreamwidth Post";
+$a->strings["Enable dreamwidth Post Addon"] = "Povolit doplněk Dreamwidth Post";
 $a->strings["dreamwidth username"] = "dreamwidth uživatelské jméno";
 $a->strings["dreamwidth password"] = "dreamwidth heslo";
-$a->strings["Post to dreamwidth by default"] = "Defaultně umístit na dreamwidth";
+$a->strings["Post to dreamwidth by default"] = "Ve výchozím stavu posílat na dreamwidth";
 $a->strings["Submit"] = "Odeslat";
