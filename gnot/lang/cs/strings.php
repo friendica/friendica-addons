f8b1f15c0ce6f88434c59a14263501270be5b31d
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-18 10:25:52 +0200

    CS gnot addon

--- gnot/lang/cs/strings.php
+++ gnot/lang/cs/strings.php
@@ -2,12 +2,13 @@
 
 if(! function_exists("string_plural_select_cs")) {
 function string_plural_select_cs($n){
-	return ($n==1) ? 0 : ($n>=2 && $n<=4) ? 1 : 2;;
+	$n = intval($n);
+	return ($n == 1 && $n % 1 == 0) ? 0 : ($n >= 2 && $n <= 4 && $n % 1 == 0) ? 1: ($n % 1 != 0 ) ? 2 : 3;;
 }}
 ;
 $a->strings["Gnot settings updated."] = "Nastavení Gnot aktualizováno.";
 $a->strings["Gnot Settings"] = "Nastavení Gnot";
-$a->strings["Allows threading of email comment notifications on Gmail and anonymising the subject line."] = "Umožnit řetězení emailových komentářových notifikací na Gmailu a anonymizací řádky předmětu.";
-$a->strings["Enable this addon?"] = "Povolit tento addon/rozšíření?";
+$a->strings["Allows threading of email comment notifications on Gmail and anonymising the subject line."] = "Umožnit řetězení emailových komentářových oznámení na Gmailu a anonymizací řádky předmětu.";
+$a->strings["Enable this addon?"] = "Povolit tento doplněk?";
 $a->strings["Submit"] = "Odeslat";
-$a->strings["[Friendica:Notify] Comment to conversation #%d"] = "[Friendica:Upozornění] Komentář ke konverzaci #%d";
+$a->strings["[Friendica:Notify] Comment to conversation #%d"] = "[Friendica:Oznámení] Komentář ke konverzaci #%d";
