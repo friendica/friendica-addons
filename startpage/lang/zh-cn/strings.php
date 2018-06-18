81469bcd1f40da3597c36066f04bec0d0f29837c
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-18 11:15:02 +0200

    CS, ZH CN startpage addon

--- startpage/lang/zh-cn/strings.php
+++ startpage/lang/zh-cn/strings.php
@@ -1,6 +1,12 @@
 <?php
 
-$a->strings["Startpage Settings"] = "开始页设置";
-$a->strings["Home page to load after login  - leave blank for profile wall"] = "主页登录后表示－留空白为简介墙";
-$a->strings["Examples: &quot;network&quot; or &quot;notifications/system&quot;"] = "例如：&quot;网络&quot;或&quot;通知／系统&quot;";
+if(! function_exists("string_plural_select_zh_cn")) {
+function string_plural_select_zh_cn($n){
+	$n = intval($n);
+	return 0;;
+}}
+;
+$a->strings["Startpage Settings"] = "首页设置";
+$a->strings["Home page to load after login  - leave blank for profile wall"] = "";
+$a->strings["Examples: &quot;network&quot; or &quot;notifications/system&quot;"] = "";
 $a->strings["Submit"] = "提交";
