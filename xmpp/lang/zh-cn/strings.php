14996bd9b0955bc09197661491220c6d80bce4d3
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-06-18 11:34:12 +0200

    ZH CN xmpp addon

--- xmpp/lang/zh-cn/strings.php
+++ xmpp/lang/zh-cn/strings.php
@@ -2,6 +2,7 @@
 
 if(! function_exists("string_plural_select_zh_cn")) {
 function string_plural_select_zh_cn($n){
+	$n = intval($n);
 	return 0;;
 }}
 ;
@@ -12,5 +13,5 @@ $a->strings["Individual Credentials"] = "个人凭证";
 $a->strings["Jabber BOSH host"] = "Jabber BOSH 主机";
 $a->strings["Save Settings"] = "保存设置";
 $a->strings["Use central userbase"] = "使用中央用户库";
-$a->strings["If enabled, users will automatically login to an ejabberd server that has to be installed on this machine with synchronized credentials via the \"auth_ejabberd.php\" script."] = "";
+$a->strings["If enabled, users will automatically login to an ejabberd server that has to be installed on this machine with synchronized credentials via the \"auth_ejabberd.php\" script."] = "如果启用，用户将通过 “auth_ejabberd.php” 脚本使用同步证书自动登录到已在此设备上安装完成的 ejabberd 服务器。";
 $a->strings["Settings updated."] = "设置已更新。";
