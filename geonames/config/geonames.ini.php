57f6ac5bad1169e12a0f906f95135e54836da87d
Hypolite Petovan <mrpetovan@gmail.com>
2018-06-27 23:12:27 -0400

    [geonames] Add addon config

---
+++ geonames/config/geonames.ini.php
@@ -0,0 +1,12 @@
+<?php return <<<INI
+
+; Warning: Don't change this file! It only holds the default config values for this addon.
+; Instead overwrite these config values in config/local.ini.php in your Friendica directory
+
+[geonames]
+; username (String)
+; The geonames.org API username
+username =
+
+INI;
+//Keep this line
