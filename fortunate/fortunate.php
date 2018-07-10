f47e6cbc24958c4a7c52fa70310f84af0292ea7d
Hypolite Petovan <mrpetovan@gmail.com>
2018-07-10 08:44:13 -0400

    Revert "[fortunate] Move addon to deprecated-addons"
    This reverts commit 9ad60dfe4dd8badfc14ae3bd499250c1daf764e8.

---
+++ fortunate/fortunate.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Name: Fortunate
+ * Description: Add a random fortune cookie at the bottom of every pages. [Requires manual confguration.]
+ * Version: 1.0
+ * Author: Mike Macgirvin <http://macgirvin.com/profile/mike>
+ */
+use Friendica\Core\Addon;
+use Friendica\Util\Network;
+
+// IMPORTANT: SET THIS to your fortunate server
+
+define('FORTUNATE_SERVER', 'hostname.com');
+
+function fortunate_install()
+{
+	Addon::registerHook('page_end', 'addon/fortunate/fortunate.php', 'fortunate_fetch');
+	if (FORTUNATE_SERVER == 'hostname.com' && is_site_admin()) {
+		notice('Fortunate addon requires configuration. See README');
+	}
+}
+
+function fortunate_uninstall()
+{
+	Addon::unregisterHook('page_end', 'addon/fortunate/fortunate.php', 'fortunate_fetch');
+}
+
+
+function fortunate_fetch(&$a, &$b)
+{
+	$a->page['htmlhead'] .= '<link rel="stylesheet" type="text/css" href="' 
+		. $a->get_baseurl() . '/addon/fortunate/fortunate.css' . '" media="all" />' . "\r\n";
+
+	if (FORTUNATE_SERVER != 'hostname.com') {
+		$s = Network::fetchUrl('http://' . FORTUNATE_SERVER . '/cookie.php?numlines=2&equal=1&rand=' . mt_rand());
+		$b .= '<div class="fortunate">' . $s . '</div>';
+	}
+}
