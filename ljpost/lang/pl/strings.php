871fe55ebba7ef2c5393a72a27bf3f36410e6c21
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-08-06 14:07:13 +0200

    PL translation ljpost THX waldis

--- ljpost/lang/pl/strings.php
+++ ljpost/lang/pl/strings.php
@@ -2,13 +2,14 @@
 
 if(! function_exists("string_plural_select_pl")) {
 function string_plural_select_pl($n){
+	$n = intval($n);
 	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
 }}
 ;
-$a->strings["Post to LiveJournal"] = "Opublikuj w LiveJournal";
-$a->strings["LiveJournal Post Settings"] = "Ustawienia Postów LiveJournal";
-$a->strings["Enable LiveJournal Post Addon"] = "Włącz dodatek LiveJournal";
-$a->strings["LiveJournal username"] = "Nazwa użytkownika LiveJournal";
-$a->strings["LiveJournal password"] = "Hasło LiveJournal";
-$a->strings["Post to LiveJournal by default"] = "Opublikuj domyślnie w LiveJournal";
+$a->strings["Post to Insanejournal"] = "Opublikuj w Insanejournal";
+$a->strings["InsaneJournal Post Settings"] = "Ustawienia InsaneJournal Post";
+$a->strings["Enable InsaneJournal Post Addon"] = "Włącz dodatek InsaneJournal";
+$a->strings["InsaneJournal username"] = "nazwa użytkownika InsaneJournal";
+$a->strings["InsaneJournal password"] = "Hasło InsaneJournal";
+$a->strings["Post to InsaneJournal by default"] = "Wyślij domyślnie do InsaneJournal";
 $a->strings["Submit"] = "Wyślij";
