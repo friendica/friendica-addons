8a700150cc8c0d430ca2b245618cc3aa4df375c7
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-08-06 14:08:40 +0200

    PL translation libertree THX waldis

--- libertree/lang/pl/strings.php
+++ libertree/lang/pl/strings.php
@@ -2,11 +2,12 @@
 
 if(! function_exists("string_plural_select_pl")) {
 function string_plural_select_pl($n){
+	$n = intval($n);
 	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
 }}
 ;
 $a->strings["Post to libertree"] = "Opublikuj na libertree";
-$a->strings["libertree Post Settings"] = "libertree Ustawienia Post";
+$a->strings["libertree Post Settings"] = "Ustawienia libertree Post";
 $a->strings["Enable Libertree Post Addon"] = "Włącz dodatek Libertree Post";
 $a->strings["Libertree API token"] = "Token API Libertree";
 $a->strings["Libertree site URL"] = "Adres URL witryny Libertree";
