b7717db8ead4da1ac39ee462a2b5609759f9b046
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-08-17 11:32:24 +0200

    ZH CN translation of several addons THX 朱陈锬

--- public_server/lang/zh-cn/strings.php
+++ public_server/lang/zh-cn/strings.php
@@ -1,5 +1,12 @@
 <?php
 
+if(! function_exists("string_plural_select_zh_cn")) {
+function string_plural_select_zh_cn($n){
+	$n = intval($n);
+	return 0;;
+}}
+;
 $a->strings["Administrator"] = "管理员";
-$a->strings["Your account on %s will expire in a few days."] = "您账户在%s几天后过期了。";
-$a->strings["Your Friendica account is about to expire."] = "您Friendica账户快过期。";
+$a->strings["Your account on %s will expire in a few days."] = "您在 %s 的帐号将在几天内到期。";
+$a->strings["Your Friendica account is about to expire."] = "您的 Friendica 帐号即将到期。";
+$a->strings["Hi %1\$s,\n\nYour account on %2\$s will expire in less than five days. You may keep your account by logging in at least once every 30 days"] = "您好 %1\$s，\n\n您在 %2\$s 的帐号将在五天内到期。您可以通过每 30 天至少登录一次的方式保留您的帐号。";
---
+++ smileybutton/lang/zh-cn/strings.php
@@ -0,0 +1,12 @@
+<?php
+
+if(! function_exists("string_plural_select_zh_cn")) {
+function string_plural_select_zh_cn($n){
+	$n = intval($n);
+	return 0;;
+}}
+;
+$a->strings["Smileybutton settings"] = "表情按钮设置";
+$a->strings["You can hide the button and show the smilies directly."] = "您可以隐藏按钮并直接显示表情符号。";
+$a->strings["Hide the button"] = "隐藏按钮";
+$a->strings["Save Settings"] = "保存设置";
