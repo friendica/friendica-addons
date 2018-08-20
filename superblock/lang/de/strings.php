c97690bc1230926f9ada4f9744e3055ed9c424ed
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-08-20 13:06:55 +0200

    CS, DE translation blockem and superblock addon (#713)
    * CS translation blockem and superblock addon THX Aditoo
    
    * DE translation blockem and superblock addon THX Abrax

--- superblock/lang/de/strings.php
+++ superblock/lang/de/strings.php
@@ -2,12 +2,13 @@
 
 if(! function_exists("string_plural_select_de")) {
 function string_plural_select_de($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
-$a->strings["\"Superblock\" Settings"] = "\"Superblock\" Einstellungen";
+$a->strings["Superblock"] = "Superblock";
 $a->strings["Comma separated profile URLS to block"] = "Profil-URLs, die geblockt werden sollen (durch Kommas getrennt)";
-$a->strings["Save Settings"] = "Einstellungen speichern";
-$a->strings["SUPERBLOCK Settings saved."] = "SUPERBLOCK Einstellungen gespeichert";
-$a->strings["Block Completely"] = "Komplett blockieren";
-$a->strings["superblock settings updated"] = "Superblock Einstellungen wurden aktualisiert";
+$a->strings["Save Settings"] = "Einstellungen Speichern";
+$a->strings["SUPERBLOCK Settings saved."] = "Superblock Einstellungen gespeichert";
+$a->strings["Block Completely"] = "Vollständig blockieren";
+$a->strings["superblock settings updated"] = "Superblock Einstellungen aktualisiert";
