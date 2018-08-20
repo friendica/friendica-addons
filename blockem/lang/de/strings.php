03e2a162d92f092e7cc8f33c6a2ba041b9219a81
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-08-20 09:46:35 +0200

    DE translation blockem and superblock addon THX Abrax

--- blockem/lang/de/strings.php
+++ blockem/lang/de/strings.php
@@ -2,10 +2,11 @@
 
 if(! function_exists("string_plural_select_de")) {
 function string_plural_select_de($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
-$a->strings["\"Blockem\""] = "\"Blockem\"";
+$a->strings["Blockem"] = "Blockem";
 $a->strings["Hides user's content by collapsing posts. Also replaces their avatar with generic image."] = "Verbirgt Inhalte von Kontakten durch kollabieren der Beiträge. Des weiteren wird das Profilbild durch einen generischen Avatar ersetzt.";
 $a->strings["Comma separated profile URLS:"] = "Komma separierte Liste von Profil URLs";
 $a->strings["Save Settings"] = "Einstellungen speichern";
