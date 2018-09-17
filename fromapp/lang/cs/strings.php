ed9297ea25ffe77667c69a488bbb9f497a90e42d
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2018-09-17 21:58:44 +0200

    CS translation fromapp THX Aditoo

--- fromapp/lang/cs/strings.php
+++ fromapp/lang/cs/strings.php
@@ -2,11 +2,12 @@
 
 if(! function_exists("string_plural_select_cs")) {
 function string_plural_select_cs($n){
-	return ($n==1) ? 0 : ($n>=2 && $n<=4) ? 1 : 2;;
+	$n = intval($n);
+	return ($n == 1 && $n % 1 == 0) ? 0 : ($n >= 2 && $n <= 4 && $n % 1 == 0) ? 1: ($n % 1 != 0 ) ? 2 : 3;;
 }}
 ;
-$a->strings["Fromapp settings updated."] = "Fromapp nastavení aktualizováno.";
-$a->strings["FromApp Settings"] = "FromApp nastavení";
-$a->strings["The application name you would like to show your posts originating from."] = "Jméno zdrojové aplikace";
-$a->strings["Use this application name even if another application was used."] = "Použij toto jméno aplikace i když byla použita jiná aplikace";
+$a->strings["Fromapp settings updated."] = "Nastavení FromApp aktualizována.";
+$a->strings["FromApp Settings"] = "Nastavení FromApp";
+$a->strings["The application name you would like to show your posts originating from."] = "Jméno aplikace, která má být zobrazena jako zdroj Vašich příspěvků.";
+$a->strings["Use this application name even if another application was used."] = "Použít toto jméno aplikace, i když byla použita jiná aplikace";
 $a->strings["Submit"] = "Odeslat";
