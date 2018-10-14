b040bee1d0d31404f6d207545d927fab6d82c5e5
Hypolite Petovan <hypolite@mrpetovan.com>
2018-10-14 07:17:15 -0400

    [fromgplus] Fix Undefined property: stdClass::$published notice

--- fromgplus/fromgplus.php
+++ fromgplus/fromgplus.php
@@ -526,12 +526,12 @@ function fromgplus_fetch($a, $uid) {
 						if (function_exists("share_header"))
 							$post .= share_header($item->object->actor->displayName, $item->object->actor->url,
 										$item->object->actor->image->url, "",
-										DateTimeFormat::utc($item->object->published),$item->object->url);
+										DateTimeFormat::utc($item->published),$item->object->url);
 						else
 							$post .= "[share author='".str_replace("'", "&#039;",$item->object->actor->displayName).
 									"' profile='".$item->object->actor->url.
 									"' avatar='".$item->object->actor->image->url.
-									"' posted='".DateTimeFormat::utc($item->object->published).
+									"' posted='".DateTimeFormat::utc($item->published).
 									"' link='".$item->object->url."']";
 
 						$post .= fromgplus_html2bbcode($item->object->content);
