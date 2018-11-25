b87b840db58167baa1431673116c64a064be169e
Hypolite Petovan <hypolite@mrpetovan.com>
2018-11-25 02:11:53 -0500

    [geonames] Update config file style/name

---
+++ geonames/config/geonames.config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Warning: Don't change this file! It only holds the default config values for this addon.
+// Instead overwrite these config values in config/addon.config.php in your Friendica directory
+
+return [
+	'geonames' => [
+		//username (String)
+		//The geonames.org API username
+		'username' => '',
+	],
+];
