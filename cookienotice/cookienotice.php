f3a5f16fc17e6400cae27276ac31ebe2d531bd84
Peter Liebetrau <peter.liebetrau@gmail.com>
2019-01-20 14:20:20 +0100

    convert spaces to tabs, removed netbeans project files

--- cookienotice/cookienotice.php
+++ cookienotice/cookienotice.php
@@ -13,64 +13,63 @@ use Friendica\Core\L10n;
 
 function cookienotice_install()
 {
-    $file = 'addon/cookienotice/cookienotice.php';
-    Addon::registerHook('page_content_top', $file, 'cookienotice_page_content_top');
-    Addon::registerHook('page_end', $file, 'cookienotice_page_end');
-    Addon::registerHook('addon_settings', $file, 'cookienotice_addon_settings');
-    Addon::registerHook('addon_settings_post', $file, 'cookienotice_addon_settings_post');
+	$file = 'addon/cookienotice/cookienotice.php';
+	Addon::registerHook('page_content_top', $file, 'cookienotice_page_content_top');
+	Addon::registerHook('page_end', $file, 'cookienotice_page_end');
+	Addon::registerHook('addon_settings', $file, 'cookienotice_addon_settings');
+	Addon::registerHook('addon_settings_post', $file, 'cookienotice_addon_settings_post');
 }
 
 function cookienotice_uninstall()
 {
-    $file = 'addon/cookienotice/cookienotice.php';
-    Addon::unregisterHook('page_content_top', $file, 'cookienotice_page_content_top');
-    Addon::unregisterHook('page_end', $file, 'cookienotice_page_end');
-    Addon::unregisterHook('addon_settings', $file, 'cookienotice_addon_settings');
-    Addon::unregisterHook('addon_settings_post', $file, 'cookienotice_addon_settings_post');
+	$file = 'addon/cookienotice/cookienotice.php';
+	Addon::unregisterHook('page_content_top', $file, 'cookienotice_page_content_top');
+	Addon::unregisterHook('page_end', $file, 'cookienotice_page_end');
+	Addon::unregisterHook('addon_settings', $file, 'cookienotice_addon_settings');
+	Addon::unregisterHook('addon_settings_post', $file, 'cookienotice_addon_settings_post');
 }
 
 function cookienotice_addon_settings(&$a, &$s)
 {
-    if (!is_site_admin())
-        return;
+	if (!is_site_admin())
+		return;
 
-    /* Add our stylesheet to the page so we can make our settings look nice */
+	/* Add our stylesheet to the page so we can make our settings look nice */
 
-    $a->page['htmlhead'] .= '<link rel="stylesheet"  type="text/css" href="/addon/cookienotice/cookienotice.css" media="all" />' . "\r\n";
+	$a->page['htmlhead'] .= '<link rel="stylesheet"  type="text/css" href="/addon/cookienotice/cookienotice.css" media="all" />' . "\r\n";
 
 
-    $text = Config::get('cookienotice', 'text');
-    if (!$text) {
-        $text = '';
-    }
-    $oktext = Config::get('cookienotice', 'oktext');
-    if (!$oktext) {
-        $oktext = '';
-    }
+	$text = Config::get('cookienotice', 'text');
+	if (!$text) {
+		$text = '';
+	}
+	$oktext = Config::get('cookienotice', 'oktext');
+	if (!$oktext) {
+		$oktext = '';
+	}
 
-    $t = get_markup_template("settings.tpl", "addon/cookienotice/");
-    $s .= replace_macros($t, [
-        '$title'       => L10n::t('"cookienotice" Settings'),
-        '$description' => L10n::t('<b>Configure your cookie usage notice.</b> It should just be a notice, saying that the website uses cookies. It is shown as long as a user didnt confirm clicking the OK button.'),
-        '$text'        => ['cookienotice-text', L10n::t('Cookie Usage Notice'), $text, L10n::t('The cookie usage notice')],
-        '$oktext'      => ['cookienotice-oktext', L10n::t('OK Button Text'), $oktext, L10n::t('The OK Button text')],
-        '$submit'      => L10n::t('Save Settings')
-    ]);
+	$t = get_markup_template("settings.tpl", "addon/cookienotice/");
+	$s .= replace_macros($t, [
+		'$title'	   => L10n::t('"cookienotice" Settings'),
+		'$description' => L10n::t('<b>Configure your cookie usage notice.</b> It should just be a notice, saying that the website uses cookies. It is shown as long as a user didnt confirm clicking the OK button.'),
+		'$text'		=> ['cookienotice-text', L10n::t('Cookie Usage Notice'), $text, L10n::t('The cookie usage notice')],
+		'$oktext'	  => ['cookienotice-oktext', L10n::t('OK Button Text'), $oktext, L10n::t('The OK Button text')],
+		'$submit'	  => L10n::t('Save Settings')
+	]);
 
-    return;
+	return;
 }
 
 function cookienotice_addon_settings_post(&$a, &$b)
 {
+	if (!is_site_admin())
+		return;
 
-    if (!is_site_admin())
-        return;
-
-    if ($_POST['cookienotice-submit']) {
-        Config::set('cookienotice', 'text', trim(strip_tags($_POST['cookienotice-text'])));
-        Config::set('cookienotice', 'oktext', trim(strip_tags($_POST['cookienotice-oktext'])));
-        info(L10n::t('cookienotice Settings saved.') . EOL);
-    }
+	if ($_POST['cookienotice-submit']) {
+		Config::set('cookienotice', 'text', trim(strip_tags($_POST['cookienotice-text'])));
+		Config::set('cookienotice', 'oktext', trim(strip_tags($_POST['cookienotice-oktext'])));
+		info(L10n::t('cookienotice Settings saved.') . EOL);
+	}
 }
 
 /**
@@ -81,8 +80,8 @@ function cookienotice_addon_settings_post(&$a, &$b)
  */
 function cookienotice_page_content_top($a, &$b)
 {
-    $head                = file_get_contents(__DIR__ . '/templates/head.tpl');
-    $a->page['htmlhead'] .= $head;
+	$head				= file_get_contents(__DIR__ . '/templates/head.tpl');
+	$a->page['htmlhead'] .= $head;
 }
 
 /**
@@ -94,16 +93,15 @@ function cookienotice_page_content_top($a, &$b)
  */
 function cookienotice_page_end($a, &$b)
 {
+	$text   = (string) Config::get('cookienotice', 'text');
+	$oktext = (string) Config::get('cookienotice', 'oktext');
 
-    $text   = (string) Config::get('cookienotice', 'text');
-    $oktext = (string) Config::get('cookienotice', 'oktext');
+	$page_end_tpl = get_markup_template("cookienotice.tpl", "addon/cookienotice/");
 
-    $page_end_tpl = get_markup_template("cookienotice.tpl", "addon/cookienotice/");
+	$page_end = replace_macros($page_end_tpl, [
+		'$text'   => $text,
+		'$oktext' => $oktext,
+	]);
 
-    $page_end = replace_macros($page_end_tpl, [
-        '$text'   => $text,
-        '$oktext' => $oktext,
-    ]);
-
-    $b .= $page_end;
+	$b .= $page_end;
 }
--- cookienotice/templates/settings.tpl
+++ cookienotice/templates/settings.tpl
@@ -5,9 +5,9 @@
 	<span class="fakelink" onclick="openClose('settings_cookienotice_expanded'); openClose('settings_cookienotice_inflated');">
 		<h3>{{$title}}</h3>
 	</span>
-        <p>{{$description}}</p>
+		<p>{{$description}}</p>
 	{{include file="field_textarea.tpl" field=$text}}
-        {{include file="field_input.tpl" field=$oktext}}
+		{{include file="field_input.tpl" field=$oktext}}
 	<div class="settings-submit-wrapper" >
 		<input type="submit" id="cookienotice-submit" name="cookienotice-submit" class="settings-submit" value="{{$submit}}" />
 	</div>
