567d819810f4073eade0d99432c805f6289d860b
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-02-02 08:32:11 +0100

    added ZH-CN translation of cookiemessage addon

---
+++ cookienotice/lang/zh-cn/strings.php
@@ -0,0 +1,19 @@
+<?php
+
+if(! function_exists("string_plural_select_zh_cn")) {
+function string_plural_select_zh_cn($n){
+	$n = intval($n);
+	return 0;;
+}}
+;
+$a->strings["This website uses cookies. If you continue browsing this website, you agree to the usage of cookies."] = "本网站将使用 cookies。如果您继续浏览本网站，即表示您同意使用 cookies。";
+$a->strings["OK"] = "好的";
+$a->strings["\"cookienotice\" Settings"] = "“cookie 布告”设置";
+$a->strings["<b>Configure your cookie usage notice.</b> It should just be a notice, saying that the website uses cookies. It is shown as long as a user didnt confirm clicking the OK button."] = "<b>配置您的 cookie 使用布告。</b> 它将会显示一条布告称此网站使用 cookies 。它将会一直显示直到用户点击确定按钮。";
+$a->strings["Cookie Usage Notice"] = "Cookie 使用布告";
+$a->strings["The cookie usage notice"] = "cookie 使用布告";
+$a->strings["OK Button Text"] = "确认按钮文本";
+$a->strings["The OK Button text"] = "确认按钮文本";
+$a->strings["Save Settings"] = "保存设置";
+$a->strings["cookienotice Settings saved."] = "cookies 布告设置已保存";
+$a->strings["This website uses cookies to recognize revisiting and logged in users. You accept the usage of these cookies by continue browsing this website."] = "本网站使用 cookies 来识别登录和重访的用户。如果您继续浏览本网站即表示您接受使用上述 cookies。";
