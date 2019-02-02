7d5e65324327cffc285f03b3e40f75606da00d58
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-02-02 08:34:09 +0100

    cookienotice CS translation THX JeroenED

--- cookienotice/lang/nl/strings.php
+++ cookienotice/lang/nl/strings.php
@@ -6,6 +6,8 @@ function string_plural_select_nl($n){
 	return ($n != 1);;
 }}
 ;
+$a->strings["This website uses cookies. If you continue browsing this website, you agree to the usage of cookies."] = "";
+$a->strings["OK"] = "";
 $a->strings["\"cookienotice\" Settings"] = "";
 $a->strings["<b>Configure your cookie usage notice.</b> It should just be a notice, saying that the website uses cookies. It is shown as long as a user didnt confirm clicking the OK button."] = "";
 $a->strings["Cookie Usage Notice"] = "";
@@ -14,3 +16,4 @@ $a->strings["OK Button Text"] = "OK knop tekst";
 $a->strings["The OK Button text"] = "";
 $a->strings["Save Settings"] = "";
 $a->strings["cookienotice Settings saved."] = "";
+$a->strings["This website uses cookies to recognize revisiting and logged in users. You accept the usage of these cookies by continue browsing this website."] = "";
