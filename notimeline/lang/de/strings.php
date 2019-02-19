b7f9b4cc5554db7dfd0f59b6333b52bbd2dff154
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-02-19 07:07:03 +0100

    DE translation of notimeline updated THX rompe

--- notimeline/lang/de/strings.php
+++ notimeline/lang/de/strings.php
@@ -2,10 +2,11 @@
 
 if(! function_exists("string_plural_select_de")) {
 function string_plural_select_de($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
 $a->strings["No Timeline settings updated."] = "Keine Timeline-Einstellungen aktualisiert.";
 $a->strings["No Timeline Settings"] = "Keine Timeline-Einstellungen";
-$a->strings["Disable Archive selector on profile wall"] = "Deaktiviere Archiv-Auswahl auf Deiner Pinnwand";
+$a->strings["Disable Archive selector on profile wall"] = "Deaktiviere Archiv-Auswahl auf deiner Pinnwand";
 $a->strings["Submit"] = "Senden";
