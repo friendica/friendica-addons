20feae4610801f13b76e25a46528f95faeb581fc
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-03-11 18:08:37 +0100

    piwik addon update EN_GB translation THX Andy H3

--- piwik/lang/en-gb/strings.php
+++ piwik/lang/en-gb/strings.php
@@ -2,14 +2,15 @@
 
 if(! function_exists("string_plural_select_en_gb")) {
 function string_plural_select_en_gb($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
-$a->strings["This website is tracked using the <a href='http://www.piwik.org'>Piwik</a> analytics tool."] = "Traffic on this website is tracked using the <a href='http://www.piwik.org'>Piwik</a> analytics tool.";
-$a->strings["If you do not want that your visits are logged in this way you <a href='%s'>can set a cookie to prevent Piwik from tracking further visits of the site</a> (opt-out)."] = "If you do not want your visits to be logged in this way, you <a href='%s'>can set a cookie to prevent Piwik from tracking your future visits to this site</a> (opt-out).";
+$a->strings["This website is tracked using the <a href='http://www.matomo.org'>Matomo</a> analytics tool."] = "This website is tracking, using the <a href='http://www.matomo.org'>Matomo</a> analytics tool.";
+$a->strings["If you do not want that your visits are logged in this way you <a href='%s'>can set a cookie to prevent Matomo / Piwik from tracking further visits of the site</a> (opt-out)."] = "If you do not want that your visits logged in this way you <a href='%s'>can set a cookie to prevent Matomo / Piwik from tracking further visits of the site</a> (opt-out).";
 $a->strings["Save Settings"] = "Save settings";
-$a->strings["Piwik Base URL"] = "Piwik base URL";
-$a->strings["Absolute path to your Piwik installation. (without protocol (http/s), with trailing slash)"] = "Absolute path to your Piwik installation. (without the protocol (http or https), but with the trailing slash(/) )";
+$a->strings["Matomo (Piwik) Base URL"] = "Matomo (Piwik) Base URL";
+$a->strings["Absolute path to your Matomo (Piwik) installation. (without protocol (http/s), with trailing slash)"] = "Absolute path to your Matomo (Piwik) installation. (without protocol (http/s), with trailing slash)";
 $a->strings["Site ID"] = "Site ID";
 $a->strings["Show opt-out cookie link?"] = "Show opt-out cookie link?";
 $a->strings["Asynchronous tracking"] = "Asynchronous tracking";
