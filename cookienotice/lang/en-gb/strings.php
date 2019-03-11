b1efac7321497f985a3e165b1662f544fc36acec
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-03-11 18:08:10 +0100

    cookienotice addon EN GB translation THX Andy H3

---
+++ cookienotice/lang/en-gb/strings.php
@@ -0,0 +1,19 @@
+<?php
+
+if(! function_exists("string_plural_select_en_gb")) {
+function string_plural_select_en_gb($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["This website uses cookies. If you continue browsing this website, you agree to the usage of cookies."] = "This website uses cookies. If you continue browsing this website, you agree to the usage of cookies.";
+$a->strings["OK"] = "OK";
+$a->strings["\"cookienotice\" Settings"] = "\"cookienotice\" settings";
+$a->strings["<b>Configure your cookie usage notice.</b> It should just be a notice, saying that the website uses cookies. It is shown as long as a user didnt confirm clicking the OK button."] = "<b>Configure your cookie usage notice.</b> It should just be a notice, saying that the website uses cookies. It is shown as long as a user didnt confirm clicking the OK button.";
+$a->strings["Cookie Usage Notice"] = "Cookie Usage Notice";
+$a->strings["The cookie usage notice"] = "The cookie usage notice";
+$a->strings["OK Button Text"] = "";
+$a->strings["The OK Button text"] = "";
+$a->strings["Save Settings"] = "";
+$a->strings["cookienotice Settings saved."] = "";
+$a->strings["This website uses cookies to recognize revisiting and logged in users. You accept the usage of these cookies by continue browsing this website."] = "";
