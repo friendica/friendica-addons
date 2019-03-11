335d142a16fb95ca52240ba308574c2fa9e719e0
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-03-11 18:20:29 +0100

    added cookienotice IT translation THX fabrixxm

---
+++ cookienotice/lang/it/strings.php
@@ -0,0 +1,19 @@
+<?php
+
+if(! function_exists("string_plural_select_it")) {
+function string_plural_select_it($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["This website uses cookies. If you continue browsing this website, you agree to the usage of cookies."] = "Questo sito web utilizza cookie. Proseguendo la navigazione su questo sito, accetti l'utilizzo dei cookie.";
+$a->strings["OK"] = "OK";
+$a->strings["\"cookienotice\" Settings"] = "Impostazioni \"cookienotice\"";
+$a->strings["<b>Configure your cookie usage notice.</b> It should just be a notice, saying that the website uses cookies. It is shown as long as a user didnt confirm clicking the OK button."] = "<b>Configura la tua notifica di utilizzo dei cookie</b> Dovrebbe essere semplicemente una nota che dice che il sito utilizza dei cookie. Viene mostrata fino a quanto l'utente conferma cliccando il bottone OK.";
+$a->strings["Cookie Usage Notice"] = "Nota Utilizzo Cookie";
+$a->strings["The cookie usage notice"] = "La nota di utilizzo dei cookie";
+$a->strings["OK Button Text"] = "Testo Bottone OK";
+$a->strings["The OK Button text"] = "Il testo del bottone OK";
+$a->strings["Save Settings"] = "Salva Impostazioni";
+$a->strings["cookienotice Settings saved."] = "Impostazioni \"cookienotice\" salvate.";
+$a->strings["This website uses cookies to recognize revisiting and logged in users. You accept the usage of these cookies by continue browsing this website."] = "Questo sito utilizza cookie per riconosce gli utenti loggati e quelli che tornano a visitare. Proseguendo la navigazione su questo sito, accetti l'utilizzo di questi cookie. ";
