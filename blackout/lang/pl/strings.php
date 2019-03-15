5d207f0b163a213c5d142bedf7f1461566baef02
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-03-15 09:52:16 +0100

    PL translation update blackout THX waldis

--- blackout/lang/pl/strings.php
+++ blackout/lang/pl/strings.php
@@ -6,10 +6,12 @@ function string_plural_select_pl($n){
 	return ($n==1 ? 0 : ($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14) ? 1 : $n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14) ? 2 : 3);;
 }}
 ;
+$a->strings["The end-date is prior to the start-date of the blackout, you should fix this"] = "Data zakończenia jest przed datą rozpoczęcia, powinieneś to naprawić";
+$a->strings["Please double check that the current settings for the blackout. Begin will be <strong>%s</strong> and it will end <strong>%s</strong>."] = "";
 $a->strings["Save Settings"] = "Zapisz ustawienia";
 $a->strings["Redirect URL"] = "Przekierowanie URL";
 $a->strings["all your visitors from the web will be redirected to this URL"] = "wszyscy Twoi goście z internetu zostaną przekierowani na ten adres URL";
 $a->strings["Begin of the Blackout"] = "Rozpocznij Blackout";
-$a->strings["format is <em>YYYY</em> year, <em>MM</em> month, <em>DD</em> day, <em>hh</em> hour and <em>mm</em> minute"] = "Format to <em>RRRR</em> rok, <em>MM</em> miesiąc, <em>DD</em> dzień, <em>hh</em>godzina i <em>mm</em> minuty";
+$a->strings["Format is <tt>YYYY-MM-DD hh:mm</tt>; <em>YYYY</em> year, <em>MM</em> month, <em>DD</em> day, <em>hh</em> hour and <em>mm</em> minute."] = "";
 $a->strings["End of the Blackout"] = "Koniec Blackout";
-$a->strings["The end-date is prior to the start-date of the blackout, you should fix this."] = "Data końcowa jest wcześniejsza niż data rozpoczęcia blackoutu, powinieneś to naprawić.";
+$a->strings["<strong>Note</strong>: The redirect will be active from the moment you press the submit button. Users currently logged in will <strong>not</strong> be thrown out but can't login again after logging out should the blackout is still in place."] = "";
