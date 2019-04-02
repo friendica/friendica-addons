4754b94a44e002ae86624106681a57021ed45f62
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-04-02 06:46:55 +0200

    fromgplus addon deprecated

--- fromgplus/lang/pt-br/strings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$a->strings["Google+ Import Settings"] = "Configurações de importação do Google+";
-$a->strings["Enable Google+ Import"] = "Habilitar a importação do Google+";
-$a->strings["Google Account ID"] = "ID da conta do Google";
-$a->strings["Submit"] = "Enviar";
-$a->strings["Google+ Import Settings saved."] = "As configurações de importação do Google+ foram salvas.";
--- fromgplus/lang/zh-cn/strings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$a->strings["Google+ Import Settings"] = "Google+进口设置";
-$a->strings["Enable Google+ Import"] = "使Google+进口可用";
-$a->strings["Google Account ID"] = "Google+用户名";
-$a->strings["Submit"] = "提交";
-$a->strings["Google+ Import Settings saved."] = "把Google+进口设置保存了";
