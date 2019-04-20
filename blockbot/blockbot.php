34fc60be77c82413559d6b4f823a2e16fc219fb7
Philipp Holzer <admin@philipp.info>
2019-04-20 20:38:32 +0200

    Rename botdetection to blockbot Adding composer/vendor to blockbot

--- blockbot/blockbot.php
+++ blockbot/blockbot.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Name: botdetection
+ * Name: blockbot
  * Description: Blocking bots based on detecting bots/crawlers/spiders via the user agent and http_from header.
  * Version: 0.1
  * Author: Philipp Holzer <admin@philipp.info>
@@ -12,19 +12,19 @@ use Friendica\Core\Hook;
 use Friendica\Core\System;
 use Jaybizzle\CrawlerDetect\CrawlerDetect;
 
-function botdetection_install() {
-	Hook::register('init_1', 'addon/botdetection/botdetection.php', 'botdetection_init_1');
+function blockbot_install() {
+	Hook::register('init_1', 'addon/blockbot/blockbot.php', 'blockbot_init_1');
 }
 
 
-function botdetection_uninstall() {
-	Hook::unregister('init_1', 'addon/botdetection/botdetection.php', 'botdetection_init_1');
+function blockbot_uninstall() {
+	Hook::unregister('init_1', 'addon/blockbot/blockbot.php', 'blockbot_init_1');
 }
 
-function botdetection_init_1(App $a) {
+function blockbot_init_1(App $a) {
 	$crawlerDetect = new CrawlerDetect();
 
 	if ($crawlerDetect->isCrawler()) {
-		System::httpExit(404, 'Bots are not allowed');
+		System::httpExit(403, 'Bots are not allowed');
 	}
 }
