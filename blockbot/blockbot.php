758a36dfc6989afb2b959e51c63bc87db8b4db05
Michael Vogel <icarus@dabo.de>
2019-04-27 17:17:11 +0200

    Rearranged user-agent list, tested against log data

--- blockbot/blockbot.php
+++ blockbot/blockbot.php
@@ -35,39 +35,34 @@ function blockbot_init_1(App $a) {
 	}
 
 	// List of strings of reported false positives
-	$agents = ['Mastodon', 'hackney', 'Faraday', 'okhttp', 'UniversalFeedParser', 'PixelFedBot', 'python-requests',
-		'WordPress', 'http.rb'];
+	$agents = ['hackney/', 'Faraday v', 'okhttp', 'UniversalFeedParser', 'PixelFedBot', 'python-requests',
+		'WordPress/', 'http.rb/'];
 	foreach ($agents as $agent) {
 		if (stristr($_SERVER['HTTP_USER_AGENT'], $agent)) {
 			// The agents had been reported to https://github.com/JayBizzle/Crawler-Detect/issues/
-			logger::notice('Already reported wrong detection', $logdata);
+			logger::notice('Reported false positive', $logdata);
 			return;
 		}
 	}
 
-	// List of strings of known "good" agents
-	$agents = ['diaspora-connection-tester', 'DiasporaFederation', 'Friendica', '(compatible; zot)',
-		'Micro.blog', 'GangGo', 'python/federation', 'GNU social', 'winHttp',
-		'Go-http-client', 'Mr.4x3 Powered', 'Test Certificate Info', 'WordPress.com', 'zgrab',
-		'curl/', 'StatusNet', 'OpenGraphReader/', 'Uptimebot/', 'python-opengraph-jaywink',
-		'fediverse.network crawler', 'Active_Pods_CheckBot_3.0', 'Social-Relay'];
+	// List of false positives' strings of known "good" agents we haven't reported (yet)
+	$agents = ['fediverse.network crawler', 'Active_Pods_CheckBot_3.0', 'Social-Relay/',
+		'curl', 'zgrab', 'Go-http-client', 'curb'];
 
 	foreach ($agents as $agent) {
 		if (stristr($_SERVER['HTTP_USER_AGENT'], $agent)) {
-			// Report every false positive here: https://github.com/JayBizzle/Crawler-Detect/issues/
-			// After report move it into the array above
-			logger::notice('False positive', $logdata);
+			logger::notice('Unreported falsely detected agent', $logdata);
 			return;
 		}
 	}
 
 	// List of known crawlers. They are added here to avoid having them logged at the end of the function.
-	// This helps to detect false positives
-	$agents = ['Mozilla/5.0 (compatible; SemrushBot/3~bl; +http://www.semrush.com/bot.html)', 'SEMrushBot',
-		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 AppEngine-Google; (+http://code.google.com/appengine; appid: s~feedly-nikon3)'];
+	// This helps to detect false positives.
+	$agents = ['SEMrushBot', 's~feedly-nikon3', 'Qwantify/Bleriot/', 'ltx71', 'Sogou web spider/',
+		'Diffbot/'];
 
 	foreach ($agents as $agent) {
-		if ($_SERVER['HTTP_USER_AGENT'] == $agent) {
+		if (stristr($_SERVER['HTTP_USER_AGENT'], $agent)) {
 			System::httpExit(403, 'Bots are not allowed');
 		}
 	}
