6646d0f963b7dcec93f1903b54238bf76fb21440
Michael <heluecht@pirati.ca>
2019-06-11 10:12:39 +0000

    One good, many bad ...

--- blockbot/blockbot.php
+++ blockbot/blockbot.php
@@ -43,7 +43,8 @@ function blockbot_init_1(App $a) {
 		'MegaIndex.ru/', 'SMUrlExpander', 'Hatena-Favicon/', 'Wappalyzer', 'FlipboardProxy/',
 		'NetcraftSurveyAgent/', 'Dataprovider.com', 'SMTBot/', 'Nimbostratus-Bot/',
 		'DuckDuckGo-Favicons-Bot/', 'IndieWebCards/', 'proximic', 'netEstate NE Crawler',
-		'AhrefsBot/', 'YandexBot/'];
+		'AhrefsBot/', 'YandexBot/', 'Exabot/', 'Mediumbot-MetaTagFetcher/',
+		'WhatsApp/', 'TelegramBot'];
 
 	foreach ($agents as $agent) {
 		if (stristr($_SERVER['HTTP_USER_AGENT'], $agent)) {
@@ -69,7 +70,7 @@ function blockbot_init_1(App $a) {
 		'Python-urllib/', 'Liferea/', 'aiohttp/', 'WordPress.com Reader', 'hackney/',
 		'Faraday v', 'okhttp', 'UniversalFeedParser', 'PixelFedBot', 'python-requests',
 		'WordPress/', 'http.rb/', 'Apache-HttpClient/', 'WordPress.com;', 'Pleroma',
-		'Dispatch/', 'Ruby', 'Uptimebot/'];
+		'Dispatch/', 'Ruby', 'Uptimebot/', 'Java/'];
 
 	foreach ($agents as $agent) {
 		if (stristr($_SERVER['HTTP_USER_AGENT'], $agent)) {
