5d8e4bf73d5b86a100e79dbc86d41de2c6f207bf
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-10-14 07:31:05 +0200

    buffer addon added CA translation THX obiolscat

---
+++ buffer/lang/ca/strings.php
@@ -0,0 +1,22 @@
+<?php
+
+if(! function_exists("string_plural_select_ca")) {
+function string_plural_select_ca($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Permission denied."] = "";
+$a->strings["Save Settings"] = "";
+$a->strings["Client ID"] = "";
+$a->strings["Client Secret"] = "";
+$a->strings["Error when registering buffer connection:"] = "";
+$a->strings["You are now authenticated to buffer. "] = "";
+$a->strings["return to the connector page"] = "";
+$a->strings["Post to Buffer"] = "";
+$a->strings["Buffer Export"] = "Exportació de buffer";
+$a->strings["Authenticate your Buffer connection"] = "";
+$a->strings["Enable Buffer Post Addon"] = "";
+$a->strings["Post to Buffer by default"] = "";
+$a->strings["Check to delete this preset"] = "";
+$a->strings["Posts are going to all accounts that are enabled by default:"] = "";
