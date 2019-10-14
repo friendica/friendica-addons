030711f11e24536c197a4d3f8f42826a6a21e90c
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-10-14 07:32:19 +0200

    group_text addon CA translation updated THX obiolscat

--- group_text/lang/ca/strings.php
+++ group_text/lang/ca/strings.php
@@ -1,6 +1,12 @@
 <?php
 
-$a->strings["Group Text settings updated."] = "";
-$a->strings["Group Text"] = "";
-$a->strings["Use a text only (non-image) group selector in the \"group edit\" menu"] = "";
-$a->strings["Submit"] = "Enviar";
+if(! function_exists("string_plural_select_ca")) {
+function string_plural_select_ca($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Group Text settings updated."] = "La configuració del text del grup s'ha actualitzat.";
+$a->strings["Group Text"] = "Missatge del grup";
+$a->strings["Use a text only (non-image) group selector in the \"group edit\" menu"] = "Utilitzeu un selector de grup de només text (que no sigui una imatge) al menú 'Edita grup'";
+$a->strings["Submit"] = "sotmetre's";
