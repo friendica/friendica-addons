6e2f4ed949a7f2da448bc413c445ceeb086de6b8
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-11-06 09:33:35 +0100

    ijpost addon updated CA translation THX Joan Bar

--- ijpost/lang/ca/strings.php
+++ ijpost/lang/ca/strings.php
@@ -1,9 +1,15 @@
 <?php
 
-$a->strings["Post to Insanejournal"] = "Enviament a Insanejournal";
-$a->strings["InsaneJournal Post Settings"] = "Ajustos d'Enviament a Insanejournal";
-$a->strings["Enable InsaneJournal Post Addon"] = "Habilita el Addon d'Enviaments a Insanejournal";
-$a->strings["InsaneJournal username"] = "Nom d'usuari de Insanejournal";
-$a->strings["InsaneJournal password"] = "Contrasenya de Insanejournal";
-$a->strings["Post to InsaneJournal by default"] = "Enviar per defecte a Insanejournal";
-$a->strings["Submit"] = "Enviar";
+if(! function_exists("string_plural_select_ca")) {
+function string_plural_select_ca($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Post to Insanejournal"] = "Enviar a Insanejournal";
+$a->strings["InsaneJournal Post Settings"] = "Configuració de la publicació de InsaneJournal";
+$a->strings["Enable InsaneJournal Post Addon"] = "Activa la secció Addon InsaneJournal";
+$a->strings["InsaneJournal username"] = "Nom d'usuari de InsaneJournal";
+$a->strings["InsaneJournal password"] = "Contrasenya de InsaneJournal";
+$a->strings["Post to InsaneJournal by default"] = "Publica-ho de manera predeterminada a InsaneJournal";
+$a->strings["Submit"] = "sotmetre's";
