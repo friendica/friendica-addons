807ad657bfacf0ec1f9027e4e4b04750ae86808e
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-11-06 09:38:09 +0100

    dwpost addon updated CA translation THX Joan Bar

--- dwpost/lang/ca/strings.php
+++ dwpost/lang/ca/strings.php
@@ -1,9 +1,15 @@
 <?php
 
-$a->strings["Post to Dreamwidth"] = "Missatge a Dreamwidth";
-$a->strings["Dreamwidth Post Settings"] = "Configuració d'enviaments a Dreamwidth";
-$a->strings["Enable dreamwidth Post Addon"] = "Habilitat el addon d'enviaments a Dreamwidth";
-$a->strings["dreamwidth username"] = "Nom d'usuari a Dreamwidth";
-$a->strings["dreamwidth password"] = "Contrasenya a Dreamwidth";
-$a->strings["Post to dreamwidth by default"] = "Enviar per defecte a Dreamwidth";
-$a->strings["Submit"] = "Enviar";
+if(! function_exists("string_plural_select_ca")) {
+function string_plural_select_ca($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Post to Dreamwidth"] = "Publica a Dreamwidth";
+$a->strings["Dreamwidth Post Settings"] = "Configuració de la publicació de Dreamwidth";
+$a->strings["Enable dreamwidth Post Addon"] = "Habilita Addon Post Post";
+$a->strings["dreamwidth username"] = "nom d'usuari de dreamwidth";
+$a->strings["dreamwidth password"] = "contrasenya de dreamwidth";
+$a->strings["Post to dreamwidth by default"] = "Publica l'amplada de somni de manera predeterminada";
+$a->strings["Submit"] = "sotmetre's";
