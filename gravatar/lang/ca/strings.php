9561b4092600f57c2b5a4048815f42d19ed1a704
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-11-06 08:30:50 +0100

    gravatar addon updated CA translation THX Joan Bar

--- gravatar/lang/ca/strings.php
+++ gravatar/lang/ca/strings.php
@@ -1,15 +1,21 @@
 <?php
 
-$a->strings["generic profile image"] = "imatge de perfil genérica";
-$a->strings["random geometric pattern"] = "Patró geometric aleatori";
-$a->strings["monster face"] = "Cara monstruosa";
-$a->strings["computer generated face"] = "Cara monstruosa generada per ordinador";
-$a->strings["retro arcade style face"] = "Cara d'estil arcade retro";
-$a->strings["Information"] = "informació";
-$a->strings["Libravatar addon is installed, too. Please disable Libravatar addon or this Gravatar addon.<br>The Libravatar addon will fall back to Gravatar if nothing was found at Libravatar."] = "";
-$a->strings["Submit"] = "Enviar";
-$a->strings["Default avatar image"] = "Imatge d'avatar per defecte";
-$a->strings["Select default avatar image if none was found at Gravatar. See README"] = "Se selecciona  la imatge d'avatar per defecte si no es troba cap en Gravatar. Veure el README";
-$a->strings["Rating of images"] = "Classificació de les imatges";
-$a->strings["Select the appropriate avatar rating for your site. See README"] = "Selecciona la classe d'avatar apropiat pel teu lloc. Veure el README";
-$a->strings["Gravatar settings updated."] = "Ajustos de Gravatar actualitzats.";
+if(! function_exists("string_plural_select_ca")) {
+function string_plural_select_ca($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["generic profile image"] = "imatge de perfil genèrica";
+$a->strings["random geometric pattern"] = "patró geomètric aleatori";
+$a->strings["monster face"] = "cara de monstre";
+$a->strings["computer generated face"] = "cara generada per ordinador";
+$a->strings["retro arcade style face"] = "cara d’estil d’arcades retro";
+$a->strings["Information"] = "Informació";
+$a->strings["Libravatar addon is installed, too. Please disable Libravatar addon or this Gravatar addon.<br>The Libravatar addon will fall back to Gravatar if nothing was found at Libravatar."] = "També hi ha instal·lat l’addon Libravatar. Inhabiliteu l'addon Libravatar o aquest addon Gravatar.<br>Si no es va trobar res a Libravatar, l'afegit de Libravatar tornarà a aparèixer a Gravatar.";
+$a->strings["Submit"] = "Presentar";
+$a->strings["Default avatar image"] = "Imatge predeterminada d’avatar";
+$a->strings["Select default avatar image if none was found at Gravatar. See README"] = "Seleccioneu la imatge d'avatar per defecte si no s'ha trobat cap a Gravatar. Vegeu LLEGIR";
+$a->strings["Rating of images"] = "Valoració d'imatges";
+$a->strings["Select the appropriate avatar rating for your site. See README"] = "Seleccioneu la qualificació d'avatar adequada per al vostre lloc. Vegeu LLEGIR";
+$a->strings["Gravatar settings updated."] = "S'han actualitzat els paràmetres de Gravatar.";
