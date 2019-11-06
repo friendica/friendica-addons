6944b54ff7275c3b82e9ffc5d967e8b655564b25
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-11-06 09:39:30 +0100

    impressum addon updated CA translation THX Joan Bar

--- impressum/lang/ca/strings.php
+++ impressum/lang/ca/strings.php
@@ -1,18 +1,24 @@
 <?php
 
-$a->strings["Impressum"] = "Impressum";
+if(! function_exists("string_plural_select_ca")) {
+function string_plural_select_ca($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Impressum"] = "empremta";
 $a->strings["Site Owner"] = "Propietari del lloc";
-$a->strings["Email Address"] = "Adreça de correu";
+$a->strings["Email Address"] = "Correu electrònic";
 $a->strings["Postal Address"] = "Adreça postal";
-$a->strings["The impressum addon needs to be configured!<br />Please add at least the <tt>owner</tt> variable to your config file. For other variables please refer to the README file of the addon."] = "El complement impressum s'ha de configurar!<br />Si us plau afegiu almenys la variable <tt>propietari </tt> al fitxer de configuració. Per a les altres variables, consulteu el fitxer README del complement.";
-$a->strings["Settings updated."] = "Ajustos actualitzats.";
-$a->strings["Submit"] = "Enviar";
-$a->strings["The page operators name."] = "Nom de la pàgina del gestor.";
-$a->strings["Site Owners Profile"] = "Perfil del Propietari del Lloc";
-$a->strings["Profile address of the operator."] = "Adreça del perfil del gestor.";
-$a->strings["How to contact the operator via snail mail. You can use BBCode here."] = "Com posar-se en contacte amb l'operador a través de correu postal. Vostè pot utilitzar BBCode aquí.";
-$a->strings["Notes"] = "Notes";
-$a->strings["Additional notes that are displayed beneath the contact information. You can use BBCode here."] = "Notes addicionals que es mostren sota de la informació de contacte. Vostè pot usar BBCode aquí.";
-$a->strings["How to contact the operator via email. (will be displayed obfuscated)"] = "Com contactar amb el gestor via correu electronic. ( es visualitzara ofuscat)";
-$a->strings["Footer note"] = "Nota a peu de pàgina";
-$a->strings["Text for the footer. You can use BBCode here."] = "Text pel peu de pàgina. Pots emprar BBCode aquí.";
+$a->strings["The impressum addon needs to be configured!<br />Please add at least the <tt>owner</tt> variable to your config file. For other variables please refer to the README file of the addon."] = "Cal configurar l’addon impressum<br />Afegiu com a mínim la secció <tt>owner</tt> variable del fitxer de configuració. Per a altres variables, consulteu el fitxer README de l’adjunció.";
+$a->strings["Settings updated."] = "La configuració s'ha actualitzat.";
+$a->strings["Submit"] = "sotmetre's";
+$a->strings["The page operators name."] = "El nom dels operadors de pàgina.";
+$a->strings["Site Owners Profile"] = "Perfil dels propietaris del lloc";
+$a->strings["Profile address of the operator."] = "Adreça del perfil de l'operador.";
+$a->strings["How to contact the operator via snail mail. You can use BBCode here."] = "Com contactar amb l'operador mitjançant correu cargol. Podeu utilitzar BBCode aquí.";
+$a->strings["Notes"] = "nota";
+$a->strings["Additional notes that are displayed beneath the contact information. You can use BBCode here."] = "Notes addicionals que es mostren a sota de la informació de contacte. Podeu utilitzar BBCode aquí.";
+$a->strings["How to contact the operator via email. (will be displayed obfuscated)"] = "Com contactar amb l'operador per correu electrònic. (es mostrarà ofuscat)";
+$a->strings["Footer note"] = "Nota de peu de pàgina";
+$a->strings["Text for the footer. You can use BBCode here."] = "Text for the footer. You can use BBCode here.";
