8fcd3e03da7c92636e883d19cf439b6683458134
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-11-06 08:49:54 +0100

    gnot addon updated CA translation THX Joan Bar

--- gnot/lang/ca/strings.php
+++ gnot/lang/ca/strings.php
@@ -1,8 +1,14 @@
 <?php
 
-$a->strings["Gnot settings updated."] = "Configuració de Gnot actualitzada";
-$a->strings["Gnot Settings"] = "Configuració de Gnot";
-$a->strings["Allows threading of email comment notifications on Gmail and anonymising the subject line."] = "Permet crear fils de les notificacions de comentaris de correu electrònic a Gmail i anonimat de la línia d'assumpte.";
-$a->strings["Enable this addon?"] = "Activar aquest addon/aplicació?";
-$a->strings["Submit"] = "Enviar";
-$a->strings["[Friendica:Notify] Comment to conversation #%d"] = "[Friendica: Notifica] Conversació comentada #%d";
+if(! function_exists("string_plural_select_ca")) {
+function string_plural_select_ca($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Gnot settings updated."] = "S'ha actualitzat la configuració de Gnot.";
+$a->strings["Gnot Settings"] = "Configuració de gnot";
+$a->strings["Allows threading of email comment notifications on Gmail and anonymising the subject line."] = "Permet llançar les notificacions de comentaris de correu electrònic a Gmail i anonimitzar la línia de l’assumpte.";
+$a->strings["Enable this addon?"] = "Activar aquest addon?";
+$a->strings["Submit"] = "Envia";
+$a->strings["[Friendica:Notify] Comment to conversation #%d"] = "[Friendica:Notify] comenta la conversa #%d";
