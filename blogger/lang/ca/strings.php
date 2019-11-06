9481be42701dce9857f94e7c47b4ee317d8b52bf
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-11-06 08:12:57 +0100

    blogger addon updated CA translation THX Joan Bar

--- blogger/lang/ca/strings.php
+++ blogger/lang/ca/strings.php
@@ -6,11 +6,11 @@ function string_plural_select_ca($n){
 	return ($n != 1);;
 }}
 ;
-$a->strings["Post to blogger"] = "";
+$a->strings["Post to blogger"] = "Publicar a blogger";
 $a->strings["Blogger Export"] = "Exportació de Blogger";
-$a->strings["Enable Blogger Post Addon"] = "";
+$a->strings["Enable Blogger Post Addon"] = "Habilita Addon Post de Blogger";
 $a->strings["Blogger username"] = "Nom d'usuari de Blogger";
-$a->strings["Blogger password"] = "";
+$a->strings["Blogger password"] = "Contrasenya de Blogger";
 $a->strings["Blogger API URL"] = "URL de l'API de Blogger";
 $a->strings["Post to Blogger by default"] = "Publica a Blogger de manera predeterminada";
 $a->strings["Save Settings"] = "Desa la configuració";
