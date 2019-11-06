a294b8129311b1eef6fd975b202f29e8f4bae3fb
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-11-06 08:22:55 +0100

    blockbot addon added CA translation THX Joan Bar

---
+++ blockbot/lang/ca/strings.php
@@ -0,0 +1,13 @@
+<?php
+
+if(! function_exists("string_plural_select_ca")) {
+function string_plural_select_ca($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Save Settings"] = "Desa la configuració";
+$a->strings["Allow \"good\" crawlers"] = "Permeti rastrejadors \"bons\"";
+$a->strings["Block GabSocial"] = "Bloquejar GabSocial";
+$a->strings["Training mode"] = "Modalitat d'entrenament";
+$a->strings["Settings updated."] = "La configuració s'ha actualitzat.";
