8237e3cc692aca686b95bf4c1628d420e9227816
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-12-16 08:13:41 +0100

    smileybutton: ET translation added THX Rain Hawk

---
+++ smileybutton/lang/et/strings.php
@@ -0,0 +1,12 @@
+<?php
+
+if(! function_exists("string_plural_select_et")) {
+function string_plural_select_et($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Smileybutton settings"] = "SmileyNupu sätted";
+$a->strings["You can hide the button and show the smilies directly."] = "Saad peita nupu ja näidata smailisid otse";
+$a->strings["Hide the button"] = "Peida nupp";
+$a->strings["Save Settings"] = "Salvesta sätted";
