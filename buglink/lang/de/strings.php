cbfda200c53b2006ce17abbd066f13b41b2f5600
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2019-12-16 08:14:55 +0100

    buglink: DE translation updated THX Rene Wagner

--- buglink/lang/de/strings.php
+++ buglink/lang/de/strings.php
@@ -2,7 +2,8 @@
 
 if(! function_exists("string_plural_select_de")) {
 function string_plural_select_de($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
-$a->strings["Report Bug"] = "Fehler melden.";
+$a->strings["Report Bug"] = "Fehler melden";
