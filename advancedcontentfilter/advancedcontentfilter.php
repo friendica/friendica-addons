382ca2b3bc7d286f0276c2ad778e6939d4e739cb
nupplaPhil <admin@philipp.info>
2019-12-23 01:36:32 +0100

    Restored Hook::register and added Hook::add

--- advancedcontentfilter/advancedcontentfilter.php
+++ advancedcontentfilter/advancedcontentfilter.php
@@ -56,10 +56,11 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 
 function advancedcontentfilter_install(App $a)
 {
-	Hook::add('dbstructure_definition'          , __FILE__, 'advancedcontentfilter_dbstructure_definition');
+	Hook::register('dbstructure_definition'     , __FILE__, 'advancedcontentfilter_dbstructure_definition');
 	Hook::register('prepare_body_content_filter', __FILE__, 'advancedcontentfilter_prepare_body_content_filter');
 	Hook::register('addon_settings'             , __FILE__, 'advancedcontentfilter_addon_settings');
 
+	Hook::add('dbstructure_definition'          , __FILE__, 'advancedcontentfilter_dbstructure_definition');
 	DBStructure::update($a->getBasePath(), false, true);
 
 	Logger::log("installed advancedcontentfilter");
@@ -67,6 +68,7 @@ function advancedcontentfilter_install(App $a)
 
 function advancedcontentfilter_uninstall()
 {
+	Hook::unregister('dbstructure_definition'     , __FILE__, 'advancedcontentfilter_dbstructure_definition');
 	Hook::unregister('prepare_body_content_filter', __FILE__, 'advancedcontentfilter_prepare_body_content_filter');
 	Hook::unregister('addon_settings'             , __FILE__, 'advancedcontentfilter_addon_settings');
 }
