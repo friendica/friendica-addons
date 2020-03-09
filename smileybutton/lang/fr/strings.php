356269792ac7beb8cb2c9f4b564959375eded090
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-03-09 11:37:08 +0100

    smiley button FR translation THX ButterflyOfFire

---
+++ smileybutton/lang/fr/strings.php
@@ -0,0 +1,12 @@
+<?php
+
+if(! function_exists("string_plural_select_fr")) {
+function string_plural_select_fr($n){
+	$n = intval($n);
+	return ($n > 1);;
+}}
+;
+$a->strings["Smileybutton settings"] = "Paramètres du bouton des Smileys";
+$a->strings["You can hide the button and show the smilies directly."] = "";
+$a->strings["Hide the button"] = "Cacher le bouton";
+$a->strings["Save Settings"] = "";
