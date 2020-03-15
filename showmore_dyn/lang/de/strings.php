1360efc93084c4f5ad087964e2dbce156514c2f7
Christian Wiwie <jonsnow@posteo.de>
2020-03-15 21:18:29 +0100

    #8374: Removed incorrect strings.php file

--- showmore_dyn/lang/de/strings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(! function_exists("string_plural_select_de")) {
-function string_plural_select_de($n){
-        return ($n != 1);;
-}}
-;
-$a->strings["Show more ..."] = "Mehr zeigen ...";
