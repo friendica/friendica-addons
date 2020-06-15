1651dd418e0f096b245d51f850ea8aacb3991bc1
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-06-15 07:55:34 +0200

    qcomment RU translation update THX Alexander An

--- qcomment/lang/ru/strings.php
+++ qcomment/lang/ru/strings.php
@@ -1,10 +1,16 @@
 <?php
 
+if(! function_exists("string_plural_select_ru")) {
+function string_plural_select_ru($n){
+	$n = intval($n);
+	return ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14) ? 1 : $n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)? 2 : 3);;
+}}
+;
 $a->strings[":-)"] = ":-)";
 $a->strings[":-("] = ":-(";
 $a->strings["lol"] = "lol";
-$a->strings["Quick Comment Settings"] = "";
-$a->strings["Quick comments are found near comment boxes, sometimes hidden. Click them to provide simple replies."] = "";
-$a->strings["Enter quick comments, one per line"] = "Введите короткие комментарии, по одному в строке:";
-$a->strings["Submit"] = "Подтвердить";
-$a->strings["Quick Comment settings saved."] = "";
+$a->strings["Quick Comment Settings"] = "Настройки быстрых комментариев";
+$a->strings["Quick comments are found near comment boxes, sometimes hidden. Click them to provide simple replies."] = "Быстрые комментарии находятся рядом с полями комментариев, иногда они скрыты. Нажмите на них, чтобы использовать готовые ответы.";
+$a->strings["Enter quick comments, one per line"] = "Введите быстрые комментарии, по одному на строку";
+$a->strings["Submit"] = "Добавить";
+$a->strings["Quick Comment settings saved."] = "Настройки сохранены.";
