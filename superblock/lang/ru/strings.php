0734b5620d59a1def43e27cd8d0e733a2a683a9a
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-06-15 09:14:40 +0200

    superblock addon RU translation update THX Alexander An

--- superblock/lang/ru/strings.php
+++ superblock/lang/ru/strings.php
@@ -1,10 +1,14 @@
 <?php
 
-$a->strings["\"Blockem\" Settings"] = "\"Blockem\" настройки";
-$a->strings["Comma separated profile URLS to block"] = "URLS, которые заблокировать (список через запятую)";
-$a->strings["Submit"] = "Подтвердить";
-$a->strings["BLOCKEM Settings saved."] = "BLOCKEM-Настройки сохранены.";
-$a->strings["Blocked %s - Click to open/close"] = "Заблокированные %s - Нажмите, чтобы открыть/закрыть";
-$a->strings["Unblock Author"] = "";
-$a->strings["Block Author"] = "Блокировать Автора";
-$a->strings["blockem settings updated"] = "\"Blockem\" настройки обновлены";
+if(! function_exists("string_plural_select_ru")) {
+function string_plural_select_ru($n){
+	$n = intval($n);
+	return ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14) ? 1 : $n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)? 2 : 3);;
+}}
+;
+$a->strings["Superblock"] = "Superblock";
+$a->strings["Comma separated profile URLS to block"] = "Ссылки на блокируемые профили через запятую";
+$a->strings["Save Settings"] = "Сохранить настройки";
+$a->strings["SUPERBLOCK Settings saved."] = "Настройки Superblock сохранены";
+$a->strings["Block Completely"] = "Заблокировать совсем";
+$a->strings["superblock settings updated"] = "настройки superblock обновлены";
