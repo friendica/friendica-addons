abf61cd93f1e91ce627d4a71b213a5dfe8a72aba
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-06-15 08:23:22 +0200

    showmore RU translation update THX Alexander An

--- showmore/lang/ru/strings.php
+++ showmore/lang/ru/strings.php
@@ -1,8 +1,14 @@
 <?php
 
-$a->strings["\"Show more\" Settings"] = "";
-$a->strings["Enable Show More"] = "Включить Показать больше";
-$a->strings["Cutting posts after how much characters"] = "Обрезание сообщения после превывения числа символов";
-$a->strings["Submit"] = "Подтвердить";
-$a->strings["Show More Settings saved."] = "";
-$a->strings["show more"] = "показать больше";
+if(! function_exists("string_plural_select_ru")) {
+function string_plural_select_ru($n){
+	$n = intval($n);
+	return ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14) ? 1 : $n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)? 2 : 3);;
+}}
+;
+$a->strings["\"Show more\" Settings"] = "Настройки \"показать ещё\"";
+$a->strings["Enable Show More"] = "Включить \"показать ещё\"";
+$a->strings["Cutting posts after how much characters"] = "Обрезать записи после превышения этого числа символов";
+$a->strings["Save Settings"] = "Сохранить настройки";
+$a->strings["Show More Settings saved."] = "Настройки сохранены.";
+$a->strings["show more"] = "показать ещё";
