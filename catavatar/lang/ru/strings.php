2ec0eb67458a5744428240881fb0f640a095076b
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-06-15 09:12:09 +0200

    added RU translation of catavatar addon THX Alexander An

---
+++ catavatar/lang/ru/strings.php
@@ -0,0 +1,16 @@
+<?php
+
+if(! function_exists("string_plural_select_ru")) {
+function string_plural_select_ru($n){
+	$n = intval($n);
+	return ($n%10==1 && $n%100!=11 ? 0 : $n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14) ? 1 : $n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)? 2 : 3);;
+}}
+;
+$a->strings["Use Cat as Avatar"] = "Поставить кота на аватар";
+$a->strings["More Random Cat!"] = "Сгенерировать ещё котов!";
+$a->strings["Reset to email Cat"] = "Сбросить на кота по-умолчанию";
+$a->strings["Cat Avatar Settings"] = "Настройки Cat Avatar";
+$a->strings["The cat hadn't found itself."] = "Кот не нашёл сам себя.";
+$a->strings["There was an error, the cat ran away."] = "Возникла ошибка, кот убежал.";
+$a->strings["Profile Photos"] = "Фото профиля";
+$a->strings["Meow!"] = "Мяу!";
