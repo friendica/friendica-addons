dc746cc21194f48a8ce174f5428bc288cf6c5f21
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-07-01 15:49:10 +0200

    makrdown addon EN GB translation added THX AndyH3

---
+++ markdown/lang/en-gb/strings.php
@@ -0,0 +1,12 @@
+<?php
+
+if(! function_exists("string_plural_select_en_gb")) {
+function string_plural_select_en_gb($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Markdown"] = "Markdown";
+$a->strings["Enable Markdown parsing"] = "Enable Markdown parsing";
+$a->strings["If enabled, self created items will additionally be parsed via Markdown."] = "If enabled, self created items will additionally be parsed via Markdown.";
+$a->strings["Save Settings"] = "Save Settings";
