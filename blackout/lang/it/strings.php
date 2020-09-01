e1918f7d4ebb6c54cc344a78daee0fed3ae59acd
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-09-01 14:38:09 +0200

    IT translation of blackout addon updated THX silkevicious

--- blackout/lang/it/strings.php
+++ blackout/lang/it/strings.php
@@ -2,13 +2,16 @@
 
 if(! function_exists("string_plural_select_it")) {
 function string_plural_select_it($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
+$a->strings["The end-date is prior to the start-date of the blackout, you should fix this"] = "La data di fine è precedente alla data di inizio blackout, dovresti sistemarle";
+$a->strings["Please double check that the current settings for the blackout. Begin will be <strong>%s</strong> and it will end <strong>%s</strong>."] = "Per favore ricontrolla le impostazioni attuali per il blackout. L'inizio sarà il <strong>%s</strong> e terminerà il <strong>%s</strong>.";
 $a->strings["Save Settings"] = "Salva Impostazioni";
 $a->strings["Redirect URL"] = "URL di reindirizzamento";
 $a->strings["all your visitors from the web will be redirected to this URL"] = "tutti i visitatori dal web verranno reindirizzati a questo URL";
 $a->strings["Begin of the Blackout"] = "Inzio del blackout";
-$a->strings["format is <em>YYYY</em> year, <em>MM</em> month, <em>DD</em> day, <em>hh</em> hour and <em>mm</em> minute"] = "il formato è <em>YYYY</em> anno, <em>MM</em> mese, <em>DD</em> giorno, <em>hh</em> ora e <em>mm</em> minuto";
+$a->strings["Format is <tt>YYYY-MM-DD hh:mm</tt>; <em>YYYY</em> year, <em>MM</em> month, <em>DD</em> day, <em>hh</em> hour and <em>mm</em> minute."] = "Il formato è <tt>YYYY-MM-DD hh:mm</tt>; <em>YYYY</em> anno, <em>MM</em> mese, <em>DD</em> giorno, <em>hh</em> ora e <em>mm</em> minuto.";
 $a->strings["End of the Blackout"] = "Fine del blackout";
-$a->strings["The end-date is prior to the start-date of the blackout, you should fix this."] = "La data di fine è precedente alla data di inizio. Dovresti sistemarla.";
+$a->strings["<strong>Note</strong>: The redirect will be active from the moment you press the submit button. Users currently logged in will <strong>not</strong> be thrown out but can't login again after logging out should the blackout is still in place."] = "<strong>Nota</strong>: Il reindirizzamento sarà attivo dal momento in cui premerai il pulsante di invio. Gli utenti attualmente autenticati <strong>non</strong> saranno disconnessi ma non potranno accedere in caso di disconnessione fintanto che il blackout sarà attivo.";
