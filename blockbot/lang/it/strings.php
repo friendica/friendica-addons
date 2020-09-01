538a3feaec9c944194c95754d2d2419a20d196b9
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-09-01 14:40:02 +0200

    IT translation of blockbot addon added THX silkevicious

---
+++ blockbot/lang/it/strings.php
@@ -0,0 +1,13 @@
+<?php
+
+if(! function_exists("string_plural_select_it")) {
+function string_plural_select_it($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Save Settings"] = "Salva Impostazioni";
+$a->strings["Allow \"good\" crawlers"] = "Permetti crawler \"buoni\"";
+$a->strings["Block GabSocial"] = "Blocca GabSocial";
+$a->strings["Training mode"] = "Modalità addestramento";
+$a->strings["Settings updated."] = "Impostazioni aggiornate.";
