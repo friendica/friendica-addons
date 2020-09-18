a423d92649cb0eedf21abeedeb3e6c9eb0ccdbeb
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-09-18 08:04:57 +0200

    irc IT translation THX Sylke Vicious

--- irc/lang/it/strings.php
+++ irc/lang/it/strings.php
@@ -2,6 +2,7 @@
 
 if(! function_exists("string_plural_select_it")) {
 function string_plural_select_it($n){
+	$n = intval($n);
 	return ($n != 1);;
 }}
 ;
@@ -11,7 +12,7 @@ $a->strings["Save Settings"] = "Salva Impostazioni";
 $a->strings["Channel(s) to auto connect (comma separated)"] = "Canale(i) a cui autocollegarsi (separati da virgola)";
 $a->strings["List of channels that shall automatically connected to when the app is launched."] = "Lista di canali che a cui connettersi automaticamente quando l'app è avviata.";
 $a->strings["Popular Channels (comma separated)"] = "Canali popolari (separati da virgola)";
-$a->strings["List of popular channels, will be displayed at the side and hotlinked for easy joining."] = "Lista di canali popolari: sarà visualizzata a lato e provvista di link per facilitare l'adesione.";
+$a->strings["List of popular channels, will be displayed at the side and hotlinked for easy joining."] = "Lista di canali popolari: sarà visualizzata a lato e provvista di collegamento per facilitare l'adesione.";
 $a->strings["IRC settings saved."] = "Impostazioni IRC salvate.";
 $a->strings["IRC Chatroom"] = "Stanza IRC";
 $a->strings["Popular Channels"] = "Canali Popolari";
