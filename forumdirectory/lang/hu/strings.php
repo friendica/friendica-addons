81879b740e9a9015b4ab936db49b358276434112
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-12-23 21:25:41 +0100

    forumdirectory addon added HU translation THX Balázs Úr

---
+++ forumdirectory/lang/hu/strings.php
@@ -0,0 +1,23 @@
+<?php
+
+if(! function_exists("string_plural_select_hu")) {
+function string_plural_select_hu($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Forum Directory"] = "Fórumkönyvtár";
+$a->strings["Public access denied."] = "Nyilvános hozzáférés megtagadva.";
+$a->strings["Global Directory"] = "Globális könyvtár";
+$a->strings["Find on this site"] = "Keresés ezen az oldalon";
+$a->strings["Finding: "] = "Találat: ";
+$a->strings["Site Directory"] = "Oldal könyvtára";
+$a->strings["Find"] = "Keresés";
+$a->strings["Age: "] = "Életkor: ";
+$a->strings["Gender: "] = "Nem: ";
+$a->strings["Location:"] = "Hely:";
+$a->strings["Gender:"] = "Nem:";
+$a->strings["Status:"] = "Állapot:";
+$a->strings["Homepage:"] = "Honlap:";
+$a->strings["About:"] = "Névjegy:";
+$a->strings["No entries (some entries may be hidden)."] = "Nincsenek bejegyzések (néhány bejegyzés rejtve lehet).";
