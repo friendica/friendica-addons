d0894c185107156eaf39bc845f473903e1055e24
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-12-23 21:22:25 +0100

    geonames addon added HU translation THX Balázs Úr

---
+++ geonames/lang/hu/strings.php
@@ -0,0 +1,12 @@
+<?php
+
+if(! function_exists("string_plural_select_hu")) {
+function string_plural_select_hu($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Geonames settings updated."] = "A földrajzi nevek beállításai frissítve.";
+$a->strings["Geonames Settings"] = "Földrajzi nevek beállításai";
+$a->strings["Enable Geonames Addon"] = "A földrajzi nevek bővítmény engedélyezése";
+$a->strings["Submit"] = "Elküldés";
