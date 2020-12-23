bcb4dd80127b220f90a81843afd8f1a8746f5836
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2020-12-23 21:27:18 +0100

    dwpost addon added HU translation THX Balázs Úr

---
+++ dwpost/lang/hu/strings.php
@@ -0,0 +1,15 @@
+<?php
+
+if(! function_exists("string_plural_select_hu")) {
+function string_plural_select_hu($n){
+	$n = intval($n);
+	return ($n != 1);;
+}}
+;
+$a->strings["Post to Dreamwidth"] = "Beküldés a Dreamwidth-re";
+$a->strings["Dreamwidth Post Settings"] = "Dreamwidth-beküldés beállításai";
+$a->strings["Enable dreamwidth Post Addon"] = "A Dreamwidth-beküldő bővítmény engedélyezése";
+$a->strings["dreamwidth username"] = "Dreamwidth felhasználónév";
+$a->strings["dreamwidth password"] = "Dreamwidth jelszó";
+$a->strings["Post to dreamwidth by default"] = "Beküldés a Dreamwidth-re alapértelmezetten";
+$a->strings["Submit"] = "Elküldés";
