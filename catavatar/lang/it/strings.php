7f4d5118b67da6a815242feff286fa8b52fdd2ec
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-02-01 07:23:21 +0100

    IT translation for catavatar translation updated THX silkevicious

--- catavatar/lang/it/strings.php
+++ catavatar/lang/it/strings.php
@@ -2,13 +2,15 @@
 
 if(! function_exists("string_plural_select_it")) {
 function string_plural_select_it($n){
-	return ($n != 1);;
+	$n = intval($n);
+	return intval($n != 1);
 }}
 ;
 $a->strings["Use Cat as Avatar"] = "Usa il Gatto come avatar";
 $a->strings["More Random Cat!"] = "Altro Gatto a caso!";
 $a->strings["Reset to email Cat"] = "Reimposta Gatto";
 $a->strings["Cat Avatar Settings"] = "Impostazioni Avatar Gatto";
+$a->strings["Set default profile avatar or randomize the cat."] = "Imposta l'immagine di profilo predefinita o crea un gatto casuale.";
 $a->strings["The cat hadn't found itself."] = "Il gatto non ha trovato sé stesso.";
 $a->strings["There was an error, the cat ran away."] = "Si è verificato un errore, il gatto è scappato.";
 $a->strings["Profile Photos"] = "Foto del profilo";
