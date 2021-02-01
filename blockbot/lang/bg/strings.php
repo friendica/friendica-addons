65f51cbdda41858c6fcf857fc5b67954d4e63abb
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-02-01 07:01:32 +0100

    blockbot addon BG translation added THX Rafael Kalachev

---
+++ blockbot/lang/bg/strings.php
@@ -0,0 +1,13 @@
+<?php
+
+if(! function_exists("string_plural_select_bg")) {
+function string_plural_select_bg($n){
+	$n = intval($n);
+	return intval($n != 1);
+}}
+;
+$a->strings["Save Settings"] = "Запази настройките";
+$a->strings["Allow \"good\" crawlers"] = "Разреши \"добри\" обхождащи роботи за уеб страници";
+$a->strings["Block GabSocial"] = "Блокирай GabSocial";
+$a->strings["Training mode"] = "Тренировъчен режим";
+$a->strings["Settings updated."] = "Настройките са обновени";
