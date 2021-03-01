5029d3b486bda19edb48b88c5ce6e4040d1ba344
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-01 08:03:01 +0100

    DE translation forumdirectory addon updated

--- forumdirectory/lang/de/strings.php
+++ forumdirectory/lang/de/strings.php
@@ -8,16 +8,8 @@ function string_plural_select_de($n){
 ;
 $a->strings["Forum Directory"] = "Foren Verzeichnis";
 $a->strings["Public access denied."] = "Öffentlicher Zugriff verweigert.";
+$a->strings["No entries (some entries may be hidden)."] = "Keine Einträge (einige Einträge könnten versteckt sein).";
 $a->strings["Global Directory"] = "Weltweites Verzeichnis";
 $a->strings["Find on this site"] = "Auf diesem Server suchen";
-$a->strings["Finding: "] = "Funde: ";
-$a->strings["Site Directory"] = "Verzeichnis";
+$a->strings["Results for:"] = "Ergebnis für:";
 $a->strings["Find"] = "Finde";
-$a->strings["Age: "] = "Alter: ";
-$a->strings["Gender: "] = "Geschlecht:";
-$a->strings["Location:"] = "Ort:";
-$a->strings["Gender:"] = "Geschlecht:";
-$a->strings["Status:"] = "Status:";
-$a->strings["Homepage:"] = "Homepage:";
-$a->strings["About:"] = "Über:";
-$a->strings["No entries (some entries may be hidden)."] = "Keine Einträge (einige Einträge könnten versteckt sein).";
