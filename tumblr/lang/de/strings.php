9c5940861a11a62fe3e7076f7450cdbe334e4966
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-01 08:00:16 +0100

    DE translation tumblr addon updated

--- tumblr/lang/de/strings.php
+++ tumblr/lang/de/strings.php
@@ -7,13 +7,15 @@ function string_plural_select_de($n){
 }}
 ;
 $a->strings["Permission denied."] = "Zugriff verweigert.";
+$a->strings["Save Settings"] = "Einstellungen speichern";
+$a->strings["Consumer Key"] = "Consumer Key";
+$a->strings["Consumer Secret"] = "Consumer Secret";
 $a->strings["You are now authenticated to tumblr."] = "Du bist nun auf tumblr authentifiziert.";
-$a->strings["return to the connector page"] = "zurück zur Connector Seite";
+$a->strings["return to the connector page"] = "zurück zur Connector-Seite";
 $a->strings["Post to Tumblr"] = "Auf Tumblr veröffentlichen";
-$a->strings["Tumblr Post Settings"] = "Tumblr-Beitragseinstellungen";
-$a->strings["(Re-)Authenticate your tumblr page"] = "(Re-)Authentifizierung deiner tumblr Seite";
+$a->strings["Tumblr Export"] = "Tumblr Export";
+$a->strings["(Re-)Authenticate your tumblr page"] = "(Re-)Authentifizierung deiner tumblr-Seite";
 $a->strings["Enable Tumblr Post Addon"] = "Tumblr-Post-Addon aktivieren";
 $a->strings["Post to Tumblr by default"] = "Standardmäßig bei Tumblr veröffentlichen";
 $a->strings["Post to page:"] = "Auf tumblr veröffentlichen";
 $a->strings["You are not authenticated to tumblr"] = "Du bist gegenüber tumblr nicht authentifiziert";
-$a->strings["Submit"] = "Senden";
