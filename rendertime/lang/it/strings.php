4ade9aecd83a01e8f117a385682ebab6d44f0cfd
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 19:05:57 +0200

    rendertime addon IT translation update THX Sylke Vicious

--- rendertime/lang/it/strings.php
+++ rendertime/lang/it/strings.php
@@ -6,4 +6,5 @@ function string_plural_select_it($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["Performance: Database: %s, Network: %s, Rendering: %s, Parser: %s, I/O: %s, Other: %s, Total: %s"] = "Performance: Database: %s, Network: %s, Rendering: %s, Parser: %s, I/O: %s, Other: %s, Total: %s";
+$a->strings["Database: %s/%s, Network: %s, Rendering: %s, Session: %s, I/O: %s, Other: %s, Total: %s"] = "Database: %s/%s, Rete: %s, Rendering: %s, Sessione: %s, I/O: %s, Altro: %s, Totale: %s";
+$a->strings["Class-Init: %s, Boot: %s, Init: %s, Content: %s, Other: %s, Total: %s"] = "Class-Init: %s, Boot: %s, Init: %s, Content: %s, Altro: %s, Totale: %s";
