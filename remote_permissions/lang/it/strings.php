d3792f4b7364d2660de5597419844a509ac6765f
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 18:24:25 +0200

    remote_permissions addon IT translation update THX Sylke Vicious

--- remote_permissions/lang/it/strings.php
+++ remote_permissions/lang/it/strings.php
@@ -8,8 +8,7 @@ function string_plural_select_it($n){
 ;
 $a->strings["Remote Permissions Settings"] = "Impostazioni Permissi Remoti";
 $a->strings["Allow recipients of your private posts to see the other recipients of the posts"] = "Permetti ai destinatari dei tuoi messaggi privati di vedere gli altri destinatari del messaggio";
-$a->strings["Submit"] = "Invia";
-$a->strings["Remote Permissions settings updated."] = "Impostazioni Permessi Remoti aggiornate.";
+$a->strings["Save Settings"] = "Salva Impostazioni";
 $a->strings["Visible to:"] = "Visibile a:";
 $a->strings["Visible to"] = "Visibile a";
 $a->strings["may only be a partial list"] = "può essere solo una lista parziale";
@@ -17,4 +16,3 @@ $a->strings["Global"] = "Globale";
 $a->strings["The posts of every user on this server show the post recipients"] = "I messaggi di tutti gli utenti di questo server mostrano i destinatari";
 $a->strings["Individual"] = "Individuale";
 $a->strings["Each user chooses whether his/her posts show the post recipients"] = "Ogni utente sceglie se i suoi messaggi mostreranno i destinatari";
-$a->strings["Settings updated."] = "Impostazioni aggiornate.";
