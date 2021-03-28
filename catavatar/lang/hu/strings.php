b7e488a2f1a1c00951465495e7a7b35be9b0a3f8
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 19:18:40 +0200

    catavatar addon HU translation update THX Balázs Úr

--- catavatar/lang/hu/strings.php
+++ catavatar/lang/hu/strings.php
@@ -10,6 +10,7 @@ $a->strings["Use Cat as Avatar"] = "Macska használata profilképként";
 $a->strings["More Random Cat!"] = "Több véletlen macskát!";
 $a->strings["Reset to email Cat"] = "Visszaállítás e-mail macskára";
 $a->strings["Cat Avatar Settings"] = "Macskaprofilkép-beállítások";
+$a->strings["Set default profile avatar or randomize the cat."] = "Alapértelmezett profilkép beállítása vagy véletlenszerű macska.";
 $a->strings["The cat hadn't found itself."] = "A macska nem találta meg önmagát.";
 $a->strings["There was an error, the cat ran away."] = "Hiba történt, a macska elfutott.";
 $a->strings["Profile Photos"] = "Profilfényképek";
