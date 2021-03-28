da5c3db6cc3c9684a2361f1c173ae11e31277b8e
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 22:09:15 +0200

    smileybutton addon HU translation added THX Balázs Úr

---
+++ smileybutton/lang/hu/strings.php
@@ -0,0 +1,12 @@
+<?php
+
+if(! function_exists("string_plural_select_hu")) {
+function string_plural_select_hu($n){
+	$n = intval($n);
+	return intval($n != 1);
+}}
+;
+$a->strings["Smileybutton settings"] = "Hangulatjelgomb beállításai";
+$a->strings["You can hide the button and show the smilies directly."] = "Elrejtheti a gombot és közvetlenül megjelenítheti a hangulatjeleket.";
+$a->strings["Hide the button"] = "A gomb elrejtése";
+$a->strings["Save Settings"] = "Beállítások mentése";
