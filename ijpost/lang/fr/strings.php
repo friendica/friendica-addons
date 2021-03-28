1a4661e984fbc8105a5e84e72c71fbb8ba19fe7a
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 19:38:22 +0200

    ijpost addon FR translation updated THX bob lebonche

--- ijpost/lang/fr/strings.php
+++ ijpost/lang/fr/strings.php
@@ -1,9 +1,15 @@
 <?php
 
-$a->strings["Post to Insanejournal"] = "Publier vers InsaneJournal";
-$a->strings["InsaneJournal Post Settings"] = "Réglages InsaneJournal";
-$a->strings["Enable InsaneJournal Post Addon"] = "Activer le connecteur InsaneJournal";
-$a->strings["InsaneJournal username"] = "Utilisateur InsaneJournal";
-$a->strings["InsaneJournal password"] = "Mot de passe InsaneJournal";
-$a->strings["Post to InsaneJournal by default"] = "Publier sur InsaneJournal par défaut";
-$a->strings["Submit"] = "Envoyer";
+if(! function_exists("string_plural_select_fr")) {
+function string_plural_select_fr($n){
+	$n = intval($n);
+	return intval($n > 1);
+}}
+;
+$a->strings["Post to Insanejournal"] = "Publier sur Insanejournal";
+$a->strings["InsaneJournal Export"] = "Export vers journal Insane ";
+$a->strings["Enable InsaneJournal Post Addon"] = "Activer l'application complémentaire InsaneJournalPost";
+$a->strings["InsaneJournal username"] = "Identifiant du InsaneJournal";
+$a->strings["InsaneJournal password"] = "Mot de passe du InsaneJournal";
+$a->strings["Post to InsaneJournal by default"] = "Publier sur le InsaneJournal par défaut";
+$a->strings["Save Settings"] = "Sauvegarder les paramètres";
