ec3e4a0baa4712bdae08ef47b9eb3a9b6b074e2b
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 19:30:53 +0200

    libertree addon RU translation added THX Alexander An

--- libertree/lang/ru/strings.php
+++ libertree/lang/ru/strings.php
@@ -1,9 +1,11 @@
 <?php
 
-$a->strings["Post to libertree"] = "";
-$a->strings["libertree Post Settings"] = "";
-$a->strings["Enable Libertree Post Addon"] = "";
-$a->strings["Libertree API token"] = "";
-$a->strings["Libertree site URL"] = "";
-$a->strings["Post to Libertree by default"] = "";
-$a->strings["Submit"] = "Подтвердить";
+if(! function_exists("string_plural_select_ru")) {
+function string_plural_select_ru($n){
+	$n = intval($n);
+	if ($n%10==1 && $n%100!=11) { return 0; } else if ($n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14)) { return 1; } else if ($n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)) { return 2; } else  { return 3; }
+}}
+;
+$a->strings["Post to libertree"] = "Отправлять в Libertree";
+$a->strings["Enable Libertree Post Addon"] = "Включить отправку в Libertree";
+$a->strings["Post to Libertree by default"] = "Отправлять записи в Libertree по-умолчанию";
