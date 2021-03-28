08ae8e4e3a5955407bf694b9c9a3dd5d4860b91a
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 22:03:59 +0200

    superblock addon HU translation added THX Balázs Úr

---
+++ superblock/lang/hu/strings.php
@@ -0,0 +1,12 @@
+<?php
+
+if(! function_exists("string_plural_select_hu")) {
+function string_plural_select_hu($n){
+	$n = intval($n);
+	return intval($n != 1);
+}}
+;
+$a->strings["Superblock"] = "Szuper tiltás";
+$a->strings["Comma separated profile URLS to block"] = "Tiltandó profil URL-ek vesszővel elválasztott listája";
+$a->strings["Save Settings"] = "Beállítások mentése";
+$a->strings["Block Completely"] = "Tiltás teljesen";
