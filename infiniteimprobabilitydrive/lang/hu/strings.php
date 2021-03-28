96a41ef2a42253b6236578b1c6cf9c5f7fd968e7
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 19:52:04 +0200

    infinite improbability drive addon HU translation added THX Balázs Úr

---
+++ infiniteimprobabilitydrive/lang/hu/strings.php
@@ -0,0 +1,9 @@
+<?php
+
+if(! function_exists("string_plural_select_hu")) {
+function string_plural_select_hu($n){
+	$n = intval($n);
+	return intval($n != 1);
+}}
+;
+$a->strings["Infinite Improbability Drive"] = "Végtelen valószínűségű meghajtó";
