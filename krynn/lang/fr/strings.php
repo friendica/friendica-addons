59146f987976c8dcc4cac8924d875e8b3bce403f
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 20:08:35 +0200

    krynn addon FR translation updated THX bob lebonche

--- krynn/lang/fr/strings.php
+++ krynn/lang/fr/strings.php
@@ -1,5 +1,12 @@
 <?php
 
-$a->strings["Krynn Settings"] = "Réglages des Krynn";
-$a->strings["Enable Krynn Addon"] = "Activer Krynn";
-$a->strings["Submit"] = "Envoyer";
+if(! function_exists("string_plural_select_fr")) {
+function string_plural_select_fr($n){
+	$n = intval($n);
+	return intval($n > 1);
+}}
+;
+$a->strings["Krynn"] = "Krynn";
+$a->strings["Krynn Settings"] = "Paramètres de Krynn";
+$a->strings["Enable Krynn Addon"] = "Activer l'application complémentaire Krynn";
+$a->strings["Save Settings"] = "Sauvegarder les paramètres";
