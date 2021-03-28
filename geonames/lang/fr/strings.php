4732761c1b1456b01d4e4f92872761efb761fa7a
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 19:40:26 +0200

    geonames addon FR translation updated THX bob lebonche

--- geonames/lang/fr/strings.php
+++ geonames/lang/fr/strings.php
@@ -6,5 +6,7 @@ function string_plural_select_fr($n){
 	return intval($n > 1);
 }}
 ;
-$a->strings["Geonames settings updated."] = "Paramètres de Geonames mis à jour.";
 $a->strings["Geonames Settings"] = "Paramètres Geonames";
+$a->strings["Replace numerical coordinates by the nearest populated location name in your posts."] = "Remplacer les coordonnées par le nom de la localité la plus proche dans votre publication.";
+$a->strings["Enable Geonames Addon"] = "Activer l'application complémentaire Geonames";
+$a->strings["Save Settings"] = "Sauvegarder les paramètres.";
