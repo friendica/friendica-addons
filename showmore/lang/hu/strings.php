e91da553518cd58b5e7d30e3b405e8219c0bb8aa
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 22:10:27 +0200

    showmore addon HU translation added THX Balázs Úr

---
+++ showmore/lang/hu/strings.php
@@ -0,0 +1,13 @@
+<?php
+
+if(! function_exists("string_plural_select_hu")) {
+function string_plural_select_hu($n){
+	$n = intval($n);
+	return intval($n != 1);
+}}
+;
+$a->strings["\"Show more\" Settings"] = "„Több megjelenítése” beállításai";
+$a->strings["Enable Show More"] = "Több megjelenítésének engedélyezése";
+$a->strings["Cutting posts after how much characters"] = "Bejegyzések vágása ennyi karakter után";
+$a->strings["Save Settings"] = "Beállítások mentése";
+$a->strings["show more"] = "több megjelenítése";
