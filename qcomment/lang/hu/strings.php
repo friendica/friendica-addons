c772d1354445a06c1db1246f5cb5dc93a8bfc7c5
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 22:18:23 +0200

    qcomment addon HU translation added THX Balázs Úr

---
+++ qcomment/lang/hu/strings.php
@@ -0,0 +1,15 @@
+<?php
+
+if(! function_exists("string_plural_select_hu")) {
+function string_plural_select_hu($n){
+	$n = intval($n);
+	return intval($n != 1);
+}}
+;
+$a->strings[":-)"] = ":-)";
+$a->strings[":-("] = ":-(";
+$a->strings["lol"] = "lol";
+$a->strings["Quick Comment Settings"] = "Gyors hozzászólás beállításai";
+$a->strings["Quick comments are found near comment boxes, sometimes hidden. Click them to provide simple replies."] = "A gyors hozzászólások a hozzászólásmezők közelében találhatók, néha rejtetten. Kattintson rájuk egyszerű válaszok küldéséhez.";
+$a->strings["Save Settings"] = "Beállítások mentése";
+$a->strings["Enter quick comments, one per line"] = "Adja meg a gyors hozzászólásokat, soronként egyet";
