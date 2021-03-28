7442d15e7c45ffedf32fdc3edbcbde7921508e72
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 18:43:42 +0200

    numfriends addon IT translation update THX Sylke Vicious

--- numfriends/lang/it/strings.php
+++ numfriends/lang/it/strings.php
@@ -6,7 +6,6 @@ function string_plural_select_it($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["Numfriends settings updated."] = "Impostazioni \"Numfriends' aggiornate.";
 $a->strings["Numfriends Settings"] = "Impostazioni Numfriends";
 $a->strings["How many contacts to display on profile sidebar"] = "Quanti contatti visualizzare nella barra laterale del profilo";
-$a->strings["Submit"] = "Invia";
+$a->strings["Save Settings"] = "Salva Impostazioni";
