9e025b4771629487d9db91e32bf2a31b80eb93da
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 20:13:39 +0200

    gnot addon FR translation updated THX bob lebonche

--- gnot/lang/fr/strings.php
+++ gnot/lang/fr/strings.php
@@ -6,5 +6,8 @@ function string_plural_select_fr($n){
 	return intval($n > 1);
 }}
 ;
-$a->strings["Gnot settings updated."] = "Paramètres de Gnot mis à jour.";
 $a->strings["Gnot Settings"] = "Paramètres Gnot";
+$a->strings["Save Settings"] = "Sauvegarder les paramètres.";
+$a->strings["Enable this addon?"] = "Activer cette application complémentaire ?";
+$a->strings["Allows threading of email comment notifications on Gmail and anonymising the subject line."] = "Permettre le filtrage des notifications de commentaires par courriel sur Gmail et l'anonymisation de l'objet.";
+$a->strings["[Friendica:Notify] Comment to conversation #%d"] = "[Friendica:Notify] Commentaire vers conversation #%d";
