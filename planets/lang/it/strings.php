d7de9d9bf729806cd362f50b1a968b6c21f3f094
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 18:54:05 +0200

    planets addon IT translation update THX Sylke Vicious

--- planets/lang/it/strings.php
+++ planets/lang/it/strings.php
@@ -6,6 +6,7 @@ function string_plural_select_it($n){
 	return intval($n != 1);
 }}
 ;
+$a->strings["Planets"] = "Pianeti";
 $a->strings["Planets Settings"] = "Impostazioni \"Pianeti\"";
 $a->strings["Enable Planets Addon"] = "Abilita il componente aggiuntivo Pianeti";
-$a->strings["Submit"] = "Invia";
+$a->strings["Save Settings"] = "Salva Impostazioni";
