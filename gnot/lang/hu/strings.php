ce50fda7b1256cbba253dad4253ecd1a7eca6c8f
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 19:23:44 +0200

    gnot addon HU translation updated THX Balázs Úr

--- gnot/lang/hu/strings.php
+++ gnot/lang/hu/strings.php
@@ -6,9 +6,8 @@ function string_plural_select_hu($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["Gnot settings updated."] = "A Gnot beállításai frissítve.";
 $a->strings["Gnot Settings"] = "Gnot beállítások";
-$a->strings["Allows threading of email comment notifications on Gmail and anonymising the subject line."] = "Lehetővé teszi az e-mailes hozzászólás értesítéseinek szálkezelését a Gmailnél, és anonimizálja a tárgy sorát.";
+$a->strings["Save Settings"] = "Beállítások mentése";
 $a->strings["Enable this addon?"] = "Engedélyezi ezt a bővítményt?";
-$a->strings["Submit"] = "Elküldés";
+$a->strings["Allows threading of email comment notifications on Gmail and anonymising the subject line."] = "Lehetővé teszi az e-mailes hozzászólás értesítéseinek szálkezelését a Gmailnél, és anonimizálja a tárgy sorát.";
 $a->strings["[Friendica:Notify] Comment to conversation #%d"] = "[Friendica: értesítés] Hozzászólás a(z) %d. beszélgetéshez";
