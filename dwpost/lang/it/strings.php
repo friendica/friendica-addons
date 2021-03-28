b99d9b551c9698ddcfa836fe24d60fb26c267f13
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 18:44:53 +0200

    dwpost addon IT translation update THX Sylke Vicious

--- dwpost/lang/it/strings.php
+++ dwpost/lang/it/strings.php
@@ -7,9 +7,9 @@ function string_plural_select_it($n){
 }}
 ;
 $a->strings["Post to Dreamwidth"] = "Invia a Dreamwidth";
-$a->strings["Dreamwidth Post Settings"] = "Impostazioni Invio a Dreamwidth";
+$a->strings["Dreamwidth Export"] = "Esporta Dreamwidth";
 $a->strings["Enable dreamwidth Post Addon"] = "Abilita il componente aggiuntivo di invio a Dreamwidth";
 $a->strings["dreamwidth username"] = "Nome utente Dreamwidth";
 $a->strings["dreamwidth password"] = "password Dreamwidth";
 $a->strings["Post to dreamwidth by default"] = "Invia sempre a Dreamwidth";
-$a->strings["Submit"] = "Salva";
+$a->strings["Save Settings"] = "Salva Impostazioni";
