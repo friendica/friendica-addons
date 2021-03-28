d6ff1c7ef631ca99e4a8367b9c9bd5c0ee656bc8
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 18:21:53 +0200

    IT mathjax addon translation update THX Sylke Vicious

--- mathjax/lang/it/strings.php
+++ mathjax/lang/it/strings.php
@@ -6,10 +6,6 @@ function string_plural_select_it($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["Settings"] = "Impostazioni";
-$a->strings["The MathJax addon renders mathematical formulae written using the LaTeX syntax surrounded by the usual $$ or an eqnarray block in the postings of your wall,network tab and private mail."] = "Iladdon\"MatJax\" renderizza formule matematiche scritta usando la sintassi LaTeX circondate dalle usuali $$ o un blocco eqnarray nei messaggi della tua bacheca, pagina Rete e messaggi privati.";
+$a->strings["The MathJax addon renders mathematical formulae written using the LaTeX syntax surrounded by the usual $$ or an eqnarray block in the postings of your wall,network tab and private mail."] = "Il plugin \"MatJax\" renderizza formule matematiche scritta usando la sintassi LaTeX circondate dalle usuali $$ o un blocco eqnarray nei messaggi della tua bacheca, pagina Rete e messaggi privati.";
 $a->strings["Use the MathJax renderer"] = "Usa il render MathJax";
-$a->strings["Submit"] = "Invia";
-$a->strings["Settings updated."] = "Impostazioni aggiornate.";
-$a->strings["MathJax Base URL"] = "Indirizzo di base di MathJax";
-$a->strings["The URL for the javascript file that should be included to use MathJax. Can be either the MathJax CDN or another installation of MathJax."] = "L'indirizzo per il file javascript che deve essere incluso per usare MathJax. Può essere sul CDN di MathJax o in un'altra installazione di MathJax.";
+$a->strings["Save Settings"] = "Salva Impostazioni";
