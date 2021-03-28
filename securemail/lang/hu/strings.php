b4f0d0bd75d000aa0408404a44a8ad995b71d651
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-28 22:11:34 +0200

    securemail addon HU translation added THX Balázs Úr

---
+++ securemail/lang/hu/strings.php
@@ -0,0 +1,16 @@
+<?php
+
+if(! function_exists("string_plural_select_hu")) {
+function string_plural_select_hu($n){
+	$n = intval($n);
+	return intval($n != 1);
+}}
+;
+$a->strings["\"Secure Mail\" Settings"] = "Biztonságos levél beállításai";
+$a->strings["Save Settings"] = "Beállítások mentése";
+$a->strings["Save and send test"] = "Mentés és teszt küldése";
+$a->strings["Enable Secure Mail"] = "Biztonságos levél engedélyezése";
+$a->strings["Public key"] = "Nyilvános kulcs";
+$a->strings["Your public PGP key, ascii armored format"] = "A nyilvános PGP kulcsa ASCII-védett formátumban";
+$a->strings["Test email sent"] = "Tesztlevél elküldve";
+$a->strings["There was an error sending the test email"] = "Hiba történt a tesztlevél küldésekor";
