889525ad81a80b3e9e41f949fca8915003ce7937
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-03-29 07:37:17 +0200

    ljpost addon DE translation updated

--- ljpost/lang/de/strings.php
+++ ljpost/lang/de/strings.php
@@ -7,9 +7,9 @@ function string_plural_select_de($n){
 }}
 ;
 $a->strings["Post to LiveJournal"] = "In LiveJournal veröffentlichen.";
-$a->strings["LiveJournal Post Settings"] = "LiveJournal Veröffentlichungs-Einstellungen";
-$a->strings["Enable LiveJournal Post Addon"] = "LiveJournal Post Addon aktivieren";
-$a->strings["LiveJournal username"] = "LiveJournal Benutzername";
-$a->strings["LiveJournal password"] = "LiveJournal Passwort";
+$a->strings["LiveJournal Post Settings"] = "LiveJournal-Veröffentlichungs-Einstellungen";
+$a->strings["Enable LiveJournal Post Addon"] = "LiveJournal-Post-Addon aktivieren";
+$a->strings["LiveJournal username"] = "LiveJournal-Benutzername";
+$a->strings["LiveJournal password"] = "LiveJournal-Passwort";
 $a->strings["Post to LiveJournal by default"] = "Standardmäßig bei LiveJournal veröffentlichen";
-$a->strings["Submit"] = "Senden";
+$a->strings["Save Settings"] = "Einstellungen speichern";
