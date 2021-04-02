57531318a66b5daea5d59adc80f62851b657667a
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-04-02 08:40:59 +0200

    grou_text addon ES translation updated THX Senex Petrovic

--- group_text/lang/es/strings.php
+++ group_text/lang/es/strings.php
@@ -6,7 +6,6 @@ function string_plural_select_es($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["Group Text settings updated."] = "Ajustes de Grupo de Texto actualizados";
 $a->strings["Group Text"] = "Grupo de Texto";
 $a->strings["Use a text only (non-image) group selector in the \"group edit\" menu"] = "Utilice sólo el selector de grupo de texto (no imagen) en el menú \"edición de grupo\"";
-$a->strings["Submit"] = "Enviar";
+$a->strings["Save Settings"] = "Guardar Ajustes";
