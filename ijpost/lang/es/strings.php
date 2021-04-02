ca343f1dc573a31613ca09982fc4d6a844477357
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-04-02 08:44:34 +0200

    ijpost addon ES translation updated THX Senex Petrovic

--- ijpost/lang/es/strings.php
+++ ijpost/lang/es/strings.php
@@ -7,9 +7,9 @@ function string_plural_select_es($n){
 }}
 ;
 $a->strings["Post to Insanejournal"] = "Publicar en Insanejournal";
-$a->strings["InsaneJournal Post Settings"] = "Ajustes de Entrada de InsaneJournal";
-$a->strings["Enable InsaneJournal Post Addon"] = "Habilitar el Addon de Entrada InsaneJournal";
+$a->strings["InsaneJournal Export"] = "InsaneJournal Exportar";
+$a->strings["Enable InsaneJournal Post Addon"] = "Habilitar el Plugin de Entrada InsaneJournal";
 $a->strings["InsaneJournal username"] = "Nombre de usuario InsaneJournal";
 $a->strings["InsaneJournal password"] = "Contraseña de InsaneJournal";
 $a->strings["Post to InsaneJournal by default"] = "Publicar en InsaneJournal por defecto";
-$a->strings["Submit"] = "Enviar";
+$a->strings["Save Settings"] = "Guardar Ajustes";
