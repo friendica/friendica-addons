28623e80b3b1193d47c6a5dac05519c75b9af29e
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-04-02 08:27:51 +0200

    blockem addon ES translation updated THX Senex Petrovic

--- blockem/lang/es/strings.php
+++ blockem/lang/es/strings.php
@@ -6,12 +6,10 @@ function string_plural_select_es($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["\"Blockem\""] = "\"Bloquealos\"";
+$a->strings["Blockem"] = "Blockem (Bloquealos)";
 $a->strings["Hides user's content by collapsing posts. Also replaces their avatar with generic image."] = "Oculta el contenido del usuario al colapsar las publicaciones. También reemplaza su avatar con una imagen genérica.";
 $a->strings["Comma separated profile URLS:"] = "URLs de perfil separadas por comas:";
 $a->strings["Save Settings"] = "Guardar configuración";
-$a->strings["BLOCKEM Settings saved."] = "Configuración de BLOQUEALOS guardado.";
 $a->strings["Filtered user: %s"] = "Usuario filtrado: %s";
 $a->strings["Unblock Author"] = "Desbloquear autor";
 $a->strings["Block Author"] = "Bloquear autor";
-$a->strings["blockem settings updated"] = "configuración de BLOQUEALOS actualizado";
