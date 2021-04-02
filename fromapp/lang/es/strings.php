0a1447da10bd5446688150a261f0eeecabb0dc79
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-04-02 08:36:44 +0200

    fromapp addon ES translation updated THX Senex Petrovic

--- fromapp/lang/es/strings.php
+++ fromapp/lang/es/strings.php
@@ -6,8 +6,7 @@ function string_plural_select_es($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["Fromapp settings updated."] = "Ajustes de Fromapp actualizados.";
 $a->strings["FromApp Settings"] = "Ajustes de FromApp";
-$a->strings["The application name you would like to show your posts originating from."] = "El nombre de la aplicación desde la que le gustaría que se mostrasen sus publicaciones.";
+$a->strings["The application name you would like to show your posts originating from. Separate different app names with a comma. A random one will then be selected for every posting."] = "El nombre de la aplicación desde la que le gustaría mostrar sus publicaciones. Separe los diferentes nombres de aplicaciones con una coma. Luego, se seleccionará uno al azar para cada publicación.";
 $a->strings["Use this application name even if another application was used."] = "Utilice este nombre de aplicación incluso si otra aplicación fue utilizada.";
-$a->strings["Submit"] = "Enviar";
+$a->strings["Save Settings"] = "Guardar Ajustes";
