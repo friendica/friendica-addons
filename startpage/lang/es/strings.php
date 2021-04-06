471361430df178b2c917ca0755d0a25af03f9fd5
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-04-06 06:49:09 +0200

    ES translation of the startpage addon updated THX Senex Petrovic

--- startpage/lang/es/strings.php
+++ startpage/lang/es/strings.php
@@ -6,7 +6,7 @@ function string_plural_select_es($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["Startpage Settings"] = "Ajustes de Startpage";
+$a->strings["Startpage"] = "Página de Inicio";
 $a->strings["Home page to load after login  - leave blank for profile wall"] = "Página principal a cargar tras el acceso - dejar en blanco para el muro de perfil";
 $a->strings["Examples: &quot;network&quot; or &quot;notifications/system&quot;"] = "Ejemplos: &quot;network&quot; o &quot;notifications/system&quot;";
-$a->strings["Submit"] = "Enviar";
+$a->strings["Save Settings"] = "Guardar ajustes";
