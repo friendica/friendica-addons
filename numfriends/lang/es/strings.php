cec21dbd4145b545624112d223ac59b07ffcb48b
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-04-06 07:02:28 +0200

    ES translation of the numfriends addon updated THX Senex Petrovic

--- numfriends/lang/es/strings.php
+++ numfriends/lang/es/strings.php
@@ -6,7 +6,6 @@ function string_plural_select_es($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["Numfriends settings updated."] = "Ajustes de Numfriends actualizados.";
 $a->strings["Numfriends Settings"] = "Ajustes de Numfriends";
 $a->strings["How many contacts to display on profile sidebar"] = "Cuántos contactos mostrar en la barra lateral del perfil";
-$a->strings["Submit"] = "Enviar";
+$a->strings["Save Settings"] = "Grabar ajustes";
