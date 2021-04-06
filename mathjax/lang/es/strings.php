e2ea0618874dd0d264bb915ce66899676fb6c8b3
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-04-06 07:09:59 +0200

    ES translation of the mathjax addon updated THX Senex Petrovic

--- mathjax/lang/es/strings.php
+++ mathjax/lang/es/strings.php
@@ -6,10 +6,6 @@ function string_plural_select_es($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["Settings"] = "Ajustes";
 $a->strings["The MathJax addon renders mathematical formulae written using the LaTeX syntax surrounded by the usual $$ or an eqnarray block in the postings of your wall,network tab and private mail."] = "El addon MathJax renderiza las fórmulas matemáticos escritas usando la sintaxis LaTeX rodeada por el usual $$ o un bloque eqnarray en las publicaciones de su muro, etiqueta de red y mail privado.";
 $a->strings["Use the MathJax renderer"] = "Usar el renderizador MathJax";
-$a->strings["Submit"] = "Enviar";
-$a->strings["Settings updated."] = "Ajustes actualizados.";
-$a->strings["MathJax Base URL"] = "URL Base de MathJax";
-$a->strings["The URL for the javascript file that should be included to use MathJax. Can be either the MathJax CDN or another installation of MathJax."] = "La URL para el archivo javascript que debería ser incluído para usar MathJax. Puede ser el CDN de MathJax u otra instalación de MathJax.";
+$a->strings["Save Settings"] = "Guardar ajustes";
