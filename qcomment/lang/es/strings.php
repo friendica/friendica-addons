e38eb1c71cc06a065fb91babc68a1af61096544c
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-04-06 06:55:56 +0200

    ES translation of the qcomment addon updated THX Senex Petrovic

--- qcomment/lang/es/strings.php
+++ qcomment/lang/es/strings.php
@@ -11,6 +11,5 @@ $a->strings[":-("] = ":-(";
 $a->strings["lol"] = "LOL";
 $a->strings["Quick Comment Settings"] = "Ajustes de Comentario Rápido";
 $a->strings["Quick comments are found near comment boxes, sometimes hidden. Click them to provide simple replies."] = "Los comentarios rápidos están junto a las cajas de comentario, a veces ocultos. Click sobre ellos para proveer respuestas simples.";
+$a->strings["Save Settings"] = "Grabar ajustes";
 $a->strings["Enter quick comments, one per line"] = "Introducir comentarios rápidos, uno por línea";
-$a->strings["Submit"] = "Enviar";
-$a->strings["Quick Comment settings saved."] = "Ajustes de Comentario Rápido guardados.";
