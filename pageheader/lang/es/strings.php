ea5a3e52c96b435a3d71475359f7df17b7eaacef
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-04-06 07:00:31 +0200

    ES translation of the pageheader addon updated THX Senex Petrovic

--- pageheader/lang/es/strings.php
+++ pageheader/lang/es/strings.php
@@ -7,5 +7,6 @@ function string_plural_select_es($n){
 }}
 ;
 $a->strings["\"pageheader\" Settings"] = "Ajustes de \"pageheader\"";
-$a->strings["Submit"] = "Enviar";
-$a->strings["pageheader Settings saved."] = "Ajustes de pageheader actualizados.";
+$a->strings["Message"] = "Mensaje";
+$a->strings["Message to display on every page on this server (or put a pageheader.html file in your docroot)"] = "Mensaje para mostrar en todas las páginas de este servidor (o poner un archivo pageheader.html en su docroot)";
+$a->strings["Save Settings"] = "Guardar ajustes";
