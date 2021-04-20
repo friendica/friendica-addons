8e101a470d55c3bd758d26f68d62ec9f74fbd793
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-04-20 10:58:20 +0200

    IT translation update jrynn addon THX Sylke Vicious

--- krynn/lang/it/strings.php
+++ krynn/lang/it/strings.php
@@ -6,6 +6,7 @@ function string_plural_select_it($n){
 	return intval($n != 1);
 }}
 ;
+$a->strings["Krynn"] = "Krynn";
 $a->strings["Krynn Settings"] = "Impostazioni Krynn";
 $a->strings["Enable Krynn Addon"] = "Abilita componente aggiuntivo Krynn";
-$a->strings["Submit"] = "Invia";
+$a->strings["Save Settings"] = "Salva Impostazioni";
