d753afb1b013d322f668d1ee2e80f5039964fcc4
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-05-24 08:17:11 +0200

    added DE translation of xmpp THX Till Mohr

--- xmpp/lang/de/strings.php
+++ xmpp/lang/de/strings.php
@@ -6,11 +6,9 @@ function string_plural_select_de($n){
 	return intval($n != 1);
 }}
 ;
-$a->strings["XMPP settings updated."] = "XMPP Einstellungen aktualisiert.";
 $a->strings["XMPP-Chat (Jabber)"] = "XMPP-Chat (Jabber)";
 $a->strings["Enable Webchat"] = "Aktiviere Webchat";
 $a->strings["Individual Credentials"] = "Individuelle Anmeldedaten";
-$a->strings["Jabber BOSH host"] = "Jabber BOSH Host";
+$a->strings["Jabber BOSH host"] = "Jabber-BOSH-Host";
 $a->strings["Save Settings"] = "Speichere Einstellungen";
 $a->strings["Use central userbase"] = "Nutze zentrale Nutzerbasis";
-$a->strings["Settings updated."] = "Einstellungen aktualisiert.";
