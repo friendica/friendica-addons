410c90460ed4bd78d9857e8a56645c6c9c43289d
Philipp <admin@philipp.info>
2021-06-26 19:46:51 +0200

    [phpmailer] Update phpmailer to v6.5.0

--- phpmailer/vendor/composer/autoload_real.php
+++ phpmailer/vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitPhpMailerAddon
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
