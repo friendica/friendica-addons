e6b880d206a86ad29d56df28d41e12be2b55f6fb
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-07-28 07:22:30 +0200

    ifttt translation updated ZH CN THX Finn Dean

--- ifttt/lang/zh-cn/strings.php
+++ ifttt/lang/zh-cn/strings.php
@@ -7,5 +7,8 @@ function string_plural_select_zh_cn($n){
 }}
 ;
 $a->strings["IFTTT Mirror"] = "IFTTT 镜像";
+$a->strings["Body for \"new status message\""] = "正文“新状态消息”";
+$a->strings["Body for \"new photo upload\""] = "正文“新照片上传”";
+$a->strings["Body for \"new link post\""] = "正文“新发布链接”";
 $a->strings["Generate new key"] = "生成新的密钥";
 $a->strings["Save Settings"] = "保存设置";
