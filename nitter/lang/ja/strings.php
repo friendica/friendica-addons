ab4f909443bf7d910abd1f3e49652f52d9ee2938
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-08-16 08:05:01 +0200

    nitter: JA translation added THX XMPPはいいぞ

---
+++ nitter/lang/ja/strings.php
@@ -0,0 +1,11 @@
+<?php
+
+if(! function_exists("string_plural_select_ja")) {
+function string_plural_select_ja($n){
+	$n = intval($n);
+	return intval(0);
+}}
+;
+$a->strings["Nitter server"] = "Nitterサーバー";
+$a->strings["Save Settings"] = "設定を保存";
+$a->strings["Links to Twitter in this posting were replaced by links to the Nitter instance at %s"] = "この投稿のTwitterへのリンクは、%sのNitterインスタンスへのリンクに置換されました";
