c76eb06020222f58c2b6c898cf26fdbc39e31fb2
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2021-11-27 17:13:05 +0100

    moved BLOGGER, BUFFER, NOTIMELINE and XMPP addons to the deprecated-addons repository
    The translation resources have been removed from transifex.
    A pull-request has been opened at the deprecated-addons repository.

--- buffer/lang/is/strings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(! function_exists("string_plural_select_is")) {
-function string_plural_select_is($n){
-	$n = intval($n);
-	return intval($n % 10 != 1 || $n % 100 == 11);
-}}
-$a->strings['Permission denied.'] = 'Heimild ekki veitt.';
--- notimeline/lang/nl/strings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(! function_exists("string_plural_select_nl")) {
-function string_plural_select_nl($n){
-	$n = intval($n);
-	return intval($n != 1);
-}}
-$a->strings['No Timeline settings updated.'] = 'Geen tijdlijn instellingen opgeslagen';
-$a->strings['No Timeline Settings'] = 'No Timeline instellingen';
