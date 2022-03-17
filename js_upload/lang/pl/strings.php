fa1e8d7c3995384942218ca6c7f2e2b7c0c2f7b8
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2022-03-17 19:09:32 +0100

    PL translation updates THX strebski

--- js_upload/lang/pl/strings.php
+++ js_upload/lang/pl/strings.php
@@ -5,9 +5,12 @@ function string_plural_select_pl($n){
 	$n = intval($n);
 	if ($n==1) { return 0; } else if (($n%10>=2 && $n%10<=4) && ($n%100<12 || $n%100>14)) { return 1; } else if ($n!=1 && ($n%10>=0 && $n%10<=1) || ($n%10>=5 && $n%10<=9) || ($n%100>=12 && $n%100<=14)) { return 2; } else  { return 3; }
 }}
-$a->strings['Drop files here to upload'] = 'Aby przesłać pliki upuść tutaj';
+$a->strings['Select files for upload'] = 'Wybierz pliki do przesłania';
+$a->strings['Drop files here to upload'] = 'Upuść tutaj pliki do przesłania';
 $a->strings['Cancel'] = 'Anuluj';
 $a->strings['Failed'] = 'Nie powiodło się';
 $a->strings['No files were uploaded.'] = 'Żadne pliki nie zostały przesłane.';
 $a->strings['Uploaded file is empty'] = 'Przesłany plik jest pusty';
+$a->strings['Image exceeds size limit of %s'] = 'Obraz przekracza limit rozmiaru wynoszący %s';
+$a->strings['File has an invalid extension, it should be one of %s.'] = 'Plik ma nieprawidłowe rozszerzenie, powinno być jednym z: %s.';
 $a->strings['Upload was cancelled, or server error encountered'] = 'Przesyłanie zostało anulowane lub wystąpił błąd serwera';
