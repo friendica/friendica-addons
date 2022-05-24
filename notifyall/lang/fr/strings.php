18eb65d8ea859505c85297d853cdfd8649529fd1
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2022-05-24 08:00:56 +0200

    notifyall addon FR translation updated THX kalon33

--- notifyall/lang/fr/strings.php
+++ notifyall/lang/fr/strings.php
@@ -3,8 +3,9 @@
 if(! function_exists("string_plural_select_fr")) {
 function string_plural_select_fr($n){
 	$n = intval($n);
-	return intval($n > 1);
+	if (($n == 0 || $n == 1)) { return 0; } else if ($n != 0 && $n % 1000000 == 0) { return 1; } else  { return 2; }
 }}
+$a->strings['%s Administrator'] = 'L\'administrateur de %s';
 $a->strings['Send email to all members'] = 'Envoyer un courriel à tous les membres';
 $a->strings['No recipients found.'] = 'Aucun destinataire trouvé.';
 $a->strings['Emails sent'] = 'Courriels envoyés';
