8d1001f8eb0f09a6ce0baab87968da408882b9c6
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2022-06-07 06:42:04 +0200

    ijpost translation update THX SpcCw

--- ijpost/lang/ru/strings.php
+++ ijpost/lang/ru/strings.php
@@ -1,9 +1,13 @@
 <?php
 
-$a->strings["Post to Insanejournal"] = "";
-$a->strings["InsaneJournal Post Settings"] = "";
-$a->strings["Enable InsaneJournal Post Addon"] = "Включить InsaneJournal  плагин сообщений";
-$a->strings["InsaneJournal username"] = "";
-$a->strings["InsaneJournal password"] = "";
-$a->strings["Post to InsaneJournal by default"] = "";
-$a->strings["Submit"] = "Подтвердить";
+if(! function_exists("string_plural_select_ru")) {
+function string_plural_select_ru($n){
+	$n = intval($n);
+	if ($n%10==1 && $n%100!=11) { return 0; } else if ($n%10>=2 && $n%10<=4 && ($n%100<12 || $n%100>14)) { return 1; } else if ($n%10==0 || ($n%10>=5 && $n%10<=9) || ($n%100>=11 && $n%100<=14)) { return 2; } else  { return 3; }
+}}
+$a->strings['Post to Insanejournal'] = 'Отправить в Insanejournal';
+$a->strings['Enable InsaneJournal Post Addon'] = 'Включить дополнение для InsaneJournal';
+$a->strings['InsaneJournal username'] = 'Имя пользователя InsaneJournal';
+$a->strings['InsaneJournal password'] = 'Пароль в InsaneJournal';
+$a->strings['Post to InsaneJournal by default'] = 'Отправлять записи в InsaneJournal по-умолчанию';
+$a->strings['InsaneJournal Export'] = 'Экспорт InsaneJournal';
