6c8a9bd4618cff4d7c9ae05d819a99b51cab9174
Philipp <admin@philipp.info>
2022-10-17 21:25:03 +0200

    Move Monolog to Addons

---
+++ monolog/src/IntrospectionProcessor.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @copyright Copyright (C) 2010-2022, the Friendica project
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Friendica\Addon\monolog\src;
+
+use Friendica\Core\Logger\Util\Introspection;
+use Monolog\Logger;
+use Monolog\LogRecord;
+use Monolog\Processor\ProcessorInterface;
+
+/**
+ * Injects line/file//function where the log message came from
+ */
+class IntrospectionProcessor implements ProcessorInterface
+{
+	private $level;
+
+	private $introspection;
+
+	/**
+	 * @param Introspection $introspection Holds the Introspection of the current call
+	 * @param string|int    $level         The minimum logging level at which this Processor will be triggered
+	 */
+	public function __construct(Introspection $introspection, $level = Logger::DEBUG)
+	{
+		$this->level = Logger::toMonologLevel($level);
+		$introspection->addClasses(['Monolog\\']);
+		$this->introspection = $introspection;
+	}
+
+	public function __invoke(LogRecord $record): LogRecord
+	{
+		// return if the level is not high enough
+		if ($record->level < $this->level) {
+			return $record;
+		}
+		// we should have the call source now
+		$record->extra = array_merge(
+			$record->extra,
+			$this->introspection->getRecord()
+		);
+
+		return $record;
+	}
+}
