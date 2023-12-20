96a354bc6540c1a729729ba361a26d4ec0a8b1d2
Michael <heluecht@pirati.ca>
2023-12-20 20:46:24 +0000

    Bluesky/Tumblr: Set "received" to "created" if fetched after previous poll

--- bluesky/bluesky_notifications.php
+++ bluesky/bluesky_notifications.php
@@ -6,11 +6,11 @@ function bluesky_notifications_run($argv, $argc)
 {
 	require_once 'addon/bluesky/bluesky.php';
 
-	if ($argc != 2) {
+	if ($argc != 3) {
 		return;
 	}
 
-	Logger::notice('importing notifications - start', ['user' => $argv[1]]);
-	bluesky_fetch_notifications($argv[1]);
-	Logger::notice('importing notifications - done', ['user' => $argv[1]]);
+	Logger::notice('importing notifications - start', ['user' => $argv[1], 'last_poll' => $argv[2]]);
+	bluesky_fetch_notifications($argv[1], $argv[2]);
+	Logger::notice('importing notifications - done', ['user' => $argv[1], 'last_poll' => $argv[2]]);
 }
