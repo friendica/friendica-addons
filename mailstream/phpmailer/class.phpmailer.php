c15a72991f54cff7d3f9d11b65ab7e59d796759e
Artur Weigandt <Art4@users.noreply.github.com>
2024-12-05 18:23:51 +0100

    Update mailstream/phpmailer/class.phpmailer.php
    Co-authored-by: Hypolite Petovan <hypolite@mrpetovan.com>

--- mailstream/phpmailer/class.phpmailer.php
+++ mailstream/phpmailer/class.phpmailer.php
@@ -3287,7 +3287,7 @@ class PHPMailer
         $result = 'localhost.localdomain';
         if (!empty($this->Hostname)) {
             $result = $this->Hostname;
-        } elseif (array_key_exists('SERVER_NAME', $_SERVER) and !empty($_SERVER['SERVER_NAME'])) {
+        } elseif (!empty($_SERVER['SERVER_NAME'])) {
             $result = $_SERVER['SERVER_NAME'];
         } elseif (function_exists('gethostname') && gethostname() !== false) {
             $result = gethostname();
