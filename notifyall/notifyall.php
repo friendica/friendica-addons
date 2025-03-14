06b4f164f5bb26162fa948797b4cc79e09e27fe1
Art4 <art4@wlabs.de>
2025-03-14 08:21:08 +0000

    cast db stmt object to array

--- notifyall/notifyall.php
+++ notifyall/notifyall.php
@@ -50,14 +50,14 @@ function notifyall_post()
 
 	$recipients = DBA::p("SELECT DISTINCT `email` FROM `user`" . DBA::buildCondition($condition), $condition);
 
-	if (! $recipients || !is_iterable($recipients)) {
+	if (! $recipients) {
 		DI::sysmsg()->addNotice(DI::l10n()->t('No recipients found.'));
 		return;
 	}
 
 	$notifyEmail = new NotifyAllEmail(DI::l10n(), DI::config(), DI::baseUrl(), $text);
 
-	foreach ($recipients as $recipient) {
+	foreach (DBA::toArray($recipients) as $recipient) {
 		DI::emailer()->send($notifyEmail->withRecipient($recipient['email']));
 	}
 
