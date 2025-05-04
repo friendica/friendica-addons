5a9e2a2719c82af35a34f636442a9c040ffcfa2d
Marcus Funch <mfm@magenta.dk>
2025-05-04 15:22:12 +0200

    IRC: Use friendica nickname as suggested IRC nickname, when signed in

--- irc/irc.php
+++ irc/irc.php
@@ -81,6 +81,7 @@ function irc_content()
 		if (!$sitechats) {
 			$sitechats = DI::config()->get('irc', 'sitechats');
 		}
+		$usernick = "nick=" . DI::userSession()->getLocalUserNickname() . "&";
 	} else {
 		$sitechats = DI::config()->get('irc','sitechats');
 	}
@@ -117,7 +118,7 @@ function irc_content()
   $o .= <<< EOT
 <h2>IRC chat</h2>
 <p><a href="https://tldp.org/HOWTO/IRC/beginners.html" target="_blank" rel="noopener noreferrer">A beginner's guide to using IRC. [en]</a></p>
-<iframe src="//web.libera.chat?channels=$channels" style="width:100%; max-width:900px; height: 600px;"></iframe>
+<iframe src="//web.libera.chat?{$usernick}channels=$channels" style="width:100%; max-width:900px; height: 600px;"></iframe>
 EOT;
 
 	return $o;
