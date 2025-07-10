9387db358c0edb750ba8cecd1f4624033cc6403d
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2025-07-10 06:30:21 +0200

    update PHPMailer

---
+++ phpmailer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'friendica-addons/phpmailer',
+        'pretty_version' => 'dev-2025.07-rc',
+        'version' => 'dev-2025.07-rc',
+        'reference' => '715f70032330ad14f710284104c8593c7800e125',
+        'type' => 'friendica-addon',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'friendica-addons/phpmailer' => array(
+            'pretty_version' => 'dev-2025.07-rc',
+            'version' => 'dev-2025.07-rc',
+            'reference' => '715f70032330ad14f710284104c8593c7800e125',
+            'type' => 'friendica-addon',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
