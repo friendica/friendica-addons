6669d41c13ff23a57494c541e2ffca23caa2e5b4
loma-one <loma-one@noreply.git.friendi.ca>
2026-07-31 20:35:12 +0200

    Merge pull request 'quickphoto-1' (#1663) from loma-one/friendica-addons:quickphoto-1 into develop
    Reviewed-on: https://git.friendi.ca/friendica/friendica-addons/pulls/1663
    Reviewed-by: Hypolite Petovan <hypolite@mrpetovan.com>

--- quickphoto/quickphoto.php
+++ quickphoto/quickphoto.php
@@ -2,8 +2,8 @@
 
 /**
  * Name: QuickPhoto
- * Description: Replaces the BBCode for inserted images and provides a placeholder for image descriptions.
- * Version: 1.5
+ * Description: Easily edit an image description by replacing the BBCode
+ * Version: 1.7
  * Author: Matthias Ebers <https://loma.ml/profile/feb>
  */
 
@@ -12,43 +12,56 @@ use Friendica\DI;
 
 function quickphoto_install()
 {
-	Hook::register('page_header', 'addon/quickphoto/quickphoto.php', 'quickphoto_header');
-	Hook::register('post_post', 'addon/quickphoto/quickphoto.php', 'quickphoto_post_hook');
+    Hook::register('page_header', 'addon/quickphoto/quickphoto.php', 'quickphoto_header');
+    Hook::register('post_post', 'addon/quickphoto/quickphoto.php', 'quickphoto_post_hook');
 }
 
 function quickphoto_header(&$header)
 {
-	$desc_label = DI::l10n()->t('Image description');
+    $desc_label = DI::l10n()->t('Image description');
 
-	$js_label = json_encode($desc_label);
+    $js_label = json_encode($desc_label);
 
-	$header .= "\n" . '<script type="text/javascript">var qp_i18n = { imageDesc: ' . $js_label . ' };</script>';
-	$header .= "\n" . '<script type="text/javascript" src="/addon/quickphoto/quickphoto.js?v=5.2"></script>' . "\n";
+    $addon_path = '/addon/quickphoto/';
+    $local_path = 'addon/quickphoto/';
+
+    if (file_exists($local_path . 'styles.css')) {
+        $v_css = filemtime($local_path . 'styles.css');
+        $header .= "\n" . '<link rel="stylesheet" type="text/css" href="' . $addon_path . 'styles.css?v=' . $v_css . '" media="all" />';
+    }
+
+    $header .= "\n" . '<script type="text/javascript">var qp_i18n = { imageDesc: ' . $js_label . ' };</script>';
+
+    if (file_exists($local_path . 'quickphoto.js')) {
+        $v_js = filemtime($local_path . 'quickphoto.js');
+        $header .= "\n" . '<script type="text/javascript" src="' . $addon_path . 'quickphoto.js?v=' . $v_js . '"></script>' . "\n";
+    }
 }
 
 function quickphoto_post_hook(&$item)
 {
-	if (strpos($item['body'], '[img]') === false || strpos($item['body'], '|') === false) {
-		return;
-	}
+    if (strpos($item['body'], '[img]') === false || strpos($item['body'], '|') === false) {
+        return;
+    }
 
-	$pattern = '/\[img\](.*?)\|(.*?)\[\/img\]/i';
+    $pattern = '/\\[img\\](.*?)\\|(.*?)\\[\\/img\\]/i';
 
-	$item['body'] = preg_replace_callback($pattern, function ($matches) {
-		$filename    = $matches[1];
-		$description = $matches[2];
+    $item['body'] = preg_replace_callback($pattern, function($matches) {
+        $filename = $matches[1];
+        $description = $matches[2];
 
-		$condition = [
-			'resource-id' => $filename,
-			'uid'         => DI::userSession()->getLocalUserId(),
-		];
+        $condition = [
+            'resource-id' => $filename,
+            'uid' => DI::userSession()->getLocalUserId(),
+            'imgscale'    => 0
+        ];
 
-		$photo = DI::dba()->selectFirst('photo', ['url'], $condition);
+        $photo = DI::dba()->selectFirst('photo', ['url'], $condition);
 
-		if ($photo) {
-			return '[url=' . $photo['url'] . '][img=' . $photo['url'] . ']' . $description . '[/img][/url]';
-		}
+        if ($photo) {
+            return '[url=' . $photo['url'] . '][img=' . $photo['src'] . ']' . $description . '[/img][/url]';
+        }
 
-		return $matches[0];
-	}, $item['body']);
+        return '[img]' . $filename . '[/img]';
+    }, $item['body']);
 }
